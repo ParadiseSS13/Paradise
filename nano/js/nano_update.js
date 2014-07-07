fc1d61ac981c0234efca9b5e8ba7163ebe44eb80
Mark Aherne <mark.aherne@fantom.ie>
2014-07-07 02:05:39 +0100

    Work in progress on a major revision of the NanoUI templating system.

--- nano/js/nano_update.js
+++ nano/js/nano_update.js
@@ -100,7 +100,12 @@ NanoUpdate = function ()
 		{
 			if (templateData.hasOwnProperty(key))
 			{
-				$.when($.get(templateData[key]))
+				$.when($.ajax({
+					url: templateData[key],
+					cache: false,
+					dataType: 'text'
+				}))
+				//$.when($.get(templateData[key]))
 					.done(function(templateMarkup) {					
 						
 						//templateMarkup = templateMarkup.replace(/ +\) *\}\}/g, ')}}');
@@ -134,7 +139,10 @@ NanoUpdate = function ()
 
 							return;
 						}
-					});    
+					})					
+					.fail(function () {
+						alert('Loading template ' + key + '(' + templateData[key] + ') failed!');
+					});;    
 			}
 		}	
 	};
@@ -194,9 +202,12 @@ NanoUpdate = function ()
 	// Execute all callbacks in the callbacks array/object provided, updateData is passed to them for processing and potential modification
 	var executeCallbacks = function (callbacks, updateData)
 	{	
-		for (var index in callbacks)
+		for (var key in callbacks)
 		{
-			updateData = callbacks[index].call(this, updateData);
+			if (callbacks.hasOwnProperty(key) && jQuery.isFunction(callbacks[key]))
+			{
+				updateData = callbacks[key].call(this, updateData);
+			}
 		}
 		
 		return updateData;
