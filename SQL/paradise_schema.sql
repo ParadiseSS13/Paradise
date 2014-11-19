eda047c9ccaa86a446f70387664408b46ad4b338
Markolie <markvanalphen2@gmail.com>
2014-11-19 01:33:38 +0100

    Add default empty schema

---
+++ SQL/paradise_schema.sql
@@ -0,0 +1,599 @@
+CREATE DATABASE  IF NOT EXISTS `paradise` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `paradise`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: paradise
+-- ------------------------------------------------------
+-- Server version	5.6.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `characters`
+--
+
+DROP TABLE IF EXISTS `characters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `slot` int(2) NOT NULL,
+  `OOC_Notes` mediumtext NOT NULL,
+  `real_name` varchar(45) NOT NULL,
+  `name_is_always_random` tinyint(1) NOT NULL,
+  `gender` varchar(11) NOT NULL,
+  `age` smallint(4) NOT NULL,
+  `species` varchar(45) NOT NULL,
+  `language` varchar(45) NOT NULL,
+  `hair_red` smallint(4) NOT NULL,
+  `hair_green` smallint(4) NOT NULL,
+  `hair_blue` smallint(4) NOT NULL,
+  `facial_red` smallint(4) NOT NULL,
+  `facial_green` smallint(4) NOT NULL,
+  `facial_blue` smallint(4) NOT NULL,
+  `skin_tone` smallint(4) NOT NULL,
+  `skin_red` smallint(4) NOT NULL,
+  `skin_green` smallint(4) NOT NULL,
+  `skin_blue` smallint(4) NOT NULL,
+  `hair_style_name` varchar(45) NOT NULL,
+  `facial_style_name` varchar(45) NOT NULL,
+  `eyes_red` smallint(4) NOT NULL,
+  `eyes_green` smallint(4) NOT NULL,
+  `eyes_blue` smallint(4) NOT NULL,
+  `underwear` smallint(4) NOT NULL,
+  `undershirt` smallint(4) NOT NULL,
+  `backbag` smallint(4) NOT NULL,
+  `b_type` varchar(45) NOT NULL,
+  `alternate_option` smallint(4) NOT NULL,
+  `job_support_high` mediumint(8) NOT NULL,
+  `job_support_med` mediumint(8) NOT NULL,
+  `job_support_low` mediumint(8) NOT NULL,
+  `job_medsci_high` mediumint(8) NOT NULL,
+  `job_medsci_med` mediumint(8) NOT NULL,
+  `job_medsci_low` mediumint(8) NOT NULL,
+  `job_engsec_high` mediumint(8) NOT NULL,
+  `job_engsec_med` mediumint(8) NOT NULL,
+  `job_engsec_low` mediumint(8) NOT NULL,
+  `job_karma_high` mediumint(8) NOT NULL,
+  `job_karma_med` mediumint(8) NOT NULL,
+  `job_karma_low` mediumint(8) NOT NULL,
+  `flavor_text` mediumtext NOT NULL,
+  `med_record` mediumtext NOT NULL,
+  `sec_record` mediumtext NOT NULL,
+  `gen_record` mediumtext NOT NULL,
+  `be_special` mediumint(8) NOT NULL,
+  `disabilities` mediumint(8) NOT NULL,
+  `player_alt_titles` mediumtext NOT NULL,
+  `organ_data` mediumtext NOT NULL,
+  `nanotrasen_relation` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `characters`
+--
+
+LOCK TABLES `characters` WRITE;
+/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
+/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customuseritems`
+--
+
+DROP TABLE IF EXISTS `customuseritems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customuseritems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cuiCKey` varchar(36) NOT NULL,
+  `cuiRealName` varchar(60) NOT NULL,
+  `cuiPath` varchar(255) NOT NULL,
+  `cuiDescription` text NOT NULL,
+  `cuiReason` text NOT NULL,
+  `cuiPropAdjust` text NOT NULL,
+  `cuiJobMask` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customuseritems`
+--
+
+LOCK TABLES `customuseritems` WRITE;
+/*!40000 ALTER TABLE `customuseritems` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customuseritems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `death`
+--
+
+DROP TABLE IF EXISTS `death`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `death` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pod` text NOT NULL COMMENT 'Place of death',
+  `coord` text NOT NULL COMMENT 'X, Y, Z POD',
+  `tod` datetime NOT NULL COMMENT 'Time of death',
+  `job` text NOT NULL,
+  `special` text NOT NULL,
+  `name` text NOT NULL,
+  `byondkey` text NOT NULL,
+  `laname` text NOT NULL COMMENT 'Last attacker name',
+  `lakey` text NOT NULL COMMENT 'Last attacker key',
+  `gender` text NOT NULL,
+  `bruteloss` int(11) NOT NULL,
+  `brainloss` int(11) NOT NULL,
+  `fireloss` int(11) NOT NULL,
+  `oxyloss` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `death`
+--
+
+LOCK TABLES `death` WRITE;
+/*!40000 ALTER TABLE `death` DISABLE KEYS */;
+/*!40000 ALTER TABLE `death` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_admin`
+--
+
+DROP TABLE IF EXISTS `erro_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `rank` varchar(32) NOT NULL DEFAULT 'Administrator',
+  `level` int(2) NOT NULL DEFAULT '0',
+  `flags` int(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_admin`
+--
+
+LOCK TABLES `erro_admin` WRITE;
+/*!40000 ALTER TABLE `erro_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_admin_log`
+--
+
+DROP TABLE IF EXISTS `erro_admin_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `adminckey` varchar(32) NOT NULL,
+  `adminip` varchar(18) NOT NULL,
+  `log` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_admin_log`
+--
+
+LOCK TABLES `erro_admin_log` WRITE;
+/*!40000 ALTER TABLE `erro_admin_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_admin_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_ban`
+--
+
+DROP TABLE IF EXISTS `erro_ban`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_ban` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bantime` datetime NOT NULL,
+  `serverip` varchar(32) NOT NULL,
+  `bantype` varchar(32) NOT NULL,
+  `reason` text NOT NULL,
+  `job` varchar(32) DEFAULT NULL,
+  `duration` int(11) NOT NULL,
+  `rounds` int(11) DEFAULT NULL,
+  `expiration_time` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `ip` varchar(32) NOT NULL,
+  `a_ckey` varchar(32) NOT NULL,
+  `a_computerid` varchar(32) NOT NULL,
+  `a_ip` varchar(32) NOT NULL,
+  `who` text NOT NULL,
+  `adminwho` text NOT NULL,
+  `edits` text,
+  `unbanned` tinyint(1) DEFAULT NULL,
+  `unbanned_datetime` datetime DEFAULT NULL,
+  `unbanned_ckey` varchar(32) DEFAULT NULL,
+  `unbanned_computerid` varchar(32) DEFAULT NULL,
+  `unbanned_ip` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_ban`
+--
+
+LOCK TABLES `erro_ban` WRITE;
+/*!40000 ALTER TABLE `erro_ban` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_ban` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_feedback`
+--
+
+DROP TABLE IF EXISTS `erro_feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `round_id` int(8) NOT NULL,
+  `var_name` varchar(32) NOT NULL,
+  `var_value` int(16) DEFAULT NULL,
+  `details` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_feedback`
+--
+
+LOCK TABLES `erro_feedback` WRITE;
+/*!40000 ALTER TABLE `erro_feedback` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_player`
+--
+
+DROP TABLE IF EXISTS `erro_player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `firstseen` datetime NOT NULL,
+  `lastseen` datetime NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `lastadminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  `ooccolor` varchar(7) DEFAULT '#b82e00',
+  `UI_style` varchar(10) DEFAULT 'Midnight',
+  `UI_style_color` varchar(7) DEFAULT '#ffffff',
+  `UI_style_alpha` smallint(4) DEFAULT '255',
+  `be_special` mediumint(8) DEFAULT '0',
+  `default_slot` smallint(4) DEFAULT '1',
+  `toggles` mediumint(8) DEFAULT '383',
+  `sound` mediumint(8) DEFAULT '31',
+  `randomslot` tinyint(1) DEFAULT '0',
+  `volume` smallint(4) DEFAULT '100',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ckey` (`ckey`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_player`
+--
+
+LOCK TABLES `erro_player` WRITE;
+/*!40000 ALTER TABLE `erro_player` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_poll_option`
+--
+
+DROP TABLE IF EXISTS `erro_poll_option`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pollid` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `percentagecalc` tinyint(1) NOT NULL DEFAULT '1',
+  `minval` int(3) DEFAULT NULL,
+  `maxval` int(3) DEFAULT NULL,
+  `descmin` varchar(32) DEFAULT NULL,
+  `descmid` varchar(32) DEFAULT NULL,
+  `descmax` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_poll_option`
+--
+
+LOCK TABLES `erro_poll_option` WRITE;
+/*!40000 ALTER TABLE `erro_poll_option` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_poll_option` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_poll_question`
+--
+
+DROP TABLE IF EXISTS `erro_poll_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `polltype` varchar(16) NOT NULL DEFAULT 'OPTION',
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `adminonly` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_poll_question`
+--
+
+LOCK TABLES `erro_poll_question` WRITE;
+/*!40000 ALTER TABLE `erro_poll_question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_poll_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_poll_textreply`
+--
+
+DROP TABLE IF EXISTS `erro_poll_textreply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_textreply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `replytext` text NOT NULL,
+  `adminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_poll_textreply`
+--
+
+LOCK TABLES `erro_poll_textreply` WRITE;
+/*!40000 ALTER TABLE `erro_poll_textreply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_poll_textreply` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_poll_vote`
+--
+
+DROP TABLE IF EXISTS `erro_poll_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `optionid` int(11) NOT NULL,
+  `ckey` varchar(255) NOT NULL,
+  `ip` varchar(16) NOT NULL,
+  `adminrank` varchar(32) NOT NULL,
+  `rating` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_poll_vote`
+--
+
+LOCK TABLES `erro_poll_vote` WRITE;
+/*!40000 ALTER TABLE `erro_poll_vote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_poll_vote` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `erro_privacy`
+--
+
+DROP TABLE IF EXISTS `erro_privacy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_privacy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `option` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `erro_privacy`
+--
+
+LOCK TABLES `erro_privacy` WRITE;
+/*!40000 ALTER TABLE `erro_privacy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erro_privacy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `karma`
+--
+
+DROP TABLE IF EXISTS `karma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `karma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `spendername` text NOT NULL,
+  `spenderkey` text NOT NULL,
+  `receivername` text NOT NULL,
+  `receiverkey` text NOT NULL,
+  `receiverrole` text,
+  `receiverspecial` text,
+  `isnegative` tinyint(1) DEFAULT NULL,
+  `spenderip` text NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `karma`
+--
+
+LOCK TABLES `karma` WRITE;
+/*!40000 ALTER TABLE `karma` DISABLE KEYS */;
+/*!40000 ALTER TABLE `karma` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `karmatotals`
+--
+
+DROP TABLE IF EXISTS `karmatotals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `karmatotals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `byondkey` text NOT NULL,
+  `karma` int(11) NOT NULL,
+  `karmaspent` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `karmatotals`
+--
+
+LOCK TABLES `karmatotals` WRITE;
+/*!40000 ALTER TABLE `karmatotals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `karmatotals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `library`
+--
+
+DROP TABLE IF EXISTS `library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `library` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` text NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `category` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `library`
+--
+
+LOCK TABLES `library` WRITE;
+/*!40000 ALTER TABLE `library` DISABLE KEYS */;
+/*!40000 ALTER TABLE `library` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `population`
+--
+
+DROP TABLE IF EXISTS `population`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `population` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `playercount` int(11) DEFAULT NULL,
+  `admincount` int(11) DEFAULT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `population`
+--
+
+LOCK TABLES `population` WRITE;
+/*!40000 ALTER TABLE `population` DISABLE KEYS */;
+/*!40000 ALTER TABLE `population` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `whitelist`
+--
+
+DROP TABLE IF EXISTS `whitelist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `whitelist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` text NOT NULL,
+  `job` text,
+  `species` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `whitelist`
+--
+
+LOCK TABLES `whitelist` WRITE;
+/*!40000 ALTER TABLE `whitelist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `whitelist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-11-19  1:24:17
