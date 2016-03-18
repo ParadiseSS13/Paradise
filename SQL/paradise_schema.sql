12a51cfea16c23b92ccecffca34424dfd76a724a
ITBlackwood <itdarkpain@mail.ru>
2016-03-18 17:29:58 +0800

    SQL_schemas fixed

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -246,6 +246,7 @@ CREATE TABLE `player` (
   `sound` mediumint(8) DEFAULT '31',
   `randomslot` tinyint(1) DEFAULT '0',
   `volume` smallint(4) DEFAULT '100',
+  `nanoui_fancy` smallint(4) DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `ckey` (`ckey`)
 ) ENGINE=InnoDB AUTO_INCREMENT=32446 DEFAULT CHARSET=latin1;
