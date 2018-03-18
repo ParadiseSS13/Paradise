b59f524964ecf68d9d0640346d2467f03f0fdfc1
Tayyyyyyy <Tayyyyyyy@users.noreply.github.com>
2018-03-18 10:46:11 -0700

    Fix admin pm to not appearing

--- goon/newbrowserassets/src/components/filterClasses.js
+++ goon/newbrowserassets/src/components/filterClasses.js
@@ -2,7 +2,7 @@ const filterClasses = {
   asay: ['admin_channel'],
   msay: ['mentor_channel', 'mentor_channel_admin'],
   dsay: ['deadchat'],
-  pm: ['adminhelp', 'mentorhelp', 'playerreply'],
+  pm: ['adminhelp', 'mentorhelp', 'playerreply', 'adminpm'],
   logs: ['admin'],
   ooc: ['ooc'],
   looc: ['looc'],
