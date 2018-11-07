6adfa935bd5ed4285bf5ed059bdf04c09b37360a
tigercat2000 <nick.pilant2@gmail.com>
2018-11-07 00:02:56 -0800

    Make 4-5 actually work

---
+++ SQL/updates/4-5.py
@@ -0,0 +1,101 @@
+# Listen up. 
+# In order to run this FUCKING script on Windows, you need to make sure you have Python **3.6.7** installed.
+# It won't work on 3.5, it won't work on 2.7, it won't work on fucking 3.7. 
+# After you get that shit sorted out, you need to run `pip install mysqlclient==1.3.12`
+# If you don't include the ==1.3.12 part, good fucking luck ever getting the piece of fucking shit to build
+# Fuck Windows.
+# If you're on Linux, you can probably figure out how to install a damn python dependency. https://github.com/PyMySQL/mysqlclient-python
+# Have fun. I sure didn't.
+
+#!/usr/bin/env python3
+import MySQLdb
+import argparse
+import struct, socket
+import time
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("address", help="MySQL server address (use localhost for the current computer)")
+parser.add_argument("username", help="MySQL login username")
+parser.add_argument("password", help="MySQL login password")
+parser.add_argument("database", help="Database name")
+parser.add_argument("curtable", help="Name of the current feedback table (remember prefixes if you use them)")
+parser.add_argument("newtable", help="Name of the new table to insert to, can't be same as the source table (remember prefixes)")
+
+args = parser.parse_args()
+db = MySQLdb.connect(host=args.address, user=args.username, passwd=args.password, db=args.database)
+cursor = db.cursor()
+current_table = args.curtable
+new_table = args.newtable
+
+# Populate the target table with legacy data.
+cursor.execute("SELECT round_id, var_name, details FROM {0} WHERE var_name IN ( \
+'round_start', 'round_end', 'server_ip', 'game_mode', 'round_end_result' )".format(current_table))
+
+var_name_mapping = {
+	'round_start': 'start_datetime',
+	'round_end': 'end_datetime',
+	'server_ip': 'server_ip',
+	'game_mode': 'game_mode',
+	'round_end_result': 'game_mode_result'
+}
+
+rounds = {}
+
+rows = cursor.fetchall()
+print("Found ", len(rows), "rows to parse.")
+for row in rows:
+	round_id = row[0]
+	var_name = row[1]
+	details = row[2]
+
+	if not round_id in rounds:
+		rounds[round_id] = {}
+
+	rounds_obj = rounds[round_id]
+	if not var_name_mapping[var_name] in rounds_obj:
+		if var_name == 'round_start':
+			# 'Wed Mar 21 23:55:05 2018'
+			date_obj = datetime.strptime(details, "%a %b %d %H:%M:%S %Y")
+			rounds_obj[var_name_mapping[var_name]] = date_obj
+			rounds_obj['initialize_datetime'] = date_obj
+		elif var_name == 'round_end':
+			rounds_obj[var_name_mapping[var_name]] = datetime.strptime(details, "%a %b %d %H:%M:%S %Y")
+		elif var_name == 'server_ip':
+			server_ip_arr = details.split(':')
+			if not server_ip_arr[0]:
+				server_ip_arr[0] = '0.0.0.0'
+			rounds_obj['server_ip'] = struct.unpack("!I", socket.inet_aton(server_ip_arr[0]))[0]
+			rounds_obj['server_port'] = int(server_ip_arr[1])
+		else:
+			rounds_obj[var_name_mapping[var_name]] = details
+
+print("Assembled objects for", len(rounds) ,"rounds.")
+
+for round in rounds:
+	round_id = round
+	round_obj = rounds[round]
+
+	initialize_datetime =	round_obj["initialize_datetime"]	if "initialize_datetime" in round_obj	else None
+	start_datetime =		round_obj["start_datetime"]			if "start_datetime" in round_obj		else None
+	end_datetime =			round_obj["end_datetime"]			if "end_datetime" in round_obj			else None
+	server_ip =				round_obj["server_ip"]				if "server_ip" in round_obj				else None
+	server_port =			round_obj["server_port"]			if "server_port" in round_obj			else None
+	game_mode =				round_obj["game_mode"]				if "game_mode" in round_obj				else None
+	game_mode_result =		round_obj["game_mode_result"]		if "game_mode_result" in round_obj		else None
+
+	# All of these are required fields in the new system. If they're not present in our compiled data, there's not much we can do.
+	if not initialize_datetime or not server_ip or not server_port:
+		# print("Round lost:", round_id, round_obj)
+		continue
+
+	cursor.execute("SELECT EXISTS(SELECT id FROM {0} WHERE id = {1})".format(new_table, round_id))
+	exists = cursor.fetchone()[0]
+	if not exists:
+		args = (round_id, initialize_datetime, start_datetime, end_datetime, server_ip, server_port, game_mode, game_mode_result)
+		query = "INSERT INTO {0} (id, initialize_datetime, start_datetime, end_datetime, server_ip, server_port, game_mode, game_mode_result) \
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)".format(new_table)
+		cursor.execute(query, args)
+
+cursor.close()
+db.commit()
--- SQL/updates/4-5.sql
+++ SQL/updates/4-5.sql
@@ -1,9 +1,8 @@
 -- 20 May 2017, by Jordie0608
 
 -- Created table `round` to replace tracking of the datapoints 'round_start', 'round_end', 'server_ip', 'game_mode', 'round_end_results', 'end_error', 'end_proper', 'emergency_shuttle', 'map_name' and 'station_renames' in the `feedback` table.
--- Once created this table is populated with rows from the `feedback` table.
+-- Once created, you must run 4-5.py to populate this table. Run python 4-5.py --help for usage.
 
-START TRANSACTION;
 CREATE TABLE `paradise`.`round` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `initialize_datetime` DATETIME NOT NULL,
@@ -20,14 +19,6 @@ CREATE TABLE `paradise`.`round` (
   `station_name` VARCHAR(80) NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-INSERT INTO `paradise`.`round`
-(`id`, `start_datetime`, `end_datetime`, `server_ip`, `server_port`, `commit_hash`, `game_mode`, `game_mode_result`, `end_state`, `shuttle_name`,
-	`map_name`, `station_name`)
-SELECT DISTINCT ri.round_id, IFNULL(STR_TO_DATE(st.details,'%a %b %e %H:%i:%s %Y'), TIMESTAMP(0)), STR_TO_DATE(et.details,'%a %b %e %H:%i:%s %Y'), IFNULL(INET_ATON(SUBSTRING_INDEX(IF(si.details = '', '0', IF(SUBSTRING_INDEX(si.details, ':', 1) LIKE '%_._%', si.details, '0')), ':', 1)), INET_ATON(0)), IFNULL(IF(si.details LIKE '%:_%', CAST(SUBSTRING_INDEX(si.details, ':', -1) AS UNSIGNED), '0'), '0'), ch.details, gm.details, mr.details, IFNULL(es.details, ep.details), ss.details, mn.details, sn.details
-FROM `paradise`.`feedback`AS ri
-LEFT JOIN `paradise`.`feedback` AS st ON ri.round_id = st.round_id AND st.var_name = "round_start" LEFT JOIN `paradise`.`feedback` AS et ON ri.round_id = et.round_id AND et.var_name = "round_end" LEFT JOIN `paradise`.`feedback` AS si ON ri.round_id = si.round_id AND si.var_name = "server_ip" LEFT JOIN `paradise`.`feedback` AS ch ON ri.round_id = ch.round_id AND ch.var_name = "revision" LEFT JOIN `paradise`.`feedback` AS gm ON ri.round_id = gm.round_id AND gm.var_name = "game_mode" LEFT JOIN `paradise`.`feedback` AS mr ON ri.round_id = mr.round_id AND mr.var_name = "round_end_result" LEFT JOIN `paradise`.`feedback` AS es ON ri.round_id = es.round_id AND es.var_name = "end_state" LEFT JOIN `paradise`.`feedback` AS ep ON ri.round_id = ep.round_id AND ep.var_name = "end_proper" LEFT JOIN `paradise`.`feedback` AS ss ON ri.round_id = ss.round_id AND ss.var_name = "emergency_shuttle" LEFT JOIN `paradise`.`feedback` AS mn ON ri.round_id = mn.round_id AND mn.var_name = "map_name" LEFT JOIN `paradise`.`feedback` AS sn ON ri.round_id = sn.round_id AND sn.var_name = "station_renames";
-COMMIT;
 
 -- It's not necessary to delete the rows from the `feedback` table but henceforth these datapoints will be in the `round` table.
-
 -- Remember to add a prefix to the table names if you use them
