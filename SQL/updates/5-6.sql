4ac50d4806c2d1295add4a15caae19ec62d03fa3
Kyep <Kyep@users.noreply.github.com>
2019-05-04 22:27:56 -0700

    add comment for SQL update

--- SQL/updates/5-6.sql
+++ SQL/updates/5-6.sql
@@ -1,3 +1,5 @@
+#Updating the SQL from version 5 to version 6. -Kyep
+#Creating a table to track the results of VPN/proxy lookups for IPs
 CREATE TABLE  `ipintel` (
 `ip` INT UNSIGNED NOT NULL ,
 `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL ,
