24e9771c099d13a7a41dc5530947b602cd768abf
AffectedArc07 <josephcase55@hotmail.com>
2019-05-22 19:02:05 +0100

    Tweaks

--- scripts/discord_webhook.py
+++ scripts/discord_webhook.py
@@ -10,11 +10,6 @@ import sys
 # Arg 0 is the script name itself (discord_webhook.py)
 scriptname = sys.argv[0] # This exists so that the script name wont get sent in the final
 url = sys.argv[1] # First entry
-message = ""
-for item in sys.argv:
-    if item == scriptname or item == url:
-        pass
-    else:
-        message += str(item) + " "
+message = " ".join(sys.argv[2:])
 
 requests.post(url, ("{\"content\": \""+str(message)+"\"}"), headers={"Content-Type": "application/json"})
