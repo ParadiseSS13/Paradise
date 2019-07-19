88f0d1e98c28fbba5efbe95de169803f6a71fb57
Aurorablade <lemastera@gmail.com>
2019-07-19 18:52:05 -0400

    Rename for branch update

---
+++ SQL/updates/7-8.sql
@@ -0,0 +1,9 @@
+#Updating the SQL from version 7 to version 8. -fethas
+#Adding new columns to contain special character names.
+ALTER TABLE `characters`
+	ADD `clown_name` varchar(45) NOT NULL AFTER `autohiss`,
+	ADD `mime_name` varchar(45) NOT NULL AFTER `clown_name`,
+	ADD `ai_name` varchar(45) NOT NULL AFTER `mime_name`,
+	ADD `cyborg_name` varchar(45) NOT NULL AFTER `ai_name`,
+	ADD `diety_name` varchar(45) NOT NULL AFTER `cyborg_name`,
+	ADD `religion_name` varchar(45) NOT NULL AFTER `diety_name`;
