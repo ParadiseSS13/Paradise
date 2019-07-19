f3049e6fad8be3c39495ba0b0a25687f1a06d3a5
Aurorablade <lemastera@gmail.com>
2019-07-19 18:54:02 -0400

    Delete Old file so i can update to the new sql file
    NEW computer setup...christ.

--- SQL/updates/6-7.sql
+++
@@ -1,9 +0,0 @@
-#Updating the SQL from version 6 to version 7. -fethas
-#Adding new columns to contain special character names.
-ALTER TABLE `characters`
-	ADD `clown_name` varchar(45) NOT NULL AFTER `autohiss`,
-	ADD `mime_name` varchar(45) NOT NULL AFTER `clown_name`,
-	ADD `ai_name` varchar(45) NOT NULL AFTER `mime_name`,
-	ADD `cyborg_name` varchar(45) NOT NULL AFTER `ai_name`,
-	ADD `diety_name` varchar(45) NOT NULL AFTER `cyborg_name`,
-	ADD `religion_name` varchar(45) NOT NULL AFTER `diety_name`;
