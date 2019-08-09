adb3463a8afd7f29a46285fcea5cb5820c23df75
Couls <coul422@gmail.com>
2019-08-09 02:21:45 -0400

    Update the update file

--- SQL/updates/8-9.sql
+++ SQL/updates/8-9.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `feedback`.`notes` ADD COLUMN `secret` TINYINT(1) NOT NULL DEFAULT '1'  AFTER `server`
+# Add secret column to notes to allow for admins to hide and show admin remarks to players
+ALTER TABLE `notes` ADD COLUMN `secret` TINYINT(1) NOT NULL DEFAULT '1'  AFTER `server`
