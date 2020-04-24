de5abba74bc31c3e99ca2a9d2301fdc59e659199
AffectedArc07 <josephcase55@hotmail.com>
2020-04-24 19:54:16 +0100

    RUST gone bust

--- tools/travis/build_rust_g.sh
+++ tools/travis/build_rust_g.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 git clone https://github.com/ParadiseSS13/rust-g.git --depth 1
 
 cd rust-g
+# Install and prepare rust for building
+source ~/.cargo/env
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-host i686-unknown-linux-gnu
+
+# Build it
 cargo build --release
 
+# Symlink it to the next dir down (Server root)
 ln -s $PWD/target/release/librust_g.so ../librust_g.so
