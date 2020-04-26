83d7830e8d9695ac4485ae205f13bc16b1782565
AffectedArc07 <josephcase55@hotmail.com>
2020-04-26 10:38:33 +0100

    Cleans up the remains of the old changelogs

--- tools/oldChangelog/makeChangelog.bat
+++ tools/oldChangelog/makeChangelog.bat
@@ -1,4 +1,5 @@
 @echo off
 rem Cheridan asked for this. - N3X
-call python ss13_genchangelog.py ../html/changelog.html ../html/changelogs
+rem This has been updated to use the old changelog directories as of PR #13051 -AA
+call python ss13_genchangelog.py ../../html/archived_changelog/changelog.html ../../html/archived_changelog/changelogs
 pause
