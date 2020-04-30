88b56d739eb40df458be629cb648a43fb78ad1ff
AffectedArc07 <josephcase55@hotmail.com>
2020-05-01 00:38:01 +0100

    Removes un-needed stuff

--- tools/travis/install_libmariadb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-wget http://www.byond.com/download/db/mariadb_client-2.0.0-linux.tgz
-tar -xvf mariadb_client-2.0.0-linux.tgz
-mv mariadb_client-2.0.0-linux/libmariadb.so ./
-rm -rf mariadb_client-2.0.0-linux.tgz mariadb_client-2.0.0-linux
-
