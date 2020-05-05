b65030fec5976581f618983180a762cb5434ce9a
AffectedArc07 <josephcase55@hotmail.com>
2020-05-05 10:13:01 +0100

    I need to learn to read my todos

--- tools/travis/check_grep.sh
+++ tools/travis/check_grep.sh
@@ -14,12 +14,10 @@ if grep -P 'step_[xy]' _maps/**/*.dmm;	then
     echo "ERROR: step_x/step_y variables detected in maps, please remove them."
     st=1
 fi;
-#### TODO: Uncomment after managing all globs ####
-#if grep -P '^/*var/' code/**/*.dm; then
-    #echo "ERROR: Unmanaged global var use detected in code, please use the helpers."
-    #st=1
-#fi;
-####
+if grep -P '^/*var/' code/**/*.dm; then
+    echo "ERROR: Unmanaged global var use detected in code, please use the helpers."
+    st=1
+fi;
 nl='
 '
 nl=$'\n'
