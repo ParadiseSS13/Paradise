7ea6f190c5055ee4609de6e8a9abd8d2d95560ae
AffectedArc07 <josephcase55@hotmail.com>
2020-05-18 08:34:28 +0100

    Changelog Overhaul (#13051)
    * Changelog Overhaul
    
    * SQL Update
    
    * This is why I hate merge conflicts
    
    * Comment Correction
    
    * Farie Fixes + Tested with blank DB
    
    * Colours + Titles
    
    * Colour tweaks
    
    * I fell victim to my own CI Chains!
    
    * Cleans up the remains of the old changelogs
    
    * Fixes formatting
    
    * Kyet Changes
    
    * Date

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -595,3 +595,17 @@ CREATE TABLE `connection_log` (
   `computerid` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `changelog`
+--
+DROP TABLE IF EXISTS `changelog`;
+CREATE TABLE `changelog` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`pr_number` INT(11) NOT NULL,
+	`date_merged` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`author` VARCHAR(32) NOT NULL,
+	`cl_type` ENUM('FIX','WIP','TWEAK','SOUNDADD','SOUNDDEL','CODEADD','CODEDEL','IMAGEADD','IMAGEDEL','SPELLCHECK','EXPERIMENT') NOT NULL,
+	`cl_entry` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
