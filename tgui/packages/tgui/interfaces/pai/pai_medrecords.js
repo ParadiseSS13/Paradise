960efdd5e78fb4f041973202d4fbd1aa22f8a994
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2020-10-15 12:19:57 +0100

    [READY] TGUI pAIs (#14524)
    * TGUI pAIs
    
    * Nukes an old template
    
    * pAI Crew Manifest
    
    * pAI Atmos Scan
    
    * pAI Medical Records
    
    * pAI Security Records + code cleanup
    
    * pAI directives + More refactoring
    
    * Sorts the toggles out
    
    * pAI messenger
    
    * WIP - Radios
    
    * pAI radio - working
    
    * pAI Signalers
    
    * Nukes some arleady gone templates
    
    * Forgot to purge this too
    
    * pAI Door Jack
    
    * pAI Bioscan
    
    * Mochi tweaks
    
    * Farie Fixes
    
    * Spacing

--- tgui/packages/tgui/interfaces/pai/pai_medrecords.js
+++ tgui/packages/tgui/interfaces/pai/pai_medrecords.js
@@ -1,7 +1,7 @@
 import { useBackend } from "../../backend";
 import { SimpleRecords } from "../common/SimpleRecords";
 
-export const pda_medical = (props, context) => {
+export const pai_medrecords = (props, context) => {
   const { data } = useBackend(context);
-  return <SimpleRecords data={data} recordType="MED" />;
+  return <SimpleRecords data={data.app_data} recordType="MED" />;
 };
