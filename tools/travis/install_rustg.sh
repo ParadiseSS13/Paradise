03fbe7b3d8cde079e51aa1881cf0ae752f2e1cef
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2020-10-27 10:12:12 +0000

    *sigh*

--- tools/travis/install_rustg.sh
+++ tools/travis/install_rustg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source dependencies.sh
+source _build_dependencies.sh
 
 mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/librust_g.so "https://github.com/ParadiseSS13/rust-g/releases/download/$RUSTG_VERSION/librust_g.so"
