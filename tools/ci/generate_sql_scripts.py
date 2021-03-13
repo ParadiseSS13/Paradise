3ff86ad4a6541772a01a7706a14d8b534e9d9931
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-03-13 10:47:50 +0000

    And this

--- tools/ci/generate_sql_scripts.py
+++ tools/ci/generate_sql_scripts.py
@@ -79,7 +79,6 @@ scriptLines.append("mysql -u root -proot -e 'DROP DATABASE feedback;'\n")
 scriptLines.append("mysql -u root -proot < SQL/paradise_schema_prefixed.sql\n")
 scriptLines.append("mysql -u root -proot -e 'DROP DATABASE feedback;'\n")
 scriptLines.append("mysql -u root -proot < SQL/paradise_schema.sql\n")
-scriptLines.append("mysql -u root -proot -e 'GRANT ALL on feedback.* TO `ci_sql`@`127.0.0.1` IDENTIFIED BY \"not_a_strong_password\";'\n")
 
 outputScript = open("tools/ci/validate_sql.sh", "w+")
 outputScript.writelines(scriptLines)
