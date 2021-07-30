c541ac2800befbc96b1b7fc8691e9866550a95a4
SabreML <57483089+SabreML@users.noreply.github.com>
2021-07-30 14:06:28 +0100

    Review 1, plus json fixes

--- tools/loadout_converter/loadout_converter.py
+++ tools/loadout_converter/loadout_converter.py
@@ -3,14 +3,10 @@
 
 # Note: This must ONLY be run once, and ONLY if your loadout records are using the old system.
 
-import re
-import mysql.connector
-import argparse
-import json
-import time
+import re, mysql.connector, argparse, json, time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("address", help="SQL server address (use 127.0.0.1 for the current computer)")
+parser.add_argument("address", help="SQL server address (Use 127.0.0.1 for the current computer)")
 parser.add_argument("username", help="SQL login username")
 parser.add_argument("password", help="SQL login password")
 parser.add_argument("database", help="Database name (Default: paradise_gamedb)")
@@ -27,7 +23,7 @@ with open('loadout_converter.json') as json_file:
 	standard_replacements = json["standard"]
 	special_replacements = json["special"]
 
-cursor.execute("SELECT `id`, `gear` FROM `characters` WHERE `gear` <> '';")
+cursor.execute("SELECT id, gear FROM characters WHERE gear != \"\";")
 temp = cursor.fetchall()
 
 # Convert the rows into a dictionary. (Key: `id`, Value: `gear`)
@@ -36,7 +32,7 @@ loadouts = {}
 for i in temp:
 	row = {i[0]: i[1]}
 	loadouts.update(row)
-print(f"Cached gear listings.\n")
+print("Cached gear listings.")
 
 # Replace all `display_name`s in the list with their typepaths.
 print("Converting gear listings...")
@@ -54,15 +50,15 @@ for item in loadouts:
 	# Convert to JSON formatting.
 	loadouts[item] = loadouts[item].replace("&", ",")
 	loadouts[item] = "[" + loadouts[item] + "]"
-print(f"Converted {len(loadouts)} gear listings.\n")
+print("Converted {} gear listings.".format(len(loadouts)))
 
 # Update the database.
 print("Updating database entries...")
 for id in loadouts:
 	gear = loadouts[id]
-	cursor.execute(f"UPDATE `characters` SET `gear` = '{gear}' WHERE `id` = '{id}';")
+	stmt = "UPDATE characters SET gear = %s WHERE id = %s;"
+	cursor.execute(stmt, (gear, id))
 db.commit()
 
 stopwatch = time.time()
-
-print(f"Database updated in {round(stopwatch - startwatch)} seconds!")
+print("Database updated in {} seconds!".format(round(stopwatch - startwatch)))
