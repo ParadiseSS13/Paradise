f8ccfbafcb2c7a019db9b495bbd6076f1d0cf9bc
SabreML <57483089+SabreML@users.noreply.github.com>
2021-07-31 17:33:06 +0100

    (Hopefully) final tweaks

--- tools/loadout_converter/loadout_converter.py
+++ tools/loadout_converter/loadout_converter.py
@@ -19,7 +19,8 @@ startwatch = time.time()
 
 with open('loadout_converter.json') as json_file:
 	json = json.load(json_file)
-	# Format for both variables is (Key: `display_name`, Value: typepath)
+	removals = json["remove"]
+	# Format for both replacement variables is (Key: `display_name`, Value: typepath)
 	standard_replacements = json["standard"]
 	special_replacements = json["special"]
 
@@ -34,23 +35,32 @@ for i in temp:
 	loadouts.update(row)
 print("Cached gear listings.")
 
+
 # Replace all `display_name`s in the list with their typepaths.
 print("Converting gear listings...")
-for item in loadouts:
-	for name in standard_replacements:
-		loadouts[item] = loadouts[item].replace(name, f"\"{standard_replacements[name]}\"")
+for gear in loadouts:
+	for item in standard_replacements:
+		loadouts[gear] = loadouts[gear].replace(item, f"\"{standard_replacements[item]}\"")
 
-	for thing in special_replacements:
-		loadouts[item] = re.sub(fr'(^|&)(?:{thing})($|&)', fr'\1"{special_replacements[thing]}"\2', loadouts[item])
+	for item in removals:
+		loadouts[gear] = re.sub(fr'{item}(&|)|(&|){item}', "", loadouts[gear])
+		# Replace anything in `removals` with "", plus an & before OR after if there is one.
+
+	for item in special_replacements:
+		loadouts[gear] = re.sub(fr'(^|&)(?:{item})($|&)', fr'\1"{special_replacements[item]}"\2', loadouts[gear])
 		# This is horrible, but as far as I can tell there's no other way of doing it.
 		# If the subtype is converted first and then the parent type, you get "/datum/gear/accessory//datum/gear/accessory/scarf/christmas".
 		# If the parent type is converted first and then the subtype, you get "/datum/gear/accessory/scarf%2c+christmas".
-		# This regex ensures that either the start of the string, the end of the string, or an & character are surrounding `thing`, so no false matches.
+		# This regex ensures that either the start of the string, the end of the string, or an & character are surrounding `item`, so no false matches.
+
+	if not loadouts[gear]:
+		continue # If after the replacements there's nothing left, don't reformat it.
 
 	# Convert to JSON formatting.
-	loadouts[item] = loadouts[item].replace("&", ",")
-	loadouts[item] = "[" + loadouts[item] + "]"
+	loadouts[gear] = loadouts[gear].replace("&", ",")
+	loadouts[gear] = "[" + loadouts[gear] + "]"
 print("Converted {} gear listings.".format(len(loadouts)))
+
 
 # Update the database.
 print("Updating database entries...")
