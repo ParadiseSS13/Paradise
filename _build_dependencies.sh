04e3481bf3353b80c016424f986d320cab331dca
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-09-07 11:43:42 +0100

    The 514 patch

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -4,6 +4,6 @@ export SPACEMANDMM_TAG=suite-1.6
 # For TGUI
 export NODE_VERSION=12
 # Byond Major
-export BYOND_MAJOR=513
+export BYOND_MAJOR=514
 # Byond Minor
-export BYOND_MINOR=1528
+export BYOND_MINOR=1556
