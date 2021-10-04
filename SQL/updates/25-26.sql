78d0c532df5c2d4945230ad11c16bcf73293f2eb
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-10-04 19:21:44 +0100

    Better schema

--- SQL/updates/25-26.sql
+++ SQL/updates/25-26.sql
@@ -3,11 +3,11 @@
 
 CREATE TABLE `pai_saves` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`ckey` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`pai_name` LONGTEXT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`description` LONGTEXT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`preferred_role` LONGTEXT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`ooc_comments` LONGTEXT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`ckey` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`pai_name` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`description` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`preferred_role` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`ooc_comments` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`id`) USING BTREE,
 	UNIQUE INDEX `ckey` (`ckey`) USING BTREE
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
