f1fdaef0cc43c63e3e8250c84c83855b184ddd58
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-10-21 13:32:22 +0100

    Cleanup from multi instance PR

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -625,7 +625,7 @@ CREATE TABLE `pai_saves` (
 CREATE TABLE `instance_data_cache` (
 	`server_id` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`key_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`key_value` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`key_value` VARCHAR(12345) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`last_updated` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
 	PRIMARY KEY (`server_id`, `key_name`) USING HASH
 ) COLLATE='utf8mb4_general_ci' ENGINE=MEMORY;
