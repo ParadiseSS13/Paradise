ed78f3f7000683dc82005265c08c5d4621eba23c
Aziz Chynaliev <azizonkg@gmail.com>
2022-01-25 02:54:49 +0300

    Update _build_dependencies.sh

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -2,10 +2,10 @@
 # For dreamchecker
 export SPACEMANDMM_TAG=suite-1.6
 # For TGUI
-export NODE_VERSION=12
+export NODE_VERSION=16
 # Byond Major
-export BYOND_MAJOR=513
+export BYOND_MAJOR=514
 # Byond Minor
-export BYOND_MINOR=1528
+export BYOND_MINOR=1575
 # For the RUSTG library. Not actually installed by CI but kept as a reference
-export RUSTG_VERSION=2.1-P
+export RUSTG_VERSION=2.1.1
