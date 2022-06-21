9cdb44d27264a24dfa63ae913f1458d0e3b92058
S34N <12197162+S34NW@users.noreply.github.com>
2022-06-21 23:39:38 +0100

    Apply suggestions from code review

--- tools/hooks/python.sh
+++ tools/hooks/python.sh
@@ -2,7 +2,7 @@
 set -e
 if [ "$*" = "-m precommit" ]; then
 	echo "Hooks are being updated..."
-	echo "Details: https://github.com/tgstation/tgstation/pull/55658"
+	echo "Details: https://github.com/ParadiseSS13/Paradise/pull/18034 and https://github.com/tgstation/tgstation/pull/55658"
 	if [ "$(uname -o)" = "Msys" ]; then
 		tools/hooks/Install.bat
 	else
@@ -12,6 +12,6 @@ if [ "$*" = "-m precommit" ]; then
 	exec tools/hooks/pre-commit.hook
 else
 	echo "tools/hooks/python.sh is replaced by tools/bootstrap/python"
-	echo "Details: https://github.com/tgstation/tgstation/pull/55658"
+	echo "Details: https://github.com/ParadiseSS13/Paradise/pull/18034 and https://github.com/tgstation/tgstation/pull/55658"
 	exit 1
 fi
