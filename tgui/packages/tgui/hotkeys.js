b9cc4bf729d434a0c91b3984d3515acde6a69fac
hal9000PR <charliesteeples@outlook.com>
2022-07-24 14:30:52 +0100

    TGUI hotkey passthrough listener

--- tgui/packages/tgui/hotkeys.js
+++ tgui/packages/tgui/hotkeys.js
@@ -100,6 +100,34 @@ const getKeyData = (e) => {
   };
 };
 
+const keyCodeToByond = keyCode => {
+  if (keyCode === 16) {return 'Shift';}
+  if (keyCode === 17) {return 'Ctrl';}
+  if (keyCode === 18) {return 'Alt';}
+  if (keyCode === 33) {return 'Northeast';}
+  if (keyCode === 34) {return 'Southeast';}
+  if (keyCode === 35) {return 'Southwest';}
+  if (keyCode === 36) {return 'Northwest';}
+  if (keyCode === 37) {return 'West';}
+  if (keyCode === 38) {return 'North';}
+  if (keyCode === 39) {return 'East';}
+  if (keyCode === 40) {return 'South';}
+  if (keyCode === 45) {return 'Insert';}
+  if (keyCode === 46) {return 'Delete';}
+  if (keyCode >= 48 && keyCode <= 57 || keyCode >= 65 && keyCode <= 90) {
+    return String.fromCharCode(keyCode);
+  }
+  if (keyCode >= 96 && keyCode <= 105) {
+    return 'Numpad' + (keyCode - 96);
+  }
+  if (keyCode >= 112 && keyCode <= 123) {
+    return 'F' + (keyCode - 111);
+  }
+  if (keyCode === 188) {return ',';}
+  if (keyCode === 189) {return '-';}
+  if (keyCode === 190) {return '.';}
+};
+
 /**
  * Keyboard passthrough logic. This allows you to keep doing things
  * in game while the browser window is focused.
@@ -114,6 +142,7 @@ const handlePassthrough = (e, eventType) => {
   }
   const keyData = getKeyData(e);
   const { keyCode, ctrlKey, shiftKey } = keyData;
+  const byondKey = keyCodeToByond(keyCode);
   // NOTE: We pass through only Alt of all modifier keys, because Alt
   // modifier (for toggling run/walk) is implemented very shittily
   // in our codebase. We pass no other modifier keys, because they can
@@ -124,11 +153,11 @@ const handlePassthrough = (e, eventType) => {
   // Send this keypress to BYOND
   if (eventType === 'keydown' && !keyState[keyCode]) {
     logger.debug('passthrough', eventType, keyData);
-    return callByond('', { __keydown: keyCode });
+    return callByond('', { __keydown: byondKey });
   }
   if (eventType === 'keyup' && keyState[keyCode]) {
     logger.debug('passthrough', eventType, keyData);
-    return callByond('', { __keyup: keyCode });
+    return callByond('', { __keyup: byondKey });
   }
 };
 
