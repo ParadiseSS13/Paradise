63f737b88ee533cc42551b686ff87b6669681443
Dimach <dimach2142@gmail.com>
2022-10-22 23:25:07 +0300

    Fix: uplink pref migration and schema

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -75,7 +75,7 @@ CREATE TABLE `characters` (
   `body_accessory` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `gear` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `autohiss` tinyint(1) NOT NULL,
-  `uplink_pref` longtext COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pda',
+  `uplink_pref` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `ckey` (`ckey`)
 ) ENGINE=InnoDB AUTO_INCREMENT=125467 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- SQL/updates/20-21.sql
+++ SQL/updates/20-21.sql
@@ -2,4 +2,4 @@
 # Add new column uplink_pref (longtext) to table characters
 
 # Add column to characters
-ALTER TABLE `characters` ADD COLUMN `uplink_pref` longtext COLLATE utf8mb4_unicode_ci DEFAULT 'pda' AFTER `autohiss`;
+ALTER TABLE `characters` ADD COLUMN `uplink_pref` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER `autohiss`;
