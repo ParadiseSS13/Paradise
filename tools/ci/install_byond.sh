3d1c112448ea67b943d04a5dd1e37fa6bcb41733
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2022-11-01 21:33:07 +0000

    Attempt to multibuild on CI

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -3,7 +3,15 @@ set -euo pipefail
 
 source _build_dependencies.sh
 
-if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
+TARGET_MAJOR = $STABLE_BYOND_MAJOR
+TARGET_MINOR = $STABLE_BYOND_MINOR
+
+if [ $1 == "BETA" ]; then
+	TARGET_MAJOR = $BETA_BYOND_MAJOR
+	TARGET_MINOR = $BETA_BYOND_MINOR
+fi
+
+if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${TARGET_MAJOR}.${TARGET_MINOR}" $HOME/BYOND/version.txt;
 then
   echo "Using cached directory."
 else
@@ -11,11 +19,11 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl "http://www.byond.com/download/build/${TARGET_MAJOR}/${TARGET_MINOR}.${TARGET_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
   make here
-  echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
+  echo "$TARGET_MAJOR.$TARGET_MINOR" > "$HOME/BYOND/version.txt"
   cd ~/
 fi
