a1529e145b05eeca528b2d98377c1c4689820296
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2022-11-01 21:39:42 +0000

    AAAAAAAAAAAAAAAAAAAAAA

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -6,9 +6,13 @@ source _build_dependencies.sh
 TARGET_MAJOR=$STABLE_BYOND_MAJOR
 TARGET_MINOR=$STABLE_BYOND_MINOR
 
-if [ $1 == "BETA" ]; then
-	TARGET_MAJOR=$BETA_BYOND_MAJOR
-	TARGET_MINOR=$BETA_BYOND_MINOR
+if [ -z "$1" ]; then
+	echo "No arg specified. Assuming STABLE."
+else
+	if [ "$1" == "BETA" ]; then
+		TARGET_MAJOR=$BETA_BYOND_MAJOR
+		TARGET_MINOR=$BETA_BYOND_MINOR
+	fi
 fi
 
 if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${TARGET_MAJOR}.${TARGET_MINOR}" $HOME/BYOND/version.txt;
