6c424468115306d2e2a85cf13663c7507885382c
Nukechickenu64 <92064523+Nukechickenu64@users.noreply.github.com>
2022-11-05 02:20:35 -0400

    fuck you paradise this my shit now idc if its cobblerigged to

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Run the server and the unit tests
-DreamDaemon paradise.dmb -close -trusted -verbose
+DreamDaemon abbadon.dmb -close -trusted -verbose
 
 # Check if the unit tests actually suceeded
 cat data/clean_run.lk
