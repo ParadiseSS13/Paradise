b991fb62970c534951392e60d10930ca0e515ded
Bizzonium <azizonkg@gmail.com>
2022-12-13 20:55:18 +0600

    Fix: default tts_seed to null

--- SQL/updates/25-26.sql
+++ SQL/updates/25-26.sql
@@ -1,2 +1,2 @@
 # Add TTS voice seed preference
-ALTER TABLE `characters` ADD `tts_seed` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT 'Xenia' AFTER `uplink_pref`;
+ALTER TABLE `characters` ADD `tts_seed` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER `uplink_pref`;
