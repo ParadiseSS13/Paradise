1de0721d901e27d677c8390f733fb1b6d460daf8
Henri215 <77684085+Henri215@users.noreply.github.com>
2023-01-16 22:48:36 -0300

    Removes coin mechanic from vending machines, premium items cost money now (#20107)
    * Moving premium items out of premium
    
    * removing coin code from vendors
    
    * tgui rebuild
    
    * sirryan review
    
    * sirryan review part 2
    
    * tgui rebuild
    
    * rebuild, for real this time

--- tgui/packages/tgui/interfaces/Vending.js
+++ tgui/packages/tgui/interfaces/Vending.js
@@ -12,16 +12,12 @@ const VendingRow = (props, context) => {
     usermoney,
     inserted_cash,
     vend_ready,
-    coin_name,
     inserted_item_name,
   } = data;
   const free = !chargesMoney || product.price === 0;
   let buttonText = 'ERROR!';
   let rowIcon = '';
-  if (product.req_coin) {
-    buttonText = 'COIN';
-    rowIcon = 'circle';
-  } else if (free) {
+  if (free) {
     buttonText = 'FREE';
     rowIcon = 'arrow-circle-down';
   } else {
@@ -30,7 +26,6 @@ const VendingRow = (props, context) => {
   }
   let buttonDisabled =
     !vend_ready ||
-    (!coin_name && product.req_coin) ||
     productStock === 0 ||
     (!free && (product.price > usermoney && product.price > inserted_cash));
   return (
@@ -84,11 +79,9 @@ export const Vending = (props, context) => {
     inserted_cash,
     chargesMoney,
     product_records = [],
-    coin_records = [],
     hidden_records = [],
     stock,
     vend_ready,
-    coin_name,
     inserted_item_name,
     panel_open,
     speaker,
@@ -96,7 +89,6 @@ export const Vending = (props, context) => {
   } = data;
   let inventory;
 
-  inventory = [...product_records, ...coin_records];
   if (data.extended_inventory) {
     inventory = [...inventory, ...hidden_records];
   }
@@ -124,21 +116,6 @@ export const Vending = (props, context) => {
                 />
               </Box>
             ))}
-          </Section>
-        )}
-        {!!coin_name && (
-          <Section
-            title="Coin"
-            buttons={
-              <Button
-                fluid
-                icon="eject"
-                content="Remove Coin"
-                onClick={() => act('remove_coin', {})}
-              />
-            }
-          >
-            <Box>{coin_name}</Box>
           </Section>
         )}
         {!!inserted_item_name && (
