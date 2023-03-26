ParadiseSS13/Paradise

commit f28f14d90b1044b676ccaf0e261fdb42205f07b3

Distinguish between plating turf layer and floor turf layer. (#20503)
* move plating layer

* atmos correctness pass

* transparent floor ordering and wire terminal stuff

* fix control flow spacing

code/__DEFINES/layers.dm
@@ -13,9 +13,18 @@
 
 #define AREA_PLANE 1
 
-#define SPACE_LAYER 1.8
-#define GRASS_UNDER_LAYER 1.9
+#define SPACE_LAYER 1.5
+#define GRASS_UNDER_LAYER 1.6
+#define PLATING_LAYER 1.7
+#define LATTICE_LAYER 1.701
+#define DISPOSAL_PIPE_LAYER 1.71
+#define GAS_PIPE_HIDDEN_LAYER 1.72
+#define WIRE_LAYER 1.73
+#define WIRE_TERMINAL_LAYER 1.75
+#define ABOVE_PLATING_LAYER 1.76 // generic for /obj/hide
 //#define TURF_LAYER 2 //For easy recordkeeping; this is a byond define
+#define TRANSPARENT_TURF_LAYER 2
+#define ABOVE_TRANSPARENT_TURF_LAYER 2.01 // put wire terminals here if T.transparent_floor
 #define MID_TURF_LAYER 2.02
 #define HIGH_TURF_LAYER 2.03
 #define TURF_PLATING_DECAL_LAYER 2.031
@@ -25,16 +34,12 @@
 #define BULLET_HOLE_LAYER 2.06
 #define ABOVE_NORMAL_TURF_LAYER 2.08
 #define ABOVE_ICYOVERLAY_LAYER 2.11
-#define LATTICE_LAYER 2.2
-#define DISPOSAL_PIPE_LAYER 2.3
-#define GAS_PIPE_HIDDEN_LAYER 2.35
-#define WIRE_LAYER 2.4
-#define TRANSPARENT_TURF_LAYER 2.41
-#define WIRE_TERMINAL_LAYER 2.45
-#define GAS_SCRUBBER_LAYER 2.46
+#define GAS_SCRUBBER_OFFSET -0.001
 #define GAS_PIPE_VISIBLE_LAYER 2.47
-#define GAS_FILTER_LAYER 2.48
-#define GAS_PUMP_LAYER 2.49
+#define GAS_PIPE_SCRUB_OFFSET 0.001
+#define GAS_PIPE_SUPPLY_OFFSET 0.002
+#define GAS_FILTER_OFFSET 0.003
+#define GAS_PUMP_OFFSET 0.004
 #define HOLOPAD_LAYER 2.491
 #define CONVEYOR_LAYER 2.495
 #define LOW_OBJ_LAYER 2.5

code/game/objects/structures/safe.dm
@@ -373,7 +373,8 @@ GLOBAL_LIST_EMPTY(safes)
 	icon_state = "floorsafe"
 	density = FALSE
 	level = 1 //Under the floor
-	layer = LOW_OBJ_LAYER
+	plane = FLOOR_PLANE
+	layer = ABOVE_PLATING_LAYER
 	drill_x_offset = -1
 	drill_y_offset = 20
 

code/game/turfs/simulated/floor.dm
@@ -139,19 +139,23 @@ GLOBAL_LIST_INIT(icons_to_ignore_at_floor_init, list("damaged1","damaged2","dama
 	var/old_icon = icon_regular_floor
 	var/old_plating = icon_plating
 	var/old_dir = dir
+	var/old_transparent_floor = transparent_floor
 
 	var/turf/simulated/floor/W = ..()
 
 	var/obj/machinery/atmospherics/R
+	var/obj/machinery/power/terminal/term
 
 	if(keep_icon)
 		W.icon_regular_floor = old_icon
 		W.icon_plating = old_plating
 	if(W.keep_dir)
 		W.dir = old_dir
-	if(W.transparent_floor)
+	if(W.transparent_floor != old_transparent_floor)
 		for(R in W)
 			R.update_icon()
+		for(term in W)
+			term.update_icon()
 	for(R in W)
 		R.update_underlays()
 	W.update_icon()

code/game/turfs/simulated/floor/plating.dm
@@ -12,6 +12,7 @@
 	clawfootstep = FOOTSTEP_HARD_CLAW
 	heavyfootstep = FOOTSTEP_GENERIC_HEAVY
 	smoothing_groups = list(SMOOTH_GROUP_TURF)
+	layer = PLATING_LAYER
 
 /turf/simulated/floor/plating/Initialize(mapload)
 	. = ..()

code/modules/atmospherics/machinery/atmospherics.dm
@@ -10,7 +10,6 @@ Pipelines + Other Objects -> Pipe network
 */
 /obj/machinery/atmospherics
 	anchored = TRUE
-	layer = GAS_PIPE_HIDDEN_LAYER  //under wires
 	resistance_flags = FIRE_PROOF
 	max_integrity = 200
 	plane = GAME_PLANE
@@ -18,6 +17,9 @@ Pipelines + Other Objects -> Pipe network
 	power_channel = PW_CHANNEL_ENVIRONMENT
 	on_blueprints = TRUE
 	armor = list(MELEE = 25, BULLET = 10, LASER = 10, ENERGY = 100, BOMB = 0, BIO = 100, RAD = 100, FIRE = 100, ACID = 70)
+
+	layer = GAS_PIPE_HIDDEN_LAYER  //under wires
+	var/layer_offset = 0.0 // generic over VISIBLE and HIDDEN, should be less than 0.01, or you'll reorder non-pipe things
 
 	/// Can this be unwrenched?
 	var/can_unwrench = FALSE
@@ -84,14 +86,13 @@ Pipelines + Other Objects -> Pipe network
 		..(UPDATE_ICON_STATE)
 
 /obj/machinery/atmospherics/update_icon_state()
-	var/turf/T = get_turf(loc)
-	if(T && T.transparent_floor)
-		plane = FLOOR_PLANE
-	else
-		if(!T || level == 2 || !T.intact)
-			plane = GAME_PLANE
-		else
+	switch(level)
+		if(1)
 			plane = FLOOR_PLANE
+			layer = GAS_PIPE_HIDDEN_LAYER + layer_offset
+		if(2)
+			plane = GAME_PLANE
+			layer = GAS_PIPE_VISIBLE_LAYER + layer_offset
 
 /obj/machinery/atmospherics/proc/update_pipe_image()
 	pipe_image = image(src, loc, layer = ABOVE_HUD_LAYER, dir = dir) //the 20 puts it above Byond's darkness (not its opacity view)

code/modules/atmospherics/machinery/components/binary_devices/binary_atmos_base.dm
@@ -3,7 +3,8 @@
 	initialize_directions = SOUTH|NORTH
 	power_state = IDLE_POWER_USE
 
-	layer = GAS_PUMP_LAYER
+	layer = GAS_PIPE_VISIBLE_LAYER + GAS_PUMP_OFFSET
+	layer_offset = GAS_PUMP_OFFSET
 
 	var/datum/gas_mixture/air1
 	var/datum/gas_mixture/air2

code/modules/atmospherics/machinery/components/trinary_devices/trinary_base.dm
@@ -3,7 +3,8 @@
 	initialize_directions = SOUTH|NORTH|WEST
 	power_state = IDLE_POWER_USE
 
-	layer = GAS_FILTER_LAYER
+	layer = GAS_PIPE_VISIBLE_LAYER + GAS_FILTER_OFFSET
+	layer_offset = GAS_FILTER_OFFSET
 
 	var/datum/gas_mixture/air1
 	var/datum/gas_mixture/air2

code/modules/atmospherics/machinery/components/unary_devices/outlet_injector.dm
@@ -4,7 +4,8 @@ GLOBAL_LIST_EMPTY(air_injectors)
 	icon = 'icons/atmos/injector.dmi'
 	icon_state = "map_injector"
 	power_state = IDLE_POWER_USE
-	layer = GAS_SCRUBBER_LAYER
+	layer = GAS_PIPE_VISIBLE_LAYER + GAS_SCRUBBER_OFFSET
+	layer_offset = GAS_SCRUBBER_OFFSET
 
 	resistance_flags = FIRE_PROOF | UNACIDABLE | ACID_PROOF //really helpful in building gas chambers for xenomorphs
 

commit 59c723817026eec1fe923b9527c578559c057bef

Fixes layer issues caused by  #20503

code/__DEFINES/layers.dm
@@ -16,12 +16,6 @@
 #define SPACE_LAYER 1.5
 #define GRASS_UNDER_LAYER 1.6
 #define PLATING_LAYER 1.7
-#define LATTICE_LAYER 1.701
-#define DISPOSAL_PIPE_LAYER 1.71
-#define GAS_PIPE_HIDDEN_LAYER 1.72
-#define WIRE_LAYER 1.73
-#define WIRE_TERMINAL_LAYER 1.75
-#define ABOVE_PLATING_LAYER 1.76 // generic for /obj/hide
 //#define TURF_LAYER 2 //For easy recordkeeping; this is a byond define
 #define TRANSPARENT_TURF_LAYER 2
 #define ABOVE_TRANSPARENT_TURF_LAYER 2.01 // put wire terminals here if T.transparent_floor
@@ -34,8 +28,14 @@
 #define BULLET_HOLE_LAYER 2.06
 #define ABOVE_NORMAL_TURF_LAYER 2.08
 #define ABOVE_ICYOVERLAY_LAYER 2.11
-#define GAS_SCRUBBER_OFFSET -0.001
+#define LATTICE_LAYER 2.2
+#define ABOVE_PLATING_LAYER 2.25 // generic for /obj/hide
+#define DISPOSAL_PIPE_LAYER 2.3
+#define GAS_PIPE_HIDDEN_LAYER 2.35
+#define WIRE_LAYER 2.4
+#define WIRE_TERMINAL_LAYER 2.45
 #define GAS_PIPE_VISIBLE_LAYER 2.47
+#define GAS_SCRUBBER_OFFSET -0.001
 #define GAS_PIPE_SCRUB_OFFSET 0.001
 #define GAS_PIPE_SUPPLY_OFFSET 0.002
 #define GAS_FILTER_OFFSET 0.003