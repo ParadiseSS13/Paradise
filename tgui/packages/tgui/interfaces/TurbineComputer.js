a28922997d3a693fe4e8bb7ce63e242ded01e0fe
Burzah <mbsx@proton.me>
2023-06-06 21:37:07 -0700

    progress update, reconnect needs to be fixed

--- tgui/packages/tgui/interfaces/TurbineComputer.js
+++ tgui/packages/tgui/interfaces/TurbineComputer.js
@@ -32,34 +32,63 @@ export const TurbineComputer = (props, context) => {
               />
             </>
           )}>
-          {!operational && (
-            <LabeledList>
-              <LabeledList.Item
-                label="Compressor Status"
-                color={(!compressor || compressor_broken) ? 'bad' : 'good'}>
-                {compressor_broken ? compressor ? 'Offline' : 'Missing' : 'Online'}
-              </LabeledList.Item>
-              <LabeledList.Item
-                label="Turbine Status"
-                color={(!turbine || turbine_broken) ? 'bad' : 'good'}>
-                {turbine_broken ? turbine ? 'Offline' : 'Missing' : 'Online'}
-              </LabeledList.Item>
-            </LabeledList>
-          ) || (
-            <LabeledList>
-              <LabeledList.Item label="Turbine Speed">
-                {data.rpm} RPM
-              </LabeledList.Item>
-              <LabeledList.Item label="Internal Temp">
-                {data.temperature} K
-              </LabeledList.Item>
-              <LabeledList.Item label="Generated Power">
-                {data.power} W
-              </LabeledList.Item>
-            </LabeledList>
-          )}
+          {!operational ? <TurbineBroken /> : <TurbineWorking />}
         </Section>
       </Window.Content>
     </Window>
+  );
+};
+
+// Element Tree for if the turbine is broken
+const TurbineBroken = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    compressor,
+    compressor_broken,
+    turbine,
+    turbine_broken,
+    online
+  } = data;
+  return (
+    <LabeledList>
+      <LabeledList.Item
+        label="Compressor Status"
+        color={(!compressor || compressor_broken) ? 'bad' : 'good'}>
+        {compressor_broken ? compressor ? 'Offline' : 'Missing' : 'Online'}
+      </LabeledList.Item>
+      <LabeledList.Item
+        label="Turbine Status"
+        color={(!turbine || turbine_broken) ? 'bad' : 'good'}>
+        {turbine_broken ? turbine ? 'Offline' : 'Missing' : 'Online'}
+      </LabeledList.Item>
+    </LabeledList>
+  );
+};
+
+// Element Tree for if the turbine is working
+const TurbineWorking = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    compressor,
+    compressor_broken,
+    turbine,
+    turbine_broken,
+    online,
+    rpm,
+    temperature,
+    power
+  } = data;
+  return (
+    <LabeledList>
+      <LabeledList.Item label="Turbine Speed">
+        {rpm} RPM
+      </LabeledList.Item>
+      <LabeledList.Item label="Internal Temp">
+        {temperature} K
+      </LabeledList.Item>
+      <LabeledList.Item label="Generated Power">
+        {power} W
+      </LabeledList.Item>
+    </LabeledList>
   );
 };
