965f408770c4206b4216a2ce035197127073e1a0
Furior <68264134+Furrior@users.noreply.github.com>
2023-07-24 10:53:46 +0700

    Feat: whitelist database (#61)
    <!-- Пишите **НИЖЕ** заголовков и **ВЫШЕ** комментариев, иначе что то
    может пойти не так. -->
    <!-- Вы можете прочитать Contributing.MD, если хотите узнать больше. -->
    
    ## Что этот PR делает
    
    <!-- Вкратце опишите изменения, которые вносите. -->
    <!-- Опишите **все** изменения, так как противное может сказаться на
    рассмотрении этого PR'а! -->
    <!-- Если вы исправляете Issue, добавьте "Fixes #1234" (где 1234 - номер
    Issue) где-нибудь в описании PR'а. Это автоматически закроет Issue после
    принятия PR'а. -->
    
    ## Почему это хорошо для игры
    
    <!-- Опишите, почему, по вашему, следует добавить эти изменения в игру.
    -->
    
    ## Изображения изменений
    <!-- Если вы не меняли карту или спрайты, можете опустить эту секцию.
    Если хотите, можете вставить видео. -->
    
    ## Тестирование
    <!-- Как вы тестировали свой PR, если делали это вовсе? -->
    
    ## Changelog
    
    :cl:
    add: Улучшение вайтлиста. Автоматизация вписки.
    fix: Несколько эмоутов работали некоректно
    /:cl:
    
    <!-- Оба :cl:'а должны быть на месте, что-бы чейнджлог работал! Вы
    можете написать свой ник справа от первого :cl:, если хотите. Иначе
    будет использован ваш ник на ГитХабе. -->
    <!-- Вы можете использовать несколько записей с одинаковым префиксом
    (Они используются только для иконки в игре) и удалить ненужные. Помните,
    что чейнджлог должен быть понятен обычным игроком. -->
    <!-- Если чейнджлог не влияет на игроков(например, это рефактор), вы
    можете исключить всю секцию. -->

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -607,3 +607,21 @@ CREATE TABLE `tickets` (
 	CONSTRAINT `all_responses` CHECK (json_valid(`all_responses`)),
 	CONSTRAINT `awho` CHECK (json_valid(`awho`))
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
+
+--
+-- Table structure for table `ckey_whitelist`
+--
+
+DROP TABLE IF EXISTS `ckey_whitelist`;
+CREATE TABLE `ckey_whitelist`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`date` DATETIME DEFAULT now() NOT NULL,
+	`ckey` VARCHAR(32) NOT NULL,
+	`adminwho` VARCHAR(32) NOT NULL,
+	`port` INT(5) UNSIGNED NOT NULL,
+	`date_start` DATETIME DEFAULT now() NOT NULL,
+	`date_end` DATETIME NULL,
+	`is_valid` BOOLEAN DEFAULT true NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
