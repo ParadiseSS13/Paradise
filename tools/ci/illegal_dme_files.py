8e106ae7842f28bb9e2b53b1c732c2c16eaca1d3
Contrabang <91113370+Contrabang@users.noreply.github.com>
2023-07-25 13:22:09 -0700

    warriorstar review

--- tools/ci/illegal_dme_files.py
+++ tools/ci/illegal_dme_files.py
@@ -1,6 +1,10 @@
 # Naive search for illegal files. Has no semantic knowledge, just a lexical
 # search for specific endings in a .dme
 #
+# Illegal files are files that are allowed in the .dme by byond's standards,
+# but will cause actual issues on runtime. For example, dmm files are illegal
+# because they will mess with the map rotation system and z-levels in unexpected ways
+#
 # This is basically a slightly edited verison of unticked_files.py.
 # Look there for precise documentation on the methods used here.
 from pathlib import Path, PureWindowsPath
@@ -11,33 +15,36 @@ INCLUDED_FILES = [
     'paradise.dme'
 ]
 
-ILLEGAL_FILES = (
-    '.dmm'
+ILLEGAL_FILES = ( # Use a tuple here
+    '.dmm',
 )
 
 def get_illegal_files(root: Path):
-    illegal_files = set()
+    illegal_file_count = 0
     for includer in INCLUDED_FILES:
+        illegal_files = set()
         with open(root / includer, 'r') as f:
             # I've tried to optimize this, but this was the best I could get. Try placing the strips elsewhere if you dare
             lines = [line for line in f.readlines() if line.rstrip('\r\n').strip('"').endswith(ILLEGAL_FILES)]
             included = [line.replace('#include ', '').rstrip('\r\n').strip('"') for line in lines]
-            if(len(INCLUDED_FILES) > 1):
-                print(f'Found {len(included)} illegal files in {root / includer}')
             illegal_files.update([root / Path(includer).parent / Path(PureWindowsPath(i)) for i in included])
 
-    return illegal_files
+        if len(illegal_files) >= 1:
+            illegal_file_count += len(illegal_files)
+            print(f'Found {len(illegal_files)} illegal files in {root / includer}:')
+            print('\n'.join(str(x) for x in sorted(illegal_files)), '\n')
+
+    return illegal_file_count
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("root", help="paracode root directory")
+    parser.add_argument("root", help="project root directory")
     args = parser.parse_args()
 
     # Windows quoting behavior for directories adds trailing double-quote
     illegal_files = get_illegal_files(Path(args.root.strip('"')))
     if illegal_files:
-        print(f'Found {len(illegal_files)} illegal files:')
-        print('\n'.join(str(x) for x in sorted(illegal_files)))
+        print(f'Illegal files are not allowed to be included in dme files.')
         sys.exit(1)
     else:
         print(f'Found no .dme illegal files')
