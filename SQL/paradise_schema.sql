720ef620231e391713d7e1b33ebbfad0ab07bb8f
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2023-08-21 19:44:14 +0100

    [ADMIN] Allows saving of JSON datums server side (#21860)
    * Allows saving of JSON datums server side
    
    * derp
    
    * I am so livid I am webediting this
    
    * my god I have fallen off

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -620,6 +620,22 @@ CREATE TABLE `tickets` (
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
 
 --
+-- Table structure for table `json_datum_saves`
+--
+DROP TABLE IF EXISTS `json_datum_saves`;
+CREATE TABLE `json_datum_saves` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`ckey` VARCHAR(64) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`slotname` VARCHAR(32) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`slotjson` LONGTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`created` DATETIME NOT NULL DEFAULT current_timestamp(),
+	`updated` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `ckey_unique` (`ckey`, `slotname`) USING BTREE,
+	INDEX `ckey` (`ckey`) USING BTREE
+) COLLATE = 'utf8mb4_general_ci' ENGINE = InnoDB;
+
+--
 -- Table structure for table `ckey_whitelist`
 --
 
