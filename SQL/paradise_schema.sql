71882f16b0668f2c67ab31364814d8e8fd4bba69
Furior <68264134+Furrior@users.noreply.github.com>
2023-08-26 01:58:25 +0700

    Feat: wl admins (#145)
    <!-- Пишите **НИЖЕ** заголовков и **ВЫШЕ** комментариев, иначе что то
    может пойти не так. -->
    <!-- Вы можете прочитать Contributing.MD, если хотите узнать больше. -->
    
    ## Что этот PR делает
    
    <!-- Вкратце опишите изменения, которые вносите. -->
    <!-- Опишите **все** изменения, так как противное может сказаться на
    рассмотрении этого PR'а! -->
    <!-- Если вы исправляете Issue, добавьте "Fixes #1234" (где 1234 - номер
    Issue) где-нибудь в описании PR'а. Это автоматически закроет Issue после
    принятия PR'а. -->
    
    ## Почему это хорошо для игры
    
    <!-- Опишите, почему, по вашему, следует добавить эти изменения в игру.
    -->
    
    ## Изображения изменений
    <!-- Если вы не меняли карту или спрайты, можете опустить эту секцию.
    Если хотите, можете вставить видео. -->
    
    ## Тестирование
    <!-- Как вы тестировали свой PR, если делали это вовсе? -->
    
    ## Changelog
    
    :cl:
    tweak: Теперь на прайме свои админы
    /:cl:

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -625,3 +625,17 @@ CREATE TABLE `ckey_whitelist`
 	`is_valid` BOOLEAN DEFAULT true NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `admin_wl`
+--
+
+CREATE TABLE `admin_wl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_rank` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Administrator',
+  `level` int(2) NOT NULL DEFAULT '0',
+  `flags` int(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `ckey` (`ckey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
