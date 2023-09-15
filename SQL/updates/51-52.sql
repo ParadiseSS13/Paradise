a70dd9fb08f15bcb0965738aa59c5b3a7622e5a5
Octus <62493359+OctusGit@users.noreply.github.com>
2023-09-15 16:16:34 -0500

    Update SQL/updates/51-52.sql
    Co-authored-by: AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>

--- SQL/updates/51-52.sql
+++ SQL/updates/51-52.sql
@@ -1,5 +1,6 @@
 # Updating SQL from 51 to 52 -Octus
 # Add characther descriptors of height and build to preference menu
 
-ALTER TABLE `characters` CHANGE COLUMN `physique` `physique` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT `of average build`;
-ALTER TABLE `characters` CHANGE COLUMN `height` `height` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT `of average height`;
+ALTER TABLE `characters`
+	ADD COLUMN `physique` VARCHAR(45) NULL DEFAULT NULL AFTER `nanotrasen_relation`,
+	ADD COLUMN `height` VARCHAR(45) NULL DEFAULT NULL AFTER `physique`;
