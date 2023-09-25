0dd9fd0ba7f63438ba7b53e8bf50be1ade7d3718
migratingcocofruit <guycxz@gmail.com>
2023-09-25 17:31:19 +0300

    Identation and style changes

--- tgui/packages/tgui/interfaces/CompostBin.js
+++ tgui/packages/tgui/interfaces/CompostBin.js
@@ -22,76 +22,75 @@ export const CompostBin = (props, context) => {
 	
   return (
     <Window>
-      <Window.Content>
-	  <Section label = "Resources">
-      <Flex>
-        <LabeledList>
-        <LabeledList.Item label="Biomass">
-          <ProgressBar
-            value={biomass}
-            minValue={0}
-            maxValue={biomass_capacity}
-            ranges={{
-              good: [biomass_capacity * 0.5, Infinity],
-              average: [biomass_capacity * 0.25, biomass_capacity * 0.5],
-              bad: [-Infinity, biomass_capacity * 0.25],
-            }}
-          >
-            {biomass} / {biomass_capacity} Units
-          </ProgressBar>
-        </LabeledList.Item>	
-		</LabeledList>
-		</Flex>
-		<Flex>
-        <LabeledList>
-        <LabeledList.Item label="compost">
-          <ProgressBar
-            value={compost}
-            minValue={0}
-            maxValue={compost_capacity}
-            ranges={{
-              good: [compost_capacity * 0.5, Infinity],
-              average: [compost_capacity * 0.25, compost_capacity * 0.5],
-              bad: [-Infinity, compost_capacity * 0.25],
-            }}
-          >
-            {compost} / {compost_capacity} Units
-          </ProgressBar>
-        </LabeledList.Item>	
-		</LabeledList>
-		</Flex>
-		</Section>
-		<Section title = "Controls">
-			<Flex>
-				<Box inline mr="5px" color="silver">
-					Soil clumps to make:
-				</Box>
-				<NumberInput
-					animated
-					value={vendAmount}
-					width="32px"
-					minValue={1}
-					maxValue={10}
-					stepPixelSize={7}
-					onChange={(e, value) => setVendAmount(value)}
+		<Window.Content>
+			<Section label="Resources">
+				<Flex>
+					<LabeledList>
+						<LabeledList.Item label="Biomass">
+							<ProgressBar
+								value={biomass}
+								minValue={0}
+								maxValue={biomass_capacity}
+								ranges={{
+								good: [biomass_capacity * 0.5, Infinity],
+								average: [biomass_capacity * 0.25, biomass_capacity * 0.5],
+								bad: [-Infinity, biomass_capacity * 0.25],
+								}}
+							>
+							{biomass} / {biomass_capacity} Units
+								</ProgressBar>
+						</LabeledList.Item>	
+					</LabeledList>
+				</Flex>
+				<Flex>
+					<LabeledList>
+							<LabeledList.Item label="compost">
+								<ProgressBar
+								value={compost}
+								minValue={0}
+								maxValue={compost_capacity}
+								ranges={{
+								good: [compost_capacity * 0.5, Infinity],
+								average: [compost_capacity * 0.25, compost_capacity * 0.5],
+								bad: [-Infinity, compost_capacity * 0.25],
+								}}
+							>
+							{compost} / {compost_capacity} Units
+								</ProgressBar>
+						</LabeledList.Item>	
+					</LabeledList>
+				</Flex>
+			</Section>
+			<Section title = "Controls">
+				<Flex>
+					<Box inline mr="5px" color="silver">
+						Soil clumps to make:
+					</Box>
+					<NumberInput
+						animated
+						value={vendAmount}
+						width="32px"
+						minValue={1}
+						maxValue={10}
+						stepPixelSize={7}
+						onChange={(e, value) => setVendAmount(value)}
 					/>
-			</Flex>
-		    <Flex.Item mr="5px" textAlign="right" width="0%">
-              <Button
-				align = "right"
-                content="Make Soil"
-                disabled={compost < 25 * vendAmount}
-                icon="arrow-circle-down"
-                onClick={() =>
-                  act('create', {
-                    amount: vendAmount,
-                  })
-                }
-              />
-			  
-            </Flex.Item>
-		</Section>
-      </Window.Content>
-    </Window>
+				</Flex>
+				<Flex.Item mr="5px" textAlign="right" width="0%">
+					<Button
+						align = "right"
+						content="Make Soil"
+						disabled={compost < 25 * vendAmount}
+						icon="arrow-circle-down"
+						onClick={() =>
+						act('create', {
+						amount: vendAmount,
+						})
+						}
+					/>
+				</Flex.Item>
+			</Section>
+		</Window.Content>
+	</Window>
   );
 };
