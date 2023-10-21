3f941b469c2a59183105c90414b9f8946a55834f
Furior <68264134+Furrior@users.noreply.github.com>
2023-10-22 00:31:24 +0700

    Feat wip: translation tool (#515)
    <!-- Пишите **НИЖЕ** заголовков и **ВЫШЕ** комментариев, иначе что то
    может пойти не так. -->
    <!-- Вы можете прочитать Contributing.MD, если хотите узнать больше. -->
    
    ## Что этот PR делает
    Добавляет инструментарий для перевода
    <!-- Вкратце опишите изменения, которые вносите. -->
    <!-- Опишите **все** изменения, так как противное может сказаться на
    рассмотрении этого PR'а! -->
    <!-- Если вы исправляете Issue, добавьте "Fixes #1234" (где 1234 - номер
    Issue) где-нибудь в описании PR'а. Это автоматически закроет Issue после
    принятия PR'а. -->
    
    ## Почему это хорошо для игры
    
    <!-- Опишите, почему, по вашему, следует добавить эти изменения в игру.
    -->
    
    ## Изображения изменений
    <!-- Если вы не меняли карту или спрайты, можете опустить эту секцию.
    Если хотите, можете вставить видео. -->
    
    ## Тестирование
    <!-- Как вы тестировали свой PR, если делали это вовсе? -->
    
    ## Changelog
    
    :cl:
    experiment: :guitar:
    /:cl:
    
    <!-- Оба :cl:'а должны быть на месте, что-бы чейнджлог работал! Вы
    можете написать свой ник справа от первого :cl:, если хотите. Иначе
    будет использован ваш ник на ГитХабе. -->
    <!-- Вы можете использовать несколько записей с одинаковым префиксом
    (Они используются только для иконки в игре) и удалить ненужные. Помните,
    что чейнджлог должен быть понятен обычным игроком. -->
    <!-- Если чейнджлог не влияет на игроков(например, это рефактор), вы
    можете исключить всю секцию. -->

---
+++ tools/translator/converter.py
@@ -0,0 +1,40 @@
+import git
+import json
+BUILD_PATH = "./../../"
+
+
+repo = git.Repo(BUILD_PATH)
+tree = repo.head.commit.tree
+if not tree:
+    print("No changes")
+    exit()
+diff = repo.git.diff(tree)
+if not diff:
+    print("No changes")
+    exit()
+diff = [line for line in diff.split("\n") if line[0] in "+-" and not line.startswith("+++")]
+
+translation = {"files": []}
+
+lastfile = ''
+lastaction = ''
+for line in diff:
+    if line.startswith("---"):
+        lastfile = line[6:]
+        lastaction = '---'
+        translation["files"].append({"path": lastfile, "replaces": []})
+    elif line.startswith("-"):
+        if lastaction == "---" or lastaction == "+":
+            translation["files"][-1]["replaces"].append({"original": line[1:], "replace": ""})
+        elif lastaction == "-":
+            translation["files"][-1]["replaces"][-1]["original"]+=f"\n{line[1:]}"
+        lastaction = "-"
+    elif line.startswith("+"):
+        if lastaction == "-":
+            translation["files"][-1]["replaces"][-1]["replace"] = line[1:]
+        elif lastaction == "+":
+            translation["files"][-1]["replaces"][-1]["replace"] += f"\n{line[1:]}"
+        lastaction = "+"
+
+with open('ss220replace.json', 'w+', encoding='utf-8') as f:
+    json.dump(translation, f, ensure_ascii=False, indent=2)
