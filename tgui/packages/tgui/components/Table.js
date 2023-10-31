5aca21931b85b82952d0bcb7517d6f055a4028d7
Arthri <41360489+a@users.noreply.github.com>
2023-10-31 02:30:35 +0000

    Add sortable table

--- tgui/packages/tgui/components/Table.js
+++ tgui/packages/tgui/components/Table.js
@@ -1,5 +1,8 @@
 import { classes, pureComponentHooks } from 'common/react';
-import { Box, computeBoxClassName, computeBoxProps } from './Box';
+import { computeBoxClassName, computeBoxProps } from './Box';
+import { Component } from 'inferno'
+import { Button } from './Button'
+import { Icon } from './Icon'
 
 export const Table = (props) => {
   const { className, collapsing, children, ...rest } = props;
@@ -53,7 +56,132 @@ export const TableCell = (props) => {
   );
 };
 
+const resolveFunctionalProp = (props, ...data) =>
+  props
+  ? props instanceof Function
+    ? props(...data)
+    : props
+  : undefined
+  ;
+
+class SortableTable extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      sortId: props.sortId ?? props.columns[0].id,
+      sortOrder: props.sortOrder ?? 1,
+    };
+  }
+
+  render() {
+    const {
+      className,
+      columns,
+      data,
+      datumID,
+      filter,
+      headerRowProps,
+      headerCellProps,
+      datumRowProps,
+      datumCellProps,
+      datumCellChildren,
+      ...rest
+    } = this.props;
+    const {
+      sortId,
+      sortOrder,
+    } = this.state;
+
+    const columnHeaders = columns.map(({ id, name }) => {
+      return (
+        <Table.Cell key={id}>
+          <Button
+            color={sortId !== id && 'transparent'}
+            width="100%"
+            onClick={() => {
+              if (sortId === id) {
+                this.setState({
+                  sortOrder: !sortOrder,
+                });
+              } else {
+                this.setState({
+                  sortId: id,
+                  sortOrder: true,
+                });
+              }
+            }}
+            onDblClick={() =>
+              this.setState({
+                sortId: null,
+              })
+            }
+            {...headerCellProps?.all}
+            {...headerCellProps?.[id]}
+          >
+            {name}
+            {sortId === id && (
+              <Icon
+                name={sortOrder ? 'sort-up' : 'sort-down'}
+                position="absolute"
+                right={0}
+                top="50%"
+                style={{
+                  transform: "translate(0, -50%)"
+                }}
+              />
+            )}
+          </Button>
+        </Table.Cell>
+      );
+    });
+    const dataRows = (filter ? filter(data) : data)
+      .sort((a, b) => {
+        if (sortId) {
+          const i = sortOrder ? 1 : -1;
+          return a[sortId].localeCompare(b[sortId]) * i;
+        } else {
+          return 0;
+        }
+      })
+      .map((datum) => {
+        let cells = columns
+          .map(({ id, name }) => {
+            return (
+              <Table.Cell
+                key={id}
+                {...resolveFunctionalProp(datumCellProps?.all, datum[id]) ?? []}
+                {...resolveFunctionalProp(datumCellProps?.[id], datum[id]) ?? []}
+              >
+                {resolveFunctionalProp(datumCellChildren?.[id], datum[id]) ?? datum[id]}
+              </Table.Cell>
+            );
+          })
+          ;
+
+        return (
+          <Table.Row
+            key={datumID(datum)}
+            {...resolveFunctionalProp(datumRowProps, datum) ?? []}
+          >
+            {cells}
+          </Table.Row>
+        );
+      })
+      ;
+
+    return (
+      <Table className={classes([ 'SortableTable', className, computeBoxClassName(rest) ])} {...rest}>
+        <Table.Row bold {...headerRowProps}>
+          {columnHeaders}
+        </Table.Row>
+        {dataRows}
+      </Table>
+    );
+  }
+}
+
 TableCell.defaultHooks = pureComponentHooks;
 
 Table.Row = TableRow;
 Table.Cell = TableCell;
+Table.Sortable = SortableTable;
