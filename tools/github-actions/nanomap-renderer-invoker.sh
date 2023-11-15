5e142980e6fd8fe812dac779ef41cd7bfe8e6272
Aylong <69762909+Aylong220@users.noreply.github.com>
2023-11-16 00:19:44 +0200

    Fix for fix: NanoMap Render (#710)
    <!-- Пишите **НИЖЕ** заголовков и **ВЫШЕ** комментариев, иначе что то
    может пойти не так. -->
    <!-- Вы можете прочитать Contributing.MD, если хотите узнать больше. -->
    
    ## Что этот PR делает
    Теперь точно чинит рендер наномап

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -8,7 +8,7 @@ tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files
 cd "data/nanomaps"
 mv "cyberiad_nanomap_z1.png" "Cyberiad220_nanomap_z1.png"
 mv "delta_nanomap_z1.png" "Delta220_nanomap_z1.png"
-#mv "metastation_nanomap_z1.png" "MetaStation220_nanomap_z1.png" # Самая бесполезная строчка в истории
+mv "MetaStation_nanomap_z1.png" "MetaStation220_nanomap_z1.png"
 cd "../../"
 cp "data/nanomaps/Cyberiad220_nanomap_z1.png" "icons/_nanomaps"
 cp "data/nanomaps/Delta220_nanomap_z1.png" "icons/_nanomaps"
