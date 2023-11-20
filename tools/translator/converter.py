9099f8b2258084dec48aab931b3caa591d11225c
KoJIT2009 <kojit2009@gmail.com>
2023-11-21 00:39:58 +0500

    Update translate tools and add ci (#736)
    ## Что этот PR делает
    
    Переработан converter.py
    Добавлена ci проверка на перевод
    
    ## Changelog
    
    :cl:
    add: Добавлена ci проверка на перевод
    fix: Переработан converter.py
    /:cl:

--- tools/translator/converter.py
+++ tools/translator/converter.py
@@ -15,42 +15,71 @@ diff = repo.git.diff(tree)
 if not diff:
     print("No changes")
     exit()
+
+# Оставляем только стоки, где строки начинаются с "+", "-", "---"
 diff = [line for line in diff.split("\n") if line[0] in "+-" and not line.startswith("+++")]
 
-translation = {"files": []}
-
-lastfile = ''
-lastaction = ''
+# Собираем в структуру вида:
+# {
+#   "file": "player.dm",
+#   "origin": ["Test", "Test2"],
+#   "replace": ["Тест", "Тест2"]
+# }
+prepare = []
+lastFile = ''
 for line in diff:
     if line.startswith("---"):
-        lastfile = line[6:]
-        lastaction = '---'
-        translation["files"].append({"path": lastfile, "replaces": []})
+        lastFile = line[6:]
+        prepare.append({"file": lastFile, "origin": [], "replace": []})
     elif line.startswith("-"):
-        if lastaction == "---" or lastaction == "+":
-            translation["files"][-1]["replaces"].append({"original": line[1:], "replace": ""})
-        elif lastaction == "-":
-            translation["files"][-1]["replaces"][-1]["original"]+=f"\n{line[1:]}"
-        lastaction = "-"
+        prepare[-1]['origin'].append(line[1:])
     elif line.startswith("+"):
-        if lastaction == "-":
-            translation["files"][-1]["replaces"][-1]["replace"] = line[1:]
-        elif lastaction == "+":
-            translation["files"][-1]["replaces"][-1]["replace"] += f"\n{line[1:]}"
-        lastaction = "+"
+        prepare[-1]['replace'].append(line[1:])
 
-filteredTranslation = {"files": []}
-for file in translation['files']:
-    if not allowPathsRegexp.match(file['path']):
+# Фильтруем структуру: Оставляем только разрешенные файлы
+filtered = []
+for item in prepare:
+    if not allowPathsRegexp.match(item['file']):
         continue
-    filteredTranslation["files"].append(file)
+    filtered.append(item)
+
+# Собираем в структуру для хранения в файле:
+# {
+#   "files": [
+#       {
+#           "path": "player.dm",
+#           "replaces": [
+#               {"original": "Test", "replace": "Тест"},
+#               {"original": "Test2", "replace": "Тест2"}
+#           ]
+#       }
+#   ]
+# }
+jsonStructure = {"files": []}
+for item in filtered:
+    originLen = len(item["origin"])
+    replaceLen = len(item["replace"])
+
+    if originLen != replaceLen:
+        print("Changes not equals")
+        print(item)
+        exit(1)
+
+    file = {"path": item["file"], "replaces": []}
+
+    for i in range(originLen):
+        file["replaces"].append({"original": item["origin"][i], "replace": item["replace"][i]})
+
+    jsonStructure["files"].append(file)
 
 jsonFilePath = os.path.dirname(os.path.realpath(__file__)) + '/ss220replace.json'
 
+# Добавляем новые элементы к текущим в файле
 fullTranslation = json.load(open(jsonFilePath, encoding='utf-8'))
-for file in filteredTranslation['files']:
+for file in jsonStructure['files']:
     fullTranslation["files"].append(file)
 
 with open(jsonFilePath, 'w+', encoding='utf-8') as f:
     json.dump(fullTranslation, f, ensure_ascii=False, indent=2)
-print(f"Added translation for {len(filteredTranslation['files'])} files.")
+
+print(f"Added translation for {len(jsonStructure['files'])} files.")
