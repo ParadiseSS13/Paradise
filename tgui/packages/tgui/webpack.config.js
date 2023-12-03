922a8bded1db4eaff8cd0aefeaaacdd3418ea6ee
S34N <12197162+S34NW@users.noreply.github.com>
2023-12-03 20:49:23 +0000

    dep bumps

--- tgui/packages/tgui/webpack.config.js
+++ tgui/packages/tgui/webpack.config.js
@@ -68,7 +68,10 @@ module.exports = (env = {}, argv) => {
                   '@babel/plugin-transform-jscript',
                   'babel-plugin-inferno',
                   ['@babel/plugin-transform-private-methods', { loose: false }],
-                  ['@babel/plugin-proposal-class-properties', { loose: false }],
+                  [
+                    '@babel/plugin-transform-class-properties',
+                    { loose: false },
+                  ],
                   [
                     '@babel/plugin-transform-private-property-in-object',
                     { loose: false },
