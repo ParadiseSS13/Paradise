4769749af632204efe59cfc0266cf5f1caca5276
S34N <12197162+S34NW@users.noreply.github.com>
2023-12-06 23:55:09 +0000

    fixes GPS BSOD

--- tgui/packages/common/math.js
+++ tgui/packages/common/math.js
@@ -86,3 +86,10 @@ export const keyOfMatchingRange = (value, ranges) => {
     }
   }
 };
+
+/**
+ * Converts a value in radians to degrees.
+ */
+export const rad2deg = (rad) => {
+  return rad * (180 / Math.PI);
+};
