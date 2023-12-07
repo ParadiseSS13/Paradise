1220eca425400d7c1ed442b8848bac53707b7487
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2023-12-07 23:02:45 +0000

    Try this

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -14,4 +14,4 @@ export BETA_BYOND_MINOR=1619
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.6
 # RUSTG version
-export RUSTG_VERSION=v1.2.0-P2
+export RUSTG_VERSION=v3.0.0-P
