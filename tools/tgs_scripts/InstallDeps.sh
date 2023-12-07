d00c60aea48cdb06b419bba8dc7748b438c83ef2
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2023-12-07 18:40:13 +0000

    Split these out

--- tools/tgs_scripts/InstallDeps.sh
+++ tools/tgs_scripts/InstallDeps.sh
@@ -16,11 +16,13 @@ if ! ( [ -x "$has_git" ] && [ -x "$has_curl" ] && [ -f "/usr/lib/i386-linux-gnu/
 	if ! [ -x "$has_sudo" ]; then
 		dpkg --add-architecture i386
 		apt-get update
-		apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl gcc libdbus-1-dev gcc:i386
+		apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl gcc libdbus-1-dev
+		apt-get install -y gcc:i386
 	else
 		sudo dpkg --add-architecture i386
 		sudo apt-get update
-		sudo apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl gcc libdbus-1-dev gcc:i386
+		sudo apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl gcc libdbus-1-dev
+		sudo apt-get install -y gcc:i386
 	fi
 fi
 
