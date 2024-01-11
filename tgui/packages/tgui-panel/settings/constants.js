c1c1344878c2a151ad7455b81a94860e1f633b94
Aylong <alexanderkitsa@gmail.com>
2024-01-11 09:36:01 +0200

    Some little fixes and SpecMenu Stack

--- tgui/packages/tgui-panel/settings/constants.js
+++ tgui/packages/tgui-panel/settings/constants.js
@@ -24,7 +24,6 @@ export const FONTS_DISABLED = 'Default';
 
 export const FONTS = [
   FONTS_DISABLED,
-  'Verdana',
   'Arial',
   'Arial Black',
   'Comic Sans MS',
--- tgui/packages/tgui/interfaces/FloorPainter.js
+++ tgui/packages/tgui/interfaces/FloorPainter.js
@@ -56,7 +56,7 @@ export const FloorPainter = (props, context) => {
                 height="20px"
                 ml="2px"
                 mr="2px"
-                nochevron="true"
+                nochevron
                 onSelected={(val) => act('select_style', { style: val })}
               />
             </Flex.Item>
