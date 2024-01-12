0574013541050f62498cf794376ee86c1af82118
S34N <12197162+S34NW@users.noreply.github.com>
2024-01-12 20:28:41 +0000

    fix

--- tgui/packages/tgui-panel/reconnect.js
+++ tgui/packages/tgui-panel/reconnect.js
@@ -1,44 +1,30 @@
 import { Button, Flex } from 'tgui/components';
 
-let url = null;
-
-setInterval(() => {
-  Byond.winget('', 'url').then((currentUrl) => {
-    // Sometimes, for whatever reason, BYOND will give an IP with a :0 port.
-    if (currentUrl && !currentUrl.match(/:0$/)) {
-      url = currentUrl;
-    }
-  });
-}, 0);
-
 export const ReconnectButton = (props, context) => {
   return (
-    url && (
-      <Flex>
-        <Flex.Item mr="3px">
-          <Button
-            fluid
-            icon="history"
-            color="white"
-            content="Reconnect"
-            onClick={() => {
-              Byond.command('.reconnect');
-            }}
-          />
-        </Flex.Item>
-        <Flex.Item>
-          <Button
-            icon="power-off"
-            color="white"
-            tooltip="Restart game"
-            tooltipPosition="top-end"
-            onClick={() => {
-              location.href = `byond://${url}`;
-              Byond.command('.quit');
-            }}
-          />
-        </Flex.Item>
-      </Flex>
-    )
+    <Flex>
+      <Flex.Item mr="3px">
+        <Button
+          fluid
+          icon="history"
+          color="white"
+          content="Reconnect"
+          onClick={() => {
+            Byond.command('.reconnect');
+          }}
+        />
+      </Flex.Item>
+      <Flex.Item>
+        <Button
+          icon="power-off"
+          color="white"
+          tooltip="Restart game"
+          tooltipPosition="top-end"
+          onClick={() => {
+            Byond.command('.quit');
+          }}
+        />
+      </Flex.Item>
+    </Flex>
   );
 };
