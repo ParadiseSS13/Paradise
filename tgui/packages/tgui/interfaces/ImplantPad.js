79aff0594c8cad93aa939bbe04a0deb57d2bca6e
Arthri <41360489+Arthri@users.noreply.github.com>
2024-01-14 23:07:41 +0800

    Reenable no-undef rule. Enable Format On Save for VSCode (#23803)
    * Enable Format On Save for VSCode
    
    * Re-enable and fix no-undef
    
    ---------
    
    Co-authored-by: Arthri <41360489+a@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/ImplantPad.js
+++ tgui/packages/tgui/interfaces/ImplantPad.js
@@ -1,5 +1,5 @@
 import { useBackend } from '../backend';
-import { Button, Section, Box } from '../components';
+import { Button, Section, Box, LabeledList } from '../components';
 import { Window } from '../layouts';
 
 export const ImplantPad = (props, context) => {
