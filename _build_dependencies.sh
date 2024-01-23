9375b0a9b18018430999da6eefdc6022fc16c941
S34N <12197162+S34NW@users.noreply.github.com>
2024-01-23 22:23:24 +0000

    webedit heresy

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -6,11 +6,11 @@ export NODE_VERSION=18
 # Stable Byond Major
 export STABLE_BYOND_MAJOR=515
 # Stable Byond Minor
-export STABLE_BYOND_MINOR=1627
+export STABLE_BYOND_MINOR=1628
 # Beta Byond Major
 export BETA_BYOND_MAJOR=515
 # Beta Byond Minor
-export BETA_BYOND_MINOR=1627
+export BETA_BYOND_MINOR=1628
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.6
 # RUSTG version
