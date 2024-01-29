e22e047952a37de16b184f94d5aea39d5755b0c7
BiancaWilkson <42818125+BiancaWilkson@users.noreply.github.com>
2024-01-28 19:20:12 -0500

    lmao i REALLY dont know sql, thanks AA
    Co-authored-by: AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>

--- SQL/updates/53-54.sql
+++ SQL/updates/53-54.sql
@@ -1,5 +1,5 @@
 #Updating SQL from 53 to 54 -Wilk
 #Add a choice for what type of brain borgs want to have
 
-ALTER TABLE 'characters'
-	ADD COLUMN 'cyborg_brain_type' VARCHAR(11) NOT NULL DEFAULT 'MMI' AFTER 'height';
+ALTER TABLE `characters`
+	ADD COLUMN `cyborg_brain_type` VARCHAR(11) NOT NULL DEFAULT 'MMI' AFTER `height`;
