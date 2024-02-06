443b6c0ee17c4347318cd249737204c4815a9343
Aylong <alexanderkitsa@gmail.com>
2024-02-06 09:13:27 +0200

    PrizeCounterUI 1.0
    Update PrizeCounter.scss
    
    Update prize_counter.dm
    
    UI work
    
    It just works

---
+++ tgui/packages/tgui/interfaces/PrizeCounter.tsx
@@ -0,0 +1,83 @@
+import { classes } from 'common/react';
+import { useBackend } from '../backend';
+import { Button, Section, Stack, Divider } from '../components';
+import { Window } from '../layouts';
+
+type Prize = {
+  name: string;
+  desc: string;
+  item: string;
+  path: string;
+  cost: number;
+};
+
+type PrizeData = {
+  tickets: number;
+  prizes: Prize[];
+};
+
+export const PrizeCounter = (props, context) => {
+  const { act, data } = useBackend<PrizeData>(context);
+  const { tickets, prizes = [] } = data;
+  return (
+    <Window width={450} height={585} title="Arcade Ticket Exchange">
+      <Window.Content>
+        <Stack fill vertical>
+          <Stack.Item grow>
+            <Section
+              fill
+              scrollable
+              title="Available Prizes"
+              buttons={
+                <Button
+                  fluid
+                  iconRight
+                  icon="ticket-alt"
+                  disabled={!tickets}
+                  content={<>Tickets: {<b>{tickets}</b>}</>}
+                  onClick={() => act('eject')}
+                />
+              }
+            >
+              {prizes.map((prizes) => (
+                <Stack key={prizes.name} className="PrizeCounter__Item">
+                  <Stack.Item
+                    width="96px"
+                    className={classes(['prize_counter64x64', prizes.path])}
+                  />
+                  <Stack.Item width="100%">
+                    <Stack vertical textAlign="center">
+                      <Stack.Item bold mt={1}>
+                        {prizes.name}
+                      </Stack.Item>
+                      <Divider />
+                      <Stack.Item mt={0} verticalAlign="middle">
+                        {prizes.desc}
+                      </Stack.Item>
+                    </Stack>
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Button
+                      className={classes([
+                        'PrizeCounter__BuyButton',
+                        prizes.cost > tickets &&
+                          'PrizeCounter__BuyButton--disabled',
+                      ])}
+                      icon="ticket-alt"
+                      content={prizes.cost}
+                      onClick={() =>
+                        act('purchase', {
+                          'purchase': prizes.item,
+                        })
+                      }
+                    />
+                  </Stack.Item>
+                </Stack>
+              ))}
+            </Section>
+          </Stack.Item>
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
