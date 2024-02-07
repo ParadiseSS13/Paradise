3d060dde643503140fcd494ad127b52e94b886cc
Contrabang <91113370+Contrabang@users.noreply.github.com>
2024-02-07 11:42:00 -0500

    the power of ci compells you

--- tools/ci/check_grep2.py
+++ tools/ci/check_grep2.py
@@ -12,6 +12,12 @@ GREEN = "\033[0;32m"
 BLUE = "\033[0;34m"
 NC = "\033[0m"  # No Color
 
+def print_error(message: str, filename: str, line_number: int):
+    if os.getenv("GITHUB_ACTIONS") == "true": # We're on github, output in a special format.
+        print(f"::error file={filename},line={line_number},title=Check Grep::{message}")
+    else:
+        print(f"{filename}:{line_number}: {RED}{message}{NC}")
+
 IGNORE_515_PROC_MARKER_FILENAME = "__byond_version_compat.dm"
 CHECK_515_PROC_MARKER_RE = re.compile(r"\.proc/")
 def check_515_proc_syntax(lines):
@@ -163,13 +169,13 @@ if __name__ == "__main__":
             if filename != IGNORE_515_PROC_MARKER_FILENAME:
                 if failure := check_515_proc_syntax(code):
                     exit_code = 1
-                    print(f"{code_filepath}:{failure.lineno}: {RED}{failure.message}{NC}")
+                    print_error(failure.message, code_filepath, failure.lineno)
 
             for check in CODE_CHECKS:
                 code.seek(0)
                 if failure := check(code):
                     exit_code = 1
-                    print(f"{code_filepath}:{failure.lineno}: {RED}{failure.message}{NC}")
+                    print_error(failure.message, code_filepath, failure.lineno)
 
     end = time.time()
     print(f"\ncheck_grep2 tests completed in {end - start:.2f}s\n")
