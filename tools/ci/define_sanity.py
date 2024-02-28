cf4967ffa7cd71b5e7e5dbf16bc75f3ca3b755fb
Contrabang <91113370+Contrabang@users.noreply.github.com>
2024-02-28 15:09:17 -0500

    proper programming

--- tools/ci/define_sanity.py
+++ tools/ci/define_sanity.py
@@ -26,72 +26,75 @@ def post_error(define_name, file, github_error_style, fix_error = False):
             with open(file, "a") as write_file:
                 write_file.write(f"#undef {define_name}\n") # We append at the \n because files end with \n
 
+def main():
 
+    # simple way to check if we're running on github actions, or on a local machine
+    on_github = os.getenv("GITHUB_ACTIONS") == "true"
 
-# simple way to check if we're running on github actions, or on a local machine
-on_github = os.getenv("GITHUB_ACTIONS") == "true"
+    # This files/directories are expected to have "global" defines, so they must be exempt from this check.
+    # Add directories as string here to automatically be exempt in case you have a non-complaint file name.
+    excluded_files = [
+        #  Wildcard directories, all files are expected to be exempt.
+        "code/__DEFINES/*.dm",
+        "code/__HELPERS/*.dm",
+        "code/_globalvars/*.dm",
+        # TGS files come from another repository so lets not worry about them.
+        "code/modules/tgs/**/*.dm",
+    ]
 
-# This files/directories are expected to have "global" defines, so they must be exempt from this check.
-# Add directories as string here to automatically be exempt in case you have a non-complaint file name.
-excluded_files = [
-    #  Wildcard directories, all files are expected to be exempt.
-    "code/__DEFINES/*.dm",
-    "code/__HELPERS/*.dm",
-    "code/_globalvars/*.dm",
-    # TGS files come from another repository so lets not worry about them.
-    "code/modules/tgs/**/*.dm",
-]
+    define_regex = re.compile(r"^(\s+)?#define\s?([A-Z0-9_]+)\(?(.+)\)?", re.MULTILINE)
 
-define_regex = re.compile(r"^(\s+)?#define\s?([A-Z0-9_]+)\(?(.+)\)?", re.MULTILINE)
+    files_to_scan = []
 
-files_to_scan = []
+    number_of_defines = 0
 
-number_of_defines = 0
+    for code_file in glob.glob(parent_directory, recursive=True):
+        exempt_file = False
+        for exempt_directory in excluded_files:
+            if fnmatch.fnmatch(code_file, exempt_directory):
+                exempt_file = True
+                break
 
-for code_file in glob.glob(parent_directory, recursive=True):
-    exempt_file = False
-    for exempt_directory in excluded_files:
-        if fnmatch.fnmatch(code_file, exempt_directory):
-            exempt_file = True
-            break
+        if exempt_file:
+            continue
 
-    if exempt_file:
-        continue
+        # If the "base path" of the file starts with an underscore, it's assumed to be an encapsulated file holding references to the other files in its folder and is exempt from the checks.
+        if os.path.basename(code_file)[0] == "_":
+            continue
 
-    # If the "base path" of the file starts with an underscore, it's assumed to be an encapsulated file holding references to the other files in its folder and is exempt from the checks.
-    if os.path.basename(code_file)[0] == "_":
-        continue
+        files_to_scan.append(code_file)
 
-    files_to_scan.append(code_file)
+    located_error_tuples = []
 
-located_error_tuples = []
+    for applicable_file in files_to_scan:
+        with open(applicable_file, encoding="utf8") as file:
+            file_contents = file.read()
+            for define in define_regex.finditer(file_contents):
+                number_of_defines += 1
+                define_name = define.group(2)
+                if not re.search("#undef\s" + define_name, file_contents):
+                    located_error_tuples.append((define_name, applicable_file))
 
-for applicable_file in files_to_scan:
-    with open(applicable_file, encoding="utf8") as file:
-        file_contents = file.read()
-        for define in define_regex.finditer(file_contents):
-            number_of_defines += 1
-            define_name = define.group(2)
-            if not re.search("#undef\s" + define_name, file_contents):
-                located_error_tuples.append((define_name, applicable_file))
+    if number_of_defines == 0:
+        print(red("No defines found! This is likely an error."))
+        sys.exit(1)
 
-if number_of_defines == 0:
-    print(red("No defines found! This is likely an error."))
-    sys.exit(1)
+    if number_of_defines <= 1000:
+        print(red(f"Only found {number_of_defines} defines! Something has likely gone wrong as the number of local defines should not be this low."))
+        sys.exit(1)
 
-if number_of_defines <= 1000:
-    print(red(f"Only found {number_of_defines} defines! Something has likely gone wrong as the number of local defines should not be this low."))
-    sys.exit(1)
+    if len(located_error_tuples):
+        fix_errors = False
+        if(not on_github):
+            fix_errors = input(red("We found files missing #undef\'s at the end. Would you like to fix these errors?") + " [y/n]: ").lower()[0] == "y" # Check if the answer is "Yes", "y", "Y", etc
+        for error in located_error_tuples:
+            post_error(error[0], error[1], on_github, fix_errors)
 
-if len(located_error_tuples):
-    fix_errors = False
-    if(not on_github):
-        fix_errors = input(red("We found files missing #undef\'s at the end. Would you like to fix these errors?") + " [y/n]: ").lower()[0] == "y" # Check if the answer is "Yes", "y", "Y", etc
-    for error in located_error_tuples:
-        post_error(error[0], error[1], on_github, fix_errors)
+        print(red(how_to_fix_message))
+        sys.exit(1)
 
-    print(red(how_to_fix_message))
-    sys.exit(1)
+    else:
+        print(green(f"No unhandled local defines found (found {number_of_defines} defines)."))
 
-else:
-    print(green(f"No unhandled local defines found (found {number_of_defines} defines)."))
+if __name__ == "__main__":
+    main()
