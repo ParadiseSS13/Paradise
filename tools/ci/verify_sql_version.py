fd0e6999697caccf727f2f3afc0e875a3a92575b
Contrabang <91113370+Contrabang@users.noreply.github.com>
2024-04-21 13:17:31 -0400

    LETS GO

--- tools/ci/verify_sql_version.py
+++ tools/ci/verify_sql_version.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         if search_result := re.search(r"#define SQL_VERSION (\d+)", define_data):
             define_sql = int(search_result.group(1))
         else:
-            print_error(f"No byond define for SQL found in {define_path}. Please edit verify_sql_version.py to the new location of #define SQL_VERSION", define_path)
+            print_error(f"No byond define for SQL found in {define_path}", define_path)
             exit_code = 1
     else:
         print_error(f"File containing the byond define for the SQL version does not exist ({define_path})")
@@ -92,10 +92,10 @@ if __name__ == "__main__":
         exit_code = 1
     elif(exit_code == 0):
         if config_sql != updates_folder_sql:
-            print_error(f"Updates file SQL version ({updates_folder_sql}) does not match the config SQL version ({config_sql})", updates_file)
+            print_error(f"Updates file SQL version ({updates_folder_sql}) does not match the config SQL version ({config_sql})", config_path)
             exit_code = 1
         if define_sql != updates_folder_sql:
-            print_error(f"Updates file SQL version ({updates_folder_sql}) does not match the byond define SQL version ({define_sql})", updates_file)
+            print_error(f"Updates file SQL version ({updates_folder_sql}) does not match the byond define SQL version ({define_sql})", define_path)
             exit_code = 1
 
 
