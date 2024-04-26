5c4d86996e7d7850202d46630c4955c905ef3890
Arthri <41360489+a@users.noreply.github.com>
2024-04-26 09:57:19 +0000

    Remove the error

--- tgui/packages/tgui-say/index.tsx
+++ tgui/packages/tgui-say/index.tsx
@@ -4,11 +4,6 @@ import { render } from 'inferno';
 import { TguiSay } from './TguiSay';
 
 document.onreadystatechange = function () {
-  if (document.readyState !== 'complete') {
-    Byond.sendMessage('ready_state_error');
-    return;
-  }
-
   const root = document.getElementById('react-root');
   render(<TguiSay />, root);
 };
