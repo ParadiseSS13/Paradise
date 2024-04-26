cd813e6adaf07279b7c6b5a4ad032488a5e69e69
Arthri <41360489+a@users.noreply.github.com>
2024-04-26 13:37:21 +0000

    Restore precautionary if statement

--- tgui/packages/tgui-say/index.tsx
+++ tgui/packages/tgui-say/index.tsx
@@ -4,6 +4,8 @@ import { render } from 'inferno';
 import { TguiSay } from './TguiSay';
 
 document.onreadystatechange = function () {
+  if (document.readyState !== 'complete') return;
+
   const root = document.getElementById('react-root');
   render(<TguiSay />, root);
 };
