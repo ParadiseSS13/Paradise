626af2ab6498b02c080fa867c8f37e55f5be625b
S34N <12197162+S34NW@users.noreply.github.com>
2024-05-17 10:19:52 +0100

    temp fix for OD

--- tools/ci/setup_od.sh
+++ tools/ci/setup_od.sh
@@ -4,5 +4,8 @@ set -eo pipefail
 git clone https://github.com/OpenDreamProject/OpenDream.git OpenDream
 cd OpenDream
 git submodule update --init --recursive
+# Below is temp until OD is fixed
+cp ../tools/ci/global.json .
+# end temp
 dotnet restore
 dotnet build -c Release
