6f17c0b8da9e8155cb6e5fa57dd61e79091303dd
Daylight0 <markuskm2@gmail.com>
2024-06-01 04:07:34 +0300

    Fix?

--- SQL/updates/56-57.sql
+++ SQL/updates/56-57.sql
@@ -2,7 +2,7 @@
 #Allows players to set defaults for AI related variables
 
 ALTER TABLE `characters`
-	ADD COLUMN `ai_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
-	ADD COLUMN `cyborg_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
-	ADD COLUMN `core_display` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL;
+	ADD COLUMN `ai_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
+	ADD COLUMN `cyborg_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
+	ADD COLUMN `core_display` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
 	ADD COLUMN `hologram` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL;
