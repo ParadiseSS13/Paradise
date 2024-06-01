4858a05e763fc4eb97af1246104bb2cad29e0ae8
Daylight0 <markuskm2@gmail.com>
2024-06-01 03:22:25 +0300

    Missed this whilst refactoring

--- SQL/updates/56-57.sql
+++ SQL/updates/56-57.sql
@@ -5,5 +5,4 @@ ALTER TABLE `characters`
 	ADD COLUMN `ai_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
 	ADD COLUMN `cyborg_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
 	ADD COLUMN `core_display` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
-	ADD COLUMN `hologram_category` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
-	ADD COLUMN `hologram_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
+	ADD COLUMN `hologram` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL;
