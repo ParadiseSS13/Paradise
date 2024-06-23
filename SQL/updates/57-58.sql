a8ead08751107c88f7efa1a4c40c2c241f5bf2d0
Spaghetti-bit <yumyumkillkill@gmail.com>
2024-06-23 12:34:26 -0700

    Fixes a SQL query.

--- SQL/updates/57-58.sql
+++ SQL/updates/57-58.sql
@@ -1,8 +1,6 @@
 # Updates the DB from 57 to 58 -SpaghettiBit
 # Adds a runechat color preference to characters.
 
-ALTER TABLE `characters` MODIFY COLUMN `real_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL
-
 # Add runechat color after hair_gradient_alpha
 ALTER TABLE `characters`
 	ADD COLUMN `runechat_color` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL AFTER `hair_gradient_alpha`;
