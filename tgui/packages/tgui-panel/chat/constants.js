7b51d3c9db59a1e0d2993395315f11220a54f902
Burzah <116982774+Burzah@users.noreply.github.com>
2024-08-19 02:24:16 -0700

    Moves to OOC - Adjusts Voting Classes

--- tgui/packages/tgui-panel/chat/constants.js
+++ tgui/packages/tgui-panel/chat/constants.js
@@ -66,7 +66,7 @@ export const MESSAGE_TYPES = [
     type: MESSAGE_TYPE_INFO,
     name: 'Info',
     description: 'Non-urgent messages from the game and items',
-    selector: '.notice:not(.pm), .adminnotice, .info, .sinister, .cult, .interface',
+    selector: '.notice:not(.pm), .adminnotice, .info, .sinister, .cult',
   },
   {
     type: MESSAGE_TYPE_WARNING,
@@ -84,7 +84,7 @@ export const MESSAGE_TYPES = [
     type: MESSAGE_TYPE_OOC,
     name: 'OOC',
     description: 'The bluewall of global OOC messages',
-    selector: '.ooc, .adminooc',
+    selector: '.ooc, .adminooc, .interface',
   },
   {
     type: MESSAGE_TYPE_ADMINPM,
