c1b0fb0b4b1a42da95a6afad0e1d19e76c190611
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2024-08-29 15:19:59 +0300

    Update paths

--- tools/UpdatePaths/Update Paths.bat
+++ tools/UpdatePaths/Update Paths.bat
@@ -1,3 +1,3 @@
 @echo off
-call "%~dp0\..\bootstrap\python" -m UpdatePaths %* -d "../../_maps/map_files220"
+call "%~dp0\..\bootstrap\python" -m UpdatePaths %* -d "%~dp0/../../_maps/map_files220"
 pause
