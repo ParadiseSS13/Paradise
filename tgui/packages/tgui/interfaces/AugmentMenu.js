f3baafa9eb2c49d26edfa2930d6400d48b9964c5
Burzah <116982774+Burzah@users.noreply.github.com>
2024-09-30 05:35:43 -0700

    Fixes Search and Adds Ability Stages to UI

--- tgui/packages/tgui/interfaces/AugmentMenu.js
+++ tgui/packages/tgui/interfaces/AugmentMenu.js
@@ -8,67 +8,97 @@ export const AugmentMenu = (props, context) => {
     <Window width={700} height={660} theme="malfunction">
       <Window.Content>
         <Stack vertical>
-          <Abilities />
+          <Abilities context={context} />
         </Stack>
       </Window.Content>
     </Window>
   );
 };
 
-const Abilities = (props, context) => {
+const Abilities = ({ context }) => {
   const { act, data } = useBackend(context);
   const { usable_swarms, ability_tabs, known_abilities } = data;
   const [selectedTab, setSelectedTab] = useLocalState(context, 'selectedTab', ability_tabs[0]);
   const [searchText, setSearchText] = useLocalState(context, 'searchText', '');
 
-  const currentTab = ability_tabs.find((tab) => tab.category_name === selectedTab.category_name) || {};
-  const { abilities = [], category_stage } = currentTab;
+  const getFilteredAbilities = () => {
+    const currentTab = ability_tabs.find((tab) => tab.category_name === selectedTab.category_name);
+    if (!currentTab) return [];
 
-  const filteredAbilities = abilities.filter((ability) => {
-    const isWithinStage = ability.stage <= Math.min(category_stage, 4);
-    const matchesSearch = !searchText || ability.name.toLowerCase().includes(searchText.toLowerCase());
-    return isWithinStage && matchesSearch;
-  });
-
-  const handleSearch = (value) => setSearchText(value);
-  const handleTabChange = (tab) => {
-    setSelectedTab(tab);
-    setSearchText('');
+    const maxStage = Math.min(currentTab.category_stage, 4);
+    return currentTab.abilities
+      .filter(
+        (ability) =>
+          ability.stage <= maxStage && (!searchText || ability.name.toLowerCase().includes(searchText.toLowerCase()))
+      )
+      .sort((a, b) =>
+        ['intruder', 'destroyer'].includes(selectedTab.category_name.toLowerCase()) ? a.stage - b.stage : 0
+      );
   };
 
+  const abilities = getFilteredAbilities();
+  const currentTab = ability_tabs.find((tab) => tab.category_name === selectedTab.category_name);
   const showStage = ['intruder', 'destroyer'].includes(selectedTab.category_name.toLowerCase());
 
-  const renderAbilityButton = (ability, currentCost, knownAbility) => (
-    <Button
-      height="20px"
-      width="35px"
-      mb={1}
-      textAlign="center"
-      content={currentCost}
-      disabled={currentCost > usable_swarms || (knownAbility && knownAbility.current_level === knownAbility.max_level)}
-      tooltip="Purchase this ability?"
-      onClick={() => {
-        act('purchase', { ability_path: ability.ability_path });
-        setSelectedTab(selectedTab);
-      }}
-    />
-  );
+  const renderAbility = (ability) => {
+    const knownAbility = known_abilities.find((a) => a.ability_path === ability.ability_path);
+    const currentCost = knownAbility ? knownAbility.cost : ability.cost;
+    const levelInfo =
+      knownAbility && knownAbility.current_level > 0
+        ? `${knownAbility.current_level} / ${knownAbility.max_level}`
+        : `0 / ${ability.max_level}`;
+
+    return (
+      <Stack.Item key={ability.name} direction="row">
+        <Stack>
+          <Button
+            height="20px"
+            width="35px"
+            mb={1}
+            textAlign="center"
+            content={currentCost}
+            disabled={
+              currentCost > usable_swarms || (knownAbility && knownAbility.current_level === knownAbility.max_level)
+            }
+            tooltip="Purchase this ability?"
+            onClick={() => {
+              act('purchase', { ability_path: ability.ability_path });
+              setSelectedTab(selectedTab);
+            }}
+          />
+          <Stack.Item fontSize="16px">{ability.name}</Stack.Item>
+        </Stack>
+        <Stack.Item>
+          <Stack vertical>
+            <Stack.Item fontSize="13px">{ability.desc}</Stack.Item>
+            <Stack.Item color="green">
+              Level: {levelInfo}
+              {showStage && ability.stage > 0 && <span> (Stage: {ability.stage})</span>}
+            </Stack.Item>
+            <Stack.Divider />
+          </Stack>
+        </Stack.Item>
+      </Stack.Item>
+    );
+  };
 
   return (
     <Section
       title={
-        <Stack vertical>
-          <span>Swarms: {usable_swarms}</span>
-          {showStage && <span>Stage: {Math.min(category_stage, 4)}</span>}
-        </Stack>
-      }
-      buttons={
-        <Input
-          width="200px"
-          placeholder="Search Abilities"
-          onInput={(e, value) => handleSearch(value)}
-          value={searchText}
-        />
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Stack vertical style={{ marginRight: '10px' }}>
+            <span>Swarms: {usable_swarms}</span>
+            {showStage && currentTab && <span>Category Stage: {Math.min(currentTab.category_stage, 4)}</span>}
+          </Stack>
+          <div className="Section__buttons">
+            <Input
+              width="200px"
+              placeholder="Search Abilities"
+              onInput={(e, value) => setSearchText(value)}
+              value={searchText}
+            />
+          </div>
+        </div>
       }
     >
       <Tabs>
@@ -76,7 +106,10 @@ const Abilities = (props, context) => {
           <Tabs.Tab
             key={tab.category_name}
             selected={selectedTab.category_name === tab.category_name}
-            onClick={() => handleTabChange(tab)}
+            onClick={() => {
+              setSelectedTab(tab);
+              setSearchText('');
+            }}
           >
             {capitalize(tab.category_name)}
           </Tabs.Tab>
@@ -84,78 +117,62 @@ const Abilities = (props, context) => {
         <Tabs.Tab
           key="upgrades"
           selected={selectedTab.category_name === 'upgrades'}
-          onClick={() => handleTabChange({ category_name: 'upgrades' })}
+          onClick={() => setSelectedTab({ category_name: 'upgrades' })}
         >
           Upgrades
         </Tabs.Tab>
       </Tabs>
       {selectedTab.category_name === 'upgrades' ? (
-        <Upgrades />
+        <Upgrades act={act} abilityTabs={ability_tabs} knownAbilities={known_abilities} usableSwarms={usable_swarms} />
       ) : (
-        <Stack vertical>
-          {filteredAbilities.map((ability) => {
-            const knownAbility = known_abilities.find((a) => a.ability_path === ability.ability_path);
-            const currentCost = knownAbility ? knownAbility.cost : ability.cost;
-
-            return (
-              <Stack.Item key={ability.name} direction="row">
-                <Stack>
-                  {renderAbilityButton(ability, currentCost, knownAbility)}
-                  <Stack.Item fontSize="16px">{ability.name}</Stack.Item>
-                </Stack>
-                <Stack.Item>
-                  <Stack vertical>
-                    <Stack.Item fontSize="13px">{ability.desc}</Stack.Item>
-                    <Stack.Item color="green">
-                      Level: {knownAbility?.current_level || 0} / {ability.max_level}
-                    </Stack.Item>
-                    <Stack.Divider />
-                  </Stack>
-                </Stack.Item>
-              </Stack.Item>
-            );
-          })}
-        </Stack>
+        <Stack vertical>{abilities.map(renderAbility)}</Stack>
       )}
     </Section>
   );
 };
 
-const Upgrades = (props, context) => {
-  const { act, data } = useBackend(context);
-  const { usable_swarms, known_abilities } = data;
+const Upgrades = ({ act, abilityTabs, knownAbilities, usableSwarms }) => {
+  const relevantAbilities = abilityTabs
+    .flatMap((tab) =>
+      tab.abilities.filter((ability) =>
+        knownAbilities.some(
+          (known) => known.ability_path === ability.ability_path && known.current_level < known.max_level
+        )
+      )
+    )
+    .sort((a, b) => a.stage - b.stage);
 
-  return (
-    <Stack vertical>
-      {known_abilities.map(
-        (ability) =>
-          ability.current_level < ability.max_level && (
-            <Stack.Item key={ability.name} direction="row">
-              <Stack>
-                <Button
-                  height="20px"
-                  width="35px"
-                  mb={1}
-                  textAlign="center"
-                  content={ability.cost}
-                  disabled={ability.cost > usable_swarms}
-                  tooltip="Upgrade this ability?"
-                  onClick={() => act('purchase', { ability_path: ability.ability_path })}
-                />
-                <Stack.Item fontSize="16px">{ability.name}</Stack.Item>
-              </Stack>
-              <Stack.Item>
-                <Stack vertical>
-                  <Stack.Item fontSize="13px">{ability.upgrade_text}</Stack.Item>
-                  <Stack.Item color="green" fontSize="13px">
-                    Level: {ability.current_level} / {ability.max_level}
-                  </Stack.Item>
-                  <Stack.Divider />
-                </Stack>
-              </Stack.Item>
+  const renderAbility = (ability) => {
+    const knownAbility = knownAbilities.find((a) => a.ability_path === ability.ability_path);
+
+    return (
+      <Stack.Item key={ability.name} direction="row">
+        <Stack>
+          <Button
+            height="20px"
+            width="35px"
+            mb={1}
+            textAlign="center"
+            content={ability.cost}
+            disabled={ability.cost > usableSwarms}
+            tooltip="Upgrade this ability?"
+            onClick={() => act('purchase', { ability_path: ability.ability_path })}
+          />
+          <Stack.Item fontSize="16px">{ability.name}</Stack.Item>
+        </Stack>
+        <Stack.Item>
+          <Stack vertical>
+            <Stack.Item fontSize="13px">{ability.desc}</Stack.Item>
+            <Stack.Item color="green" fontSize="13px">
+              Level: {knownAbility.current_level} / {knownAbility.max_level}
+              {ability.stage > 0 && <span> (Stage: {ability.stage})</span>}
             </Stack.Item>
-          )
-      )}
-    </Stack>
-  );
+            <Stack.Divider />
+          </Stack>
+        </Stack.Item>
+      </Stack.Item>
+    );
+  };
+
+  return <Stack vertical>{relevantAbilities.map(renderAbility)}</Stack>;
 };
