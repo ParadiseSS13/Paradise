9c856eda687f9d7fa4b6683dcefe24400e2a50a6
warriorstar-orion <orion@snowfrost.garden>
2024-11-02 21:37:13 -0400

    update aa's scuffed python

--- tools/ci/generate_sql_scripts.py
+++ tools/ci/generate_sql_scripts.py
@@ -61,6 +61,8 @@ for file in orderedSqlFiles:
             scriptLines.append("python3 SQL/updates/" + str(file) + " 127.0.0.1 root root paradise_gamedb\n")
         elif file == "38-39.py":
             scriptLines.append("python3 SQL/updates/" + str(file) + " 127.0.0.1 root root paradise_gamedb\n")
+        elif file == "60-61.py":
+            scriptLines.append("python3 SQL/updates/" + str(file) + " 127.0.0.1 root root paradise_gamedb\n")
         else:
             print("ERROR: CI failed due to invalid python file in SQL/updates")
             exit(1)
