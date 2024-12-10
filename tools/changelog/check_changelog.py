b78dda5dc8e36974d61c3a5e57a3108497d9e2cd
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2024-12-10 19:22:59 +0300

    Merge Upstream Workflow (#1694)
    ## Что этот PR делает
    Добавляет воркфлоу, который мержит апстрим.
    Чейнджлог собирается из обнаруженных в истории ПРов и переводится
    (опционально).
    Отслеживаются метки изменений в конфиге, бд, вики. ПРы с этими метками
    отдельно упоминаются в теле ПРа.
    Конфликты решать самому, увы.
    
    Используется ветка для пула с апстрима, которая может быть создана, если
    не существует.
    
    В чейнджлоге остаются комментарии, содержащие текст исходного изменения,
    если был перевод, и ссылку на оригинальный ПР.
    
    ```
    fix: Сброс идентификатора агента больше не вызывает неизменного типа крови. <!-- fix: Resetting an agent ID no longer causes bloodtype to be unchangable. (https://github.com/ParadiseSS13/Paradise/pull/27509) -->
    ```
    
    ## Тестирование
    
    Пример ПРа (автор я потому что токен был мой):
    https://github.com/m-dzianishchyts/Paradise-SS220/pull/48
    
    https://github.com/m-dzianishchyts/Paradise-SS220/actions/runs/12247152346/job/34164451774

--- tools/changelog/check_changelog.py
+++ tools/changelog/check_changelog.py
@@ -2,114 +2,23 @@
 DO NOT MANUALLY RUN THIS SCRIPT.
 ---------------------------------
 
-Expected envrionmental variables:
+Expected environmental variables:
 -----------------------------------
 GITHUB_REPOSITORY: Github action variable representing the active repo (Action provided)
-BOT_TOKEN: A repository account token, this will allow the action to push the changes (Action provided)
+GITHUB_TOKEN: A repository account token, this will allow the action to push the changes (Action provided)
 GITHUB_EVENT_PATH: path to JSON file containing the event info (Action provided)
 """
 import os
-import re
-import copy
 from pathlib import Path
 from ruamel.yaml import YAML
 from github import Github
 import json
 
-DISCORD_EMBED_DESCRIPTION_LIMIT = 4096
-
-CL_BODY = re.compile(r"(:cl:|🆑)[ \t]*(?P<author>.+?)?\s*\n(?P<content>(.|\n)*?)\n/(:cl:|🆑)", re.MULTILINE)
-CL_SPLIT = re.compile(r"\s*(?:<!--.*-->)?((?P<tag>\w+)\s*:)?\s*(?P<message>.*)")
-
-DISCORD_TAG_EMOJI = {
-    "soundadd": ":notes:",
-    "sounddel": ":mute:",
-    "imageadd": ":frame_photo:",
-    "imagedel": ":scissors:",
-    "codeadd": ":sparkles:",
-    "codedel": ":wastebasket:",
-    "tweak": ":screwdriver:",
-    "fix": ":tools:",
-    "wip": ":construction_site:",
-    "spellcheck": ":pencil:",
-    "experiment": ":microscope:"
-}
-
-
-def build_changelog(pr: dict) -> dict:
-    changelog = parse_changelog(pr.body)
-    changelog["author"] = changelog["author"] or pr.user.login
-    return changelog
-
-
-def emojify_changelog(changelog: dict):
-    changelog_copy = copy.deepcopy(changelog)
-    for change in changelog_copy["changes"]:
-        if change["tag"] in DISCORD_TAG_EMOJI:
-            change["tag"] = DISCORD_TAG_EMOJI[change["tag"]]
-        else:
-            raise Exception(f"Invalid tag for emoji: {change}")
-    return changelog_copy
-
-
-def validate_changelog(changelog: dict):
-    if not changelog:
-        raise Exception("No changelog.")
-    if not changelog["author"]:
-        raise Exception("The changelog has no author.")
-    if len(changelog["changes"]) == 0:
-        raise Exception("No changes found in the changelog. Use special label if changelog is not expected.")
-    message = "\n".join(map(lambda change: f"{change['tag']} {change['message']}", changelog["changes"]))
-    if len(message) > DISCORD_EMBED_DESCRIPTION_LIMIT:
-        raise Exception(f"The changelog exceeds the length limit ({DISCORD_EMBED_DESCRIPTION_LIMIT}). Shorten it.")
-
-
-def parse_changelog(message: str) -> dict:
-    with open(Path.cwd().joinpath("tags.yml")) as file:
-        yaml = YAML(typ = 'safe', pure = True)
-        tags_config = yaml.load(file)
-    cl_parse_result = CL_BODY.search(message)
-    if cl_parse_result is None:
-        raise Exception("Failed to parse the changelog. Check changelog format.")
-    cl_changes = []
-    for cl_line in cl_parse_result.group("content").splitlines():
-        if not cl_line:
-            continue
-        change_parse_result = CL_SPLIT.search(cl_line)
-        if not change_parse_result:
-            raise Exception(f"Invalid change: '{cl_line}'")
-        tag = change_parse_result["tag"]
-        message = change_parse_result["message"]
-        if tag and tag not in tags_config['tags'].keys():
-            raise Exception(f"Invalid tag: '{cl_line}'. Valid tags: {', '.join(tags_config['tags'].keys())}")
-        if not message:
-            raise Exception(f"No message for change: '{cl_line}'")
-        if message in list(tags_config['defaults'].values()): # Check to see if the tags are associated with something that isn't the default text
-            raise Exception(f"Don't use default message for change: '{cl_line}'")
-        if tag:
-            cl_changes.append({
-                "tag": tags_config['tags'][tag],
-                "message": message
-            })
-        # Append line without tag to the previous change
-        else:
-            if len(cl_changes):
-                prev_change = cl_changes[-1]
-                prev_change["message"] += f" {message}"
-            else:
-                raise Exception(f"Change with no tag: {cl_line}")
-
-    if len(cl_changes) == 0:
-        raise Exception("No changes found in the changelog. Use special label if changelog is not expected.")
-    return {
-        "author": str.strip(cl_parse_result.group("author") or "") or None,  # I want this to be None, not empty
-        "changes": cl_changes
-    }
-
+import changelog_utils
 
 # Blessed is the GoOnStAtIoN birb ZeWaKa for thinking of this first
 repo = os.getenv("GITHUB_REPOSITORY")
-token = os.getenv("BOT_TOKEN")
+token = os.getenv("GITHUB_TOKEN")
 event_path = os.getenv("GITHUB_EVENT_PATH")
 
 with open(event_path, 'r') as f:
@@ -123,10 +32,6 @@ pr_body = pr.body or ""
 pr_author = pr.user.login
 pr_labels = pr.labels
 
-CL_INVALID = ":scroll: CL невалиден"
-CL_VALID = ":scroll: CL валиден"
-CL_NOT_NEEDED = ":scroll: CL не требуется"
-
 pr_is_mirror = pr.title.startswith("[MIRROR]")
 
 has_valid_label = False
@@ -134,12 +39,12 @@ has_invalid_label = False
 cl_required = True
 for label in pr_labels:
     print("Found label: ", label.name)
-    if label.name == CL_NOT_NEEDED:
+    if label.name == changelog_utils.CL_NOT_NEEDED:
         print("No CL needed!")
         cl_required = False
-    if label.name == CL_VALID:
+    if label.name == changelog_utils.CL_VALID:
         has_valid_label = True
-    if label.name == CL_INVALID:
+    if label.name == changelog_utils.CL_INVALID:
         has_invalid_label = True
 
 if pr_is_mirror:
@@ -148,30 +53,33 @@ if pr_is_mirror:
 if not cl_required:
     # remove invalid, remove valid
     if has_invalid_label:
-        pr.remove_from_labels(CL_INVALID)
+        pr.remove_from_labels(changelog_utils.CL_INVALID)
     if has_valid_label:
-        pr.remove_from_labels(CL_VALID)
+        pr.remove_from_labels(changelog_utils.CL_VALID)
     exit(0)
 
 try:
-    cl = build_changelog(pr)
-    cl_emoji = emojify_changelog(cl)
+    with open(Path.cwd().joinpath("tags.yml")) as file:
+        yaml = YAML(typ = 'safe', pure = True)
+        tags_config = yaml.load(file)
+    cl = changelog_utils.build_changelog(pr, tags_config)
+    cl_emoji = changelog_utils.emojify_changelog(cl)
     cl_emoji["author"] = cl_emoji["author"] or pr_author
-    validate_changelog(cl_emoji)
+    changelog_utils.validate_changelog(cl_emoji)
 except Exception as e:
     print("Changelog parsing error:")
     print(e)
 
     # add invalid, remove valid
     if not has_invalid_label:
-        pr.add_to_labels(CL_INVALID)
+        pr.add_to_labels(changelog_utils.CL_INVALID)
     if has_valid_label:
-        pr.remove_from_labels(CL_VALID)
+        pr.remove_from_labels(changelog_utils.CL_VALID)
     exit(1)
 
 # remove invalid, add valid
 if has_invalid_label:
-    pr.remove_from_labels(CL_INVALID)
+    pr.remove_from_labels(changelog_utils.CL_INVALID)
 if not has_valid_label:
-    pr.add_to_labels(CL_VALID)
+    pr.add_to_labels(changelog_utils.CL_VALID)
 print("Changelog is valid.")
---
+++ tools/merge-upstream/merge_upstream.py
@@ -0,0 +1,284 @@
+import enum
+import os
+import re
+import subprocess
+import time
+import typing
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from github import Github
+from github.PullRequest import PullRequest
+from googletrans import Translator
+
+import changelog_utils
+
+
+class UpstreamLabel(str, enum.Enum):
+    CONFIG_CHANGE = "Configuration Change"
+    SQL_CHANGE = "SQL Change"
+    WIKI_CHANGE = "Requires Wiki Update"
+
+
+class DownstreamLabel(str, enum.Enum):
+    WIKI_CHANGE = ":page_with_curl: Требуется изменение WIKI"
+
+
+class PullDetails(typing.TypedDict):
+    changelog: typing.Dict[str, list[str]]
+    merge_order: list[str]
+    config_changes: typing.Dict[str, PullRequest]
+    sql_changes: typing.Dict[str, PullRequest]
+    wiki_changes: typing.Dict[str, PullRequest]
+
+
+LABEL_BLOCK_STYLE = {
+    UpstreamLabel.CONFIG_CHANGE: "IMPORTANT",
+    UpstreamLabel.SQL_CHANGE: "IMPORTANT",
+    UpstreamLabel.WIKI_CHANGE: "NOTE",
+}
+
+
+def check_env():
+    """Check if the required environment variables are set."""
+    required_vars = [
+        "GITHUB_TOKEN",
+        "TARGET_REPO",
+        "TARGET_BRANCH",
+        "UPSTREAM_REPO",
+        "UPSTREAM_BRANCH",
+        "MERGE_BRANCH"
+    ]
+    missing_vars = [var for var in required_vars if not os.getenv(var)]
+    if missing_vars:
+        raise EnvironmentError(f"Missing required environment variables: {', '.join(missing_vars)}")
+
+
+# Environment variables
+check_env()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+TARGET_REPO = os.getenv("TARGET_REPO")
+TARGET_BRANCH = os.getenv("TARGET_BRANCH")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO")
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH")
+MERGE_BRANCH = os.getenv("MERGE_BRANCH")
+TRANSLATE_CHANGES = os.getenv("TRANSLATE_CHANGES", "False").lower() in ("true", "yes", "1")
+CHANGELOG_AUTHOR = os.getenv("CHANGELOG_AUTHOR", "")
+
+
+def run_command(command) -> str:
+    """Run a shell command and return its output."""
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        result.check_returncode()
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {command}\nExit code: {e.returncode}\nOutput: {e.output}\nError: {e.stderr}")
+        raise
+
+
+def setup_repo():
+    """Clone the repository and set up the upstream remote."""
+    print(f"Cloning repository: {TARGET_REPO}")
+    run_command(f"git clone https://{GITHUB_TOKEN}@github.com/{TARGET_REPO}.git repo")
+    os.chdir("repo")
+    run_command(f"git remote add upstream https://{GITHUB_TOKEN}@github.com/{UPSTREAM_REPO}.git")
+    print(run_command(f"git remote -v"))
+
+
+def update_merge_branch():
+    """Update the merge branch with the latest changes from upstream."""
+    print(f"Fetching branch {UPSTREAM_BRANCH} from upstream...")
+    run_command(f"git fetch upstream {UPSTREAM_BRANCH}")
+    run_command(f"git fetch origin")
+    all_branches = run_command("git branch -a").split()
+
+    if f"remotes/origin/{MERGE_BRANCH}" not in all_branches:
+        print(f"Branch '{MERGE_BRANCH}' does not exist. Creating it from upstream/{UPSTREAM_BRANCH}...")
+        run_command(f"git checkout -b {MERGE_BRANCH} upstream/{UPSTREAM_BRANCH}")
+        run_command(f"git push -u origin {MERGE_BRANCH}")
+        return
+
+    print(f"Resetting {MERGE_BRANCH} onto upstream/{UPSTREAM_BRANCH}...")
+    run_command(f"git checkout {MERGE_BRANCH}")
+    run_command(f"git reset --hard upstream/{UPSTREAM_BRANCH}")
+
+    print("Pushing changes to origin...")
+    run_command(f"git push origin {MERGE_BRANCH} --force")
+
+
+def detect_commits() -> list[str]:
+    """Detect commits from upstream not yet in downstream."""
+    print("Detecting new commits from upstream...")
+    commit_log = run_command(f"git log {TARGET_BRANCH}..{MERGE_BRANCH} --pretty=format:'%h %s'").split("\n")
+    commit_log.reverse()
+    return commit_log
+
+
+def fetch_pull(pull_id) -> PullRequest | None:
+    """Fetch the pull request from GitHub."""
+    github = Github(GITHUB_TOKEN)
+    repo = github.get_repo(UPSTREAM_REPO)
+
+    max_retries = 3
+    for attempt in range(max_retries):
+        try:
+            return repo.get_pull(int(pull_id))
+        except Exception as e:
+            print(f"Error fetching PR #{pull_id}: {e}")
+            if attempt + 1 < max_retries:
+                time.sleep(2)
+            else:
+                return None
+
+
+def build_details(commit_log: list[str]) -> PullDetails:
+    """Generate data from parsed commits."""
+    print("Building details...")
+    details = PullDetails(
+        changelog={},
+        merge_order=[match.group()[1:] for c in commit_log if (match := re.search("#\\d+", c))],
+        config_changes={},
+        sql_changes={},
+        wiki_changes={}
+    )
+    pull_cache = {}
+    translator = Translator()
+
+    with ThreadPoolExecutor() as executor:
+        futures = {}
+        for commit in commit_log:
+            match = re.search("#\\d+", commit)
+            if not match:
+                print(f"Skipping {commit}")
+                continue
+
+            pull_id = match.group()[1:]
+
+            if pull_id in pull_cache:
+                print(
+                    f"WARNING: pull duplicate found.\n"
+                    f"1: {pull_cache[pull_id]}\n"
+                    f"2: {commit}"
+                )
+                print(f"Skipping {commit}")
+                continue
+
+            pull_cache[pull_id] = commit
+            futures[executor.submit(fetch_pull, pull_id)] = pull_id
+
+        for future in as_completed(futures):
+            pull_id = futures[future]
+            pull: PullRequest | None = future.result()
+            labels = [label.name for label in pull.get_labels()]
+            pull_changes = []
+
+            if not pull:
+                continue
+
+            try:
+                for label in labels:
+                    if label == UpstreamLabel.CONFIG_CHANGE.value:
+                        details["config_changes"][pull_id] = pull
+                    elif label == UpstreamLabel.SQL_CHANGE.value:
+                        details["sql_changes"][pull_id] = pull
+                    elif label == UpstreamLabel.WIKI_CHANGE.value:
+                        details["wiki_changes"][pull_id] = pull
+
+                parsed = changelog_utils.parse_changelog(pull.body)
+                if parsed and parsed["changes"]:
+                    for change in parsed["changes"]:
+                        tag = change["tag"]
+                        message = change["message"]
+                        if TRANSLATE_CHANGES:
+                            translated_message = translator.translate(message, src="en", dest="ru").text
+                            change = f"{tag}: {translated_message} <!-- {tag}: {message} ({pull.html_url}) -->"
+                        else:
+                            change = f"{tag}: {message} <!-- ({pull.html_url}) -->"
+                        pull_changes.append(change)
+
+                if pull_changes:
+                    details["changelog"][pull_id] = pull_changes
+            except Exception as e:
+                print(
+                    f"An error occurred while processing {commit}\n"
+                    f"URL: {pull.html_url}\n"
+                    f"Body: {pull.body}"
+                )
+                raise e
+
+    return details
+
+
+def prepare_pull_body(details: PullDetails) -> str:
+    """Build new pull request body from the generated changelog."""
+    pull_body = (
+        f"This pull request merges upstream/{UPSTREAM_BRANCH}. "
+        f"Resolve possible conflicts manually and make sure all the changes are applied correctly.\n"
+    )
+
+    if not details:
+        return pull_body
+
+    label_to_changes = {
+        UpstreamLabel.CONFIG_CHANGE: details["config_changes"],
+        UpstreamLabel.SQL_CHANGE: details["sql_changes"],
+        UpstreamLabel.WIKI_CHANGE: details["wiki_changes"]
+    }
+    for label, changes in label_to_changes.items():
+        if not changes:
+            continue
+
+        pull_body += (
+            f"\n> [!{LABEL_BLOCK_STYLE[label]}]\n"
+            f"> {label.value}:\n"
+        )
+        for _, pull in sorted(changes.items()):
+            pull_body += f"> {pull.html_url}\n"
+
+    if not details["changelog"]:
+        return pull_body
+
+    pull_body += f"\n## Changelog\n"
+    pull_body += f":cl: {CHANGELOG_AUTHOR}\n" if CHANGELOG_AUTHOR else ":cl:\n"
+    for pull_id in details["merge_order"]:
+        if pull_id not in details["changelog"]:
+            continue
+        pull_body += f"{'\n'.join(details["changelog"][pull_id])}\n"
+    pull_body += "/:cl:\n"
+
+    return pull_body
+
+
+def create_pr(details: PullDetails):
+    """Create a pull request with the processed changelog."""
+    pull_body = prepare_pull_body(details)
+
+    print("Creating pull request...")
+    github = Github(GITHUB_TOKEN)
+    repo = github.get_repo(TARGET_REPO)
+
+    # Create the pull request
+    pull = repo.create_pull(
+        title=f"Merge Upstream {datetime.today().strftime('%d.%m.%Y')}",
+        body=pull_body,
+        head=MERGE_BRANCH,
+        base=TARGET_BRANCH
+    )
+
+    if details["wiki_changes"]:
+        pull.add_to_labels(DownstreamLabel.WIKI_CHANGE)
+
+    print("Pull request created successfully.")
+
+
+if __name__ == "__main__":
+    setup_repo()
+
+    update_merge_branch()
+    commit_log = detect_commits()
+
+    if commit_log:
+        details = build_details(commit_log)
+        create_pr(details)
+    else:
+        print(f"No changes detected from {UPSTREAM_REPO}/{UPSTREAM_BRANCH}. Skipping pull request creation.")
