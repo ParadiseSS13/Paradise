79be95a8e7aecafdcc62634a0ba03fc5d9cfed53
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2024-12-20 23:56:41 +0000

    aaaaaaaaaaaaaa

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -4,13 +4,13 @@ export SPACEMANDMM_TAG=suite-1.9
 # For TGUI
 export NODE_VERSION=20
 # Stable Byond Major
-export STABLE_BYOND_MAJOR=516
+export STABLE_BYOND_MAJOR=515
 # Stable Byond Minor
-export STABLE_BYOND_MINOR=1648
+export STABLE_BYOND_MINOR=1633
 # Beta Byond Major - Uncomment and update if beta cycle active
-#export BETA_BYOND_MAJOR=515
+export BETA_BYOND_MAJOR=516
 # Beta Byond Minor - Uncomment and update if beta cycle active
-#export BETA_BYOND_MINOR=1633
+export BETA_BYOND_MINOR=1648
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.6
 # RUSTG version
