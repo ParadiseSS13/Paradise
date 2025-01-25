d37e7d42d4c23e68083543ada2f0cd5e66cde210
Burzah <116982774+Burzah@users.noreply.github.com>
2025-01-24 20:10:08 -0700

    Reworks TGUI Menu

--- tgui/packages/tgui/interfaces/QuirkMenu.js
+++ tgui/packages/tgui/interfaces/QuirkMenu.js
@@ -3,58 +3,63 @@ import { Box, Button, Section, Stack, Tabs, Input } from '../components';
 import { Window } from '../layouts';
 import { ButtonCheckbox } from '../components/Button';
 
-
 export const QuirkMenu = (props, context) => {
-  return(
-    <Window width={700} height={850}>
+  return (
+    <Window width={625} height={540}>
       <Window.Content>
         <Stack fill horizontal>
-          <Quirks context={context}/>
+          <Quirks context={context} />
         </Stack>
       </Window.Content>
     </Window>
   );
 };
 
-const Quirks = ({context}) => {
-  const {act, data} = useBackend(context);
-  const {quirk_balance, selected_quirks, all_quirks} = data;
+const Quirks = ({ context }) => {
+  const { act, data } = useBackend(context);
+  const { quirk_balance, selected_quirks, all_quirks } = data;
 
   const HasChosenQuirk = (quirk) => {
-    return Boolean(selected_quirks && selected_quirks.includes(quirk.name))
+    return Boolean(selected_quirks && selected_quirks.includes(quirk.name));
   };
 
   const BalanceCheck = (quirk) => {
-    return Boolean(quirk_balance + quirk.cost > 0)
-  }
+    return Boolean(quirk_balance + quirk.cost > 0);
+  };
 
   const RenderQuirk = (quirk) => {
     const alreadyChosen = HasChosenQuirk(quirk);
-    const canPick = BalanceCheck(quirk)
+    const canPick = BalanceCheck(quirk);
     return (
-    <Stack>
-        <ButtonCheckbox checked = {alreadyChosen}
-          disabled = {canPick && !alreadyChosen}
-          onClick={() => act(alreadyChosen ? 'remove_quirk' : 'add_quirk', {path: quirk.path})}/>
-      <Stack vertical>
-        <Stack.Item bold>
-          {quirk.name}: {quirk.cost}
-        </Stack.Item>
-        <Stack.Item>
-          {quirk.desc}
-        </Stack.Item>
-      </Stack>
-    </Stack>
+      <div>
+        <Stack horizontal align="center">
+          <ButtonCheckbox
+            checked={alreadyChosen}
+            disabled={canPick && !alreadyChosen}
+            onClick={() => act(alreadyChosen ? 'remove_quirk' : 'add_quirk', { path: quirk.path })}
+            mr="10px"
+          />
+          <Stack horizontal align="center" justify="start" style={{ width: '100%' }}>
+            <Stack.Item mr="35px">
+              {quirk.name}: {quirk.cost}
+            </Stack.Item>
+            <Stack.Item style={{ flex: 2 }} mr="10px">
+              {quirk.desc}
+            </Stack.Item>
+          </Stack>
+        </Stack>
+        <hr />
+      </div>
     );
   };
 
   return (
-  <Section textAlign = 'center'>
-    <b>Current quirk balance: {quirk_balance}</b>
+    <Section textAlign="center">
+      <b>Current quirk balance: {quirk_balance}</b>
+      <hr />
       <Stack fill vertical>
         {all_quirks.map(RenderQuirk)}
       </Stack>
-  </Section>
+    </Section>
   );
 };
-
