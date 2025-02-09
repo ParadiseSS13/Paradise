c3c96c8ebb5b4bdbae144edb4064b4450645d237
Spaghetti-bit <yumyumkillkill@gmail.com>
2025-02-09 07:53:42 -0700

    Apply suggestions from code review
    Co-authored-by: AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
    Signed-off-by: Spaghetti-bit <yumyumkillkill@gmail.com>

--- SQL/updates/63-64.sql
+++ SQL/updates/63-64.sql
@@ -2,4 +2,4 @@
 # Adds a subtype race to be stored on character saves
 # Add species_subtype after pda_ringtone
 ALTER TABLE `characters`
-	ADD COLUMN `species_subtype` VARCHAR(7) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'None' AFTER `pda_ringtone`;
+	ADD COLUMN `species_subtype` VARCHAR(45) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'None' AFTER `pda_ringtone`;
