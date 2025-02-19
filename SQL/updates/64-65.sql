6ec1c55e10ed2f247fa19c653aa00444dd713c31
BiancaWilkson <42818125+BiancaWilkson@users.noreply.github.com>
2025-02-18 22:19:06 -0500

    semicolon

--- SQL/updates/64-65.sql
+++ SQL/updates/64-65.sql
@@ -1,4 +1,4 @@
 #Updates the DB from 64 to 65
 #Adds a column to the characters table that stores quirks in a JSON format
 ALTER TABLE `characters`
-	ADD column `quirks` LONGTEXT COLLATE 'utf8mb4_unicode_ci' DEFAULT NULL AFTER `species_subtype`
+	ADD column `quirks` LONGTEXT COLLATE 'utf8mb4_unicode_ci' DEFAULT NULL AFTER `species_subtype`;
