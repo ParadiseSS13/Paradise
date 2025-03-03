abb51ff7ade733272fedee6e1f27e795cd529099
chuga <98280110+chuga-git@users.noreply.github.com>
2025-03-02 23:45:07 -0600

    makes check_icon_dupenames.py and check_icon_conflicts.py faster because i'm dying of old age over here

--- tools/ci/check_icon_conflicts.py
+++ tools/ci/check_icon_conflicts.py
@@ -1,37 +1,42 @@
 from collections import defaultdict
-import glob
-import sys
-import time
-
+from glob import glob
+from time import time
+from concurrent.futures import ProcessPoolExecutor
 from ..dmi import Dmi
 
+
+def check_state(dmi_path: str) -> str:
+    names = []
+    dmi = Dmi.from_file(dmi_path)
+    for state in dmi.states:
+        if '!CONFLICT!' in state.name:
+            names.append(state.name)
+    return dmi_path, names
 
 if __name__ == "__main__":
     print("check_icon_conflicts started")
 
     count = 0
     exit_code = 0
-    start = time.time()
+    start = time()
 
     findings = defaultdict(list)
 
-    for dmi_path in glob.glob("**/*.dmi", recursive=True):
-        dmi = Dmi.from_file(dmi_path)
-        for state in dmi.states:
-            if '!CONFLICT!' in state.name:
-                findings[dmi_path].append(state.name)
-        count += 1
-
+    files = glob("**/*.dmi", recursive=True)
+    with ProcessPoolExecutor() as executor:
+        for dmi_path, names in executor.map(check_state, glob("**/*.dmi", recursive=True)):
+            if names:
+                findings[dmi_path] += names
+            count += 1
     if findings:
         exit_code = 1
-
         for filename in sorted(findings.keys()):
             states = findings[filename]
             for state in sorted(states):
                 print(f"{filename}: conflicted state {state}")
 
 
-    end = time.time()
+    end = time()
     print(f"\ncheck_icon_conflicts checked {count} files in {end - start:.2f}s\n")
 
-    sys.exit(exit_code)
+    exit(exit_code)
--- tools/ci/check_icon_dupenames.py
+++ tools/ci/check_icon_dupenames.py
@@ -1,29 +1,34 @@
 from collections import defaultdict
-import glob
-import sys
-import time
-
+from glob import glob
+from time import time
+from concurrent.futures import ProcessPoolExecutor
 from ..dmi import Dmi
 
+def check_states(dmi_path: str):
+    dmi = Dmi.from_file(dmi_path)
+    names = []
+    states = set()
+    for state in dmi.states:
+        # Movement states have the same name as their non-movement counterparts
+        if (state.name, state.movement) in states:
+            names.append(state.name)
+        states.add((state.name, state.movement))
+    return dmi_path, names
 
 if __name__ == "__main__":
     print("check_icon_dupenames started")
 
     count = 0
     exit_code = 0
-    start = time.time()
+    start = time()
 
     findings = defaultdict(list)
 
-    for dmi_path in glob.glob("**/*.dmi", recursive=True):
-        dmi = Dmi.from_file(dmi_path)
-        states = set()
-        for state in dmi.states:
-            # Movement states have the same name as their non-movement counterparts
-            if (state.name, state.movement) in states:
-                findings[dmi_path].append(state.name)
-            states.add((state.name, state.movement))
-        count += 1
+    with ProcessPoolExecutor() as executor:
+        for dmi_path, names in executor.map(check_states, glob("**/*.dmi", recursive=True)):
+            if names:
+                findings[dmi_path] += names
+            count += 1
 
     if findings:
         exit_code = 1
@@ -34,7 +39,7 @@ if __name__ == "__main__":
                 print(f"{filename}: duplicate state name `{state}`")
 
 
-    end = time.time()
+    end = time()
     print(f"\ncheck_icon_dupenames checked {count} files in {end - start:.2f}s\n")
 
-    sys.exit(exit_code)
+    exit(exit_code)
