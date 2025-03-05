9050a9f7957543f60d44a06608dc5bfdd7dbfb57
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2025-03-05 21:14:44 +0000

    What in the heck is this

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,11 @@
 mod logging;
 mod mapmanip;
 mod milla;
+
+#[cfg(all(not(feature = "byond-515"), not(feature = "byond-516")))]
+compile_error!("Please specify b515 or b515 as a feature to specify BYOND version.");
+
+#[cfg(all(feature = "byond-515", feature = "byond-516"))]
+compile_error!(
+    "Please specify ONLY b515 or b515 as a feature to specify BYOND version, not all features."
+);
