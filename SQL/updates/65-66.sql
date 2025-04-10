62475fbd9092bfafbd757bf0cf8be7b7cea6135e
BiancaWilkson <42818125+BiancaWilkson@users.noreply.github.com>
2025-04-09 21:36:53 -0400

    sql update for fixing merge conflicts

--- SQL/updates/65-66.sql
+++ SQL/updates/65-66.sql
@@ -1,4 +1,4 @@
-#Updates the DB from 64 to 65
+#Updates the DB from 65 to 66
 #Adds a column to the characters table that stores quirks in a JSON format
 ALTER TABLE `characters`
 	ADD column `quirks` LONGTEXT COLLATE 'utf8mb4_unicode_ci' DEFAULT NULL AFTER `species_subtype`;
