c09c83372fd4fa38c11031a3eec462eba6e64121
warriorstar-orion <orion@snowfrost.garden>
2025-05-10 14:25:10 -0400

    make sure we have a fallback here

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -3,6 +3,19 @@ set -euo pipefail
 
 if [ -z "${BYOND_MAJOR+x}" ]; then
   source _build_dependencies.sh
+  # if some other build step hasn't specified the specific BYOND version we're not
+  # gonna get it straight from _build_dependencies.sh so one more fallback here
+  BYOND_MAJOR=$STABLE_BYOND_MAJOR
+  BYOND_MINOR=$STABLE_BYOND_MINOR
+
+  if [ -z "$1" ]; then
+    echo "No arg specified. Assuming STABLE."
+  else
+    if [ "$1" == "BETA" ]; then
+      BYOND_MAJOR=$BETA_BYOND_MAJOR
+      BYOND_MINOR=$BETA_BYOND_MINOR
+    fi
+  fi
 fi
 
 if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
