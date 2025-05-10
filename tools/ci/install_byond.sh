154990502081cc3f74c9dd352ca00f108b7ee0a4
warriorstar-orion <orion@snowfrost.garden>
2025-05-10 14:12:00 -0400

    integrate tg's restore_or_install_byond github action

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
 
-source _build_dependencies.sh
-
-TARGET_MAJOR=$STABLE_BYOND_MAJOR
-TARGET_MINOR=$STABLE_BYOND_MINOR
-
-if [ -z "$1" ]; then
-	echo "No arg specified. Assuming STABLE."
-else
-	if [ "$1" == "BETA" ]; then
-		TARGET_MAJOR=$BETA_BYOND_MAJOR
-		TARGET_MINOR=$BETA_BYOND_MINOR
-	fi
+if [ -z "${BYOND_MAJOR+x}" ]; then
+  source _build_dependencies.sh
 fi
 
-if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${TARGET_MAJOR}.${TARGET_MINOR}" $HOME/BYOND/version.txt;
+if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
 then
   echo "Using cached directory."
 else
@@ -23,11 +13,11 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "http://www.byond.com/download/build/${TARGET_MAJOR}/${TARGET_MAJOR}.${TARGET_MINOR}_byond_linux.zip" -H "User-Agent: ParadiseSS13/Paradise CI" -o byond.zip
+  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
   make here
-  echo "$TARGET_MAJOR.$TARGET_MINOR" > "$HOME/BYOND/version.txt"
+  echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
   cd ~/
 fi
