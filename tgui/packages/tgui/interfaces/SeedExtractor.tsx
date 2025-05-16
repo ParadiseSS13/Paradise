53438afa192d77d4d95bf2c82729fc7ccd0e0d85
warriorstar-orion <orion@snowfrost.garden>
2025-05-16 13:40:12 -0400

    fix seed extractor

--- tgui/packages/tgui/interfaces/SeedExtractor.tsx
+++ tgui/packages/tgui/interfaces/SeedExtractor.tsx
@@ -1,48 +1,68 @@
-import { useState } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
 import { Button, Icon, Input, Section, Stack, Table } from 'tgui-core/components';
 
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
 import { ComplexModal } from './common/ComplexModal';
 
-export const SeedExtractor = (properties) => {
-  const { act, data } = useBackend();
-  const { loginState, currentPage } = data;
+type SeedExtractorContextType = {
+  searchTextState: [string, (value: string) => void];
+  vendAmountState: [number, (value: number) => void];
+  sortIdState: [string, (value: string) => void];
+  sortOrderState: [boolean, (value: boolean) => void];
+}
+
+const SeedExtractorContext = createContext<SeedExtractorContextType>({
+  searchTextState: ['', () => {}],
+  vendAmountState: [1, () => {}],
+  sortIdState: ['name', () => {}],
+  sortOrderState: [true, () => {}],
+})
+
+export const SeedExtractor = (props) => {
+  const searchTextState = useState('');
+  const vendAmountState = useState(1);
+  const sortIdState = useState('name');
+  const sortOrderState = useState(true);
 
   return (
     <Window theme="hydroponics" width={800} height={400}>
       <ComplexModal />
       <Window.Content>
-        <Stack fill vertical>
-          <Stack.Item>
-            <SeedExtractorActions />
-          </Stack.Item>
-          <SeedList />
-        </Stack>
+        <SeedExtractorContext.Provider value={{searchTextState, vendAmountState, sortIdState, sortOrderState}}>
+          <Stack fill vertical>
+            <Stack.Item>
+              <SeedExtractorActions />
+            </Stack.Item>
+            <SeedList />
+          </Stack>
+        </SeedExtractorContext.Provider>
       </Window.Content>
     </Window>
   );
 };
 
-const seedFilter = (searchText) => {
+type SeedFilter = (seed: Seed) => boolean;
+
+const seedFilter = (searchText: string) => {
   const eq = (actual, test) => actual === test;
   const ge = (actual, test) => actual >= test;
   const le = (actual, test) => actual <= test;
   let terms = searchText.split(' ');
-  let filters = [];
+  let filters: SeedFilter[] = [];
   for (let term of terms) {
     let parts = term.split(':');
     if (parts.length === 0) {
       continue;
     }
     if (parts.length === 1) {
-      filters.push((seed) =>
+      filters.push((seed: Seed) =>
         (seed.name + ' (' + seed.variant + ')').toLocaleLowerCase().includes(parts[0].toLocaleLowerCase())
       );
       continue;
     }
     if (parts.length > 2) {
-      return (seed) => false;
+      return (seed: Seed) => false;
     }
     let searchVal;
     let cmp = eq;
@@ -56,13 +76,13 @@ const seedFilter = (searchText) => {
       searchVal = Number(parts[1]);
     }
     if (isNaN(searchVal)) {
-      return (seed) => false;
+      return (seed: Seed) => false;
     }
     switch (parts[0].toLocaleLowerCase()) {
       case 'l':
       case 'life':
       case 'lifespan':
-        filters.push((seed) => cmp(seed.lifespan, searchVal));
+        filters.push((seed: Seed) => cmp(seed.lifespan, searchVal));
         break;
       case 'e':
       case 'end':
@@ -97,10 +117,10 @@ const seedFilter = (searchText) => {
         filters.push((seed) => cmp(seed.amount, searchVal));
         break;
       default:
-        return (seed) => false;
+        return (seed: Seed) => false;
     }
   }
-  return (seed) => {
+  return (seed: Seed) => {
     for (let filter of filters) {
       if (!filter(seed)) {
         return false;
@@ -110,13 +130,35 @@ const seedFilter = (searchText) => {
   };
 };
 
-const SeedList = (properties) => {
-  const { act, data } = useBackend();
-  const { icons, seeds, vend_amount } = data;
-  const [searchText, setSearchText] = useState('searchText', '');
-  const [vendAmount, setVendAmount] = useState('vendAmount', 1);
-  const [sortId, _setSortId] = useState('sortId', 'name');
-  const [sortOrder, _setSortOrder] = useState('sortOrder', true);
+type Seed = {
+  image: string;
+  id: number;
+  name: string;
+  variant: string | null;
+  lifespan: number;
+  endurance: number;
+  maturation: number;
+  production: number;
+  yield: number;
+  potency: number;
+  amount: number;
+}
+
+type SeedListData = {
+  icons: { [key: string]: string};
+  seeds: Seed[];
+}
+
+const SeedList = (props) => {
+  const { act, data } = useBackend<SeedListData>();
+  const { searchTextState, vendAmountState, sortIdState, sortOrderState } = useContext<SeedExtractorContextType>(SeedExtractorContext);
+  const [ searchText, setSearchText ] = searchTextState;
+  const [ vendAmount, setVendAmount ] = vendAmountState;
+  const [ sortId, setSortId ] = sortIdState;
+  const [ sortOrder, setSortOrder ] = sortOrderState;
+
+  const { icons, seeds } = data;
+
   return (
     <Stack.Item grow mt={0.5}>
       <Section fill scrollable>
@@ -131,7 +173,7 @@ const SeedList = (properties) => {
             <SortButton id="potency">Potency</SortButton>
             <SortButton id="amount">Stock</SortButton>
           </Table.Row>
-          {seeds.lenth === 0
+          {seeds.length === 0
             ? 'No seeds present.'
             : seeds
                 .filter(seedFilter(searchText))
@@ -179,10 +221,17 @@ const SeedList = (properties) => {
   );
 };
 
-const SortButton = (properties) => {
-  const [sortId, setSortId] = useState('name');
-  const [sortOrder, setSortOrder] = useState(true);
-  const { id, children } = properties;
+type SortButtonProps = {
+  id: string;
+  children: ReactNode;
+}
+
+const SortButton = (props: SortButtonProps) => {
+  const { sortIdState, sortOrderState } = useContext<SeedExtractorContextType>(SeedExtractorContext);
+  const [ sortId, setSortId ] = sortIdState;
+  const [ sortOrder, setSortOrder ] = sortOrderState;
+
+  const { id, children } = props;
   return (
     <Table.Cell>
       <Button
@@ -204,11 +253,11 @@ const SortButton = (properties) => {
   );
 };
 
-const SeedExtractorActions = (properties) => {
-  const { act, data } = useBackend();
-  const { vend_amount } = data;
-  const [searchText, setSearchText] = useState('');
-  const [vendAmount, setVendAmount] = useState(1);
+const SeedExtractorActions = (props) => {
+  const { searchTextState, vendAmountState } = useContext<SeedExtractorContextType>(SeedExtractorContext);
+  const [ searchText, setSearchText ] = searchTextState;
+  const [ vendAmount, setVendAmount ] = vendAmountState;
+
   return (
     <Stack fill>
       <Stack.Item grow>
@@ -216,11 +265,12 @@ const SeedExtractorActions = (properties) => {
           placeholder="Search by name, variant, potency:70+, production:3-, ..."
           fluid
           onChange={(value) => setSearchText(value)}
+          value={searchText}
         />
       </Stack.Item>
       <Stack.Item>
         Vend amount:
-        <Input placeholder="1" onChange={(value) => setVendAmount(Number(value) >= 1 ? Number(value) : 1)} />
+        <Input placeholder="1" onChange={(value) => setVendAmount(Number(value) >= 1 ? Number(value) : 1)} value={`${vendAmount}`} />
       </Stack.Item>
     </Stack>
   );
