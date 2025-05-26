627ffa1632158ec18bbd030e28a7441e5aec82fc
/tg/ui Builder <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-26 14:10:01 +0000

    Reformat /tg/ui

--- tgui/packages/tgui/debug/ThemeContext.ts
+++ tgui/packages/tgui/debug/ThemeContext.ts
@@ -1,6 +1,3 @@
 import { createContext } from 'react';
 
-export const ThemeContext = createContext([
-  '',
-  (value: string) => {},
-]);
+export const ThemeContext = createContext(['', (value: string) => {}]);
