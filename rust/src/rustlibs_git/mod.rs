ae963389902876a19bcd0f0e7a4ec737020ba8c3
FunnyMan3595 (Charlie Nolan) <funnyman3595@gmail.com>
2025-07-24 22:22:24 +0000

    rl_git_revparse only

--- rust/src/rustlibs_git/mod.rs
+++ rust/src/rustlibs_git/mod.rs
@@ -8,3 +8,24 @@ use gix::{bstr::BStr, open::Error as OpenError, Repository};
 thread_local! {
     static REPOSITORY: Result<Repository, OpenError> = gix::open(".");
 }
+
+#[byondapi::bind]
+fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
+    let rev_str: String = rev.get_string()?;
+    let rev_bytes = BStr::new(&rev_str);
+
+    let repo_check_result = REPOSITORY.with(|repo| -> Option<String> {
+        let repo_ref = repo.as_ref().ok()?;
+        let object = repo_ref.rev_parse_single(rev_bytes).ok()?;
+        Some(object.to_string())
+    });
+
+    match repo_check_result {
+        Some(res) => {
+            return Ok(ByondValue::new_str(res)?);
+        }
+        None => {
+            return Ok(ByondValue::null());
+        }
+    }
+}
