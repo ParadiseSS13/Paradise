03321ffd036fcbf315b8d0cfa27673917cd1e7eb
FunnyMan3595 (Charlie Nolan) <funnyman3595@gmail.com>
2025-07-25 18:24:24 +0000

    Pushing down

--- rust/src/rustlibs_git/mod.rs
+++ rust/src/rustlibs_git/mod.rs
@@ -8,13 +8,16 @@ use gix::{bstr::BStr, open::Error as OpenError, Repository};
 #[byondapi::bind]
 fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
     let rev_str: String = rev.get_string()?;
-    let rev_bytes = BStr::new(&rev_str);
-
-    let repo = gix::open(".")?;
-    if let Ok(revision) = repo.rev_parse_single(rev_bytes) {
-        return Ok(ByondValue::new_str(revision.to_string())?);
+    if let Some(rev_id) = internal_rl_git_revparse(rev_str) {
+        return Ok(ByondValue::new_str(rev_id)?);
     }
     Ok(ByondValue::null())
+}
+
+fn internal_rl_git_revparse(rev_str: String) -> Option<String> {
+    let repo = gix::open(".").ok()?;
+    let rev_bytes = BStr::new(&rev_str);
+    Some(repo.rev_parse_single(rev_bytes).ok()?.to_string())
 }
 
 #[byondapi::bind]
@@ -22,14 +25,14 @@ fn rl_git_commit_date(rev: ByondValue, ts_format: ByondValue) -> eyre::Result<By
     let rev_str: String = rev.get_string()?;
     let ts_format_str: String = ts_format.get_string()?;
 
-    let repo = gix::open(".")?;
-    if let Some(datetime) = internal_rl_get_commit_date(&repo, rev_str) {
+    if let Some(datetime) = internal_rl_get_commit_date(rev_str) {
         return Ok(ByondValue::new_str(datetime.format(&ts_format_str).to_string())?);
     }
     Ok(ByondValue::null())
 }
 
-fn internal_rl_get_commit_date(repo: &Repository, rev_str: String) -> Option<DateTime<Utc>> {
+fn internal_rl_get_commit_date(rev_str: String) -> Option<DateTime<Utc>> {
+    let repo = gix::open(".").ok()?;
     let rev_bytes: &BStr = BStr::new(&rev_str);
     let rev = repo.rev_parse_single(rev_bytes).ok()?;
     let object = rev.object().ok()?;
