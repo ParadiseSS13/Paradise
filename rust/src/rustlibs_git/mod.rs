31e6646ded1f7067312a980c8eb56d1965b53bc6
FunnyMan3595 (Charlie Nolan) <funnyman3595@gmail.com>
2025-07-25 15:57:08 +0000

    nonlocal

--- rust/src/rustlibs_git/mod.rs
+++ rust/src/rustlibs_git/mod.rs
@@ -1,42 +1,39 @@
 use byondapi::value::ByondValue;
-use chrono::{TimeZone, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 use gix::{bstr::BStr, open::Error as OpenError, Repository};
 
 // The methods in this file have the rl prefix for rustlibs as gix likes to make its own dll exports
 // That makes things reallyyyyyyyyy messy
 
+#[byondapi::bind]
+fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
+    let rev_str: String = rev.get_string()?;
+    let rev_bytes = BStr::new(&rev_str);
 
-thread_local! {
-    static REPOSITORY: Result<Repository, OpenError> = gix::open(".");
+    let repo = gix::open(".")?;
+    if let Ok(revision) = repo.rev_parse_single(rev_bytes) {
+        return Ok(ByondValue::new_str(revision.to_string())?);
+    }
+    Ok(ByondValue::null())
 }
 
 #[byondapi::bind]
-fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
-    return Ok(ByondValue::null());
-}
-
-pub fn rl_git_commit_date(rev: ByondValue, ts_format: ByondValue) -> eyre::Result<ByondValue> {
+fn rl_git_commit_date(rev: ByondValue, ts_format: ByondValue) -> eyre::Result<ByondValue> {
     let rev_str: String = rev.get_string()?;
-    let rev_bytes: &BStr = BStr::new(&rev_str);
-
     let ts_format_str: String = ts_format.get_string()?;
 
-    let repo_check_result = REPOSITORY.with(|repo| -> Option<String> {
-        let repo_ref = repo.as_ref().ok()?;
-        let rev = repo_ref.rev_parse_single(rev_bytes).ok()?;
-        let object = rev.object().ok()?;
-        let commit = object.try_into_commit().ok()?;
-        let commit_time = commit.time().ok()?;
-        let datetime = Utc.timestamp_opt(commit_time.seconds, 0).latest()?;
-        Some(datetime.format(&ts_format_str).to_string())
-    });
-
-    match repo_check_result {
-        Some(res) => {
-            return Ok(ByondValue::new_str(res)?);
-        }
-        None => {
-            return Ok(ByondValue::null());
-        }
+    let repo = gix::open(".")?;
+    if let Some(datetime) = internal_rl_get_commit_date(&repo, rev_str) {
+        return Ok(ByondValue::new_str(datetime.format(&ts_format_str).to_string())?);
     }
+    Ok(ByondValue::null())
+}
+
+fn internal_rl_get_commit_date(repo: &Repository, rev_str: String) -> Option<DateTime<Utc>> {
+    let rev_bytes: &BStr = BStr::new(&rev_str);
+    let rev = repo.rev_parse_single(rev_bytes).ok()?;
+    let object = rev.object().ok()?;
+    let commit = object.try_into_commit().ok()?;
+    let commit_time = commit.time().ok()?;
+    Utc.timestamp_opt(commit_time.seconds, 0).latest()
 }
