9031fe456c3e134d29f468436c70f27e43bc3a4e
FunnyMan3595 (Charlie Nolan) <funnyman3595@gmail.com>
2025-07-25 14:39:16 +0000

    half and half

--- rust/src/rustlibs_git/mod.rs
+++ rust/src/rustlibs_git/mod.rs
@@ -10,24 +10,9 @@ thread_local! {
     static REPOSITORY: Result<Repository, OpenError> = gix::open(".");
 }
 
-pub fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
-    let rev_str: String = rev.get_string()?;
-    let rev_bytes = BStr::new(&rev_str);
-
-    let repo_check_result = REPOSITORY.with(|repo| -> Option<String> {
-        let repo_ref = repo.as_ref().ok()?;
-        let object = repo_ref.rev_parse_single(rev_bytes).ok()?;
-        Some(object.to_string())
-    });
-
-    match repo_check_result {
-        Some(res) => {
-            return Ok(ByondValue::new_str(res)?);
-        }
-        None => {
-            return Ok(ByondValue::null());
-        }
-    }
+#[byondapi::bind]
+fn rl_git_revparse(rev: ByondValue) -> eyre::Result<ByondValue> {
+    return Ok(ByondValue::null());
 }
 
 pub fn rl_git_commit_date(rev: ByondValue, ts_format: ByondValue) -> eyre::Result<ByondValue> {
