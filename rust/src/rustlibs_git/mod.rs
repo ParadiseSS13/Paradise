20b242a60550283925bfdfc768aee094cfcad7ca
Charlie Nolan <funnyman3595@gmail.com>
2025-07-24 17:39:00 -0700

    Pointless change to make CI actually run again

--- rust/src/rustlibs_git/mod.rs
+++ rust/src/rustlibs_git/mod.rs
@@ -5,6 +5,7 @@ use gix::{bstr::BStr, open::Error as OpenError, Repository};
 // The methods in this file have the rl prefix for rustlibs as gix likes to make its own dll exports
 // That makes things reallyyyyyyyyy messy
 
+
 thread_local! {
     static REPOSITORY: Result<Repository, OpenError> = gix::open(".");
 }
