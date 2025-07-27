fcbf9543f141cd988c798923b1fdac35dba70e38
Burzah <116982774+Burzah@users.noreply.github.com>
2025-07-27 14:06:09 -0700

    Update tools/ci/check_duplicate_procs.py
    Co-authored-by: Contrabang <91113370+Contrabang@users.noreply.github.com>
    Signed-off-by: Burzah <116982774+Burzah@users.noreply.github.com>

--- tools/ci/check_duplicate_procs.py
+++ tools/ci/check_duplicate_procs.py
@@ -2,32 +2,50 @@ import re
 from collections import defaultdict
 from pathlib import Path
 
-proc_definitions = defaultdict(list)
+import sys
+import time
+import glob
+import os
 
-for file in Path(".").glob("**/*.dm"):
-    with open(file, 'r', encoding='utf-8', errors='ignore') as f:
-        for line_num, line in enumerate(f, 1):
-            if match := re.match(r"^(/[\w/]+/[\w]+)\(\)$", line.strip()):
-                proc_path = match.group(1)
-                proc_definitions[proc_path].append((file, line_num))
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BLUE = "\033[0;34m"
+NC = "\033[0m"  # No Color
+
+def print_error(message: str, filename: str, line_number: int):
+	if os.getenv("GITHUB_ACTIONS") == "true": # We're on github, output in a special format.
+		print(f"::error file={filename},line={line_number},title=Identical Variables::{filename}:{line_number}: {message}")
+	else:
+		print(f"{filename}:{line_number}: {message}")
 
 def main():
-    alert_sent = False
-    for proc_path, locations in proc_definitions.items():
-        if len(locations) > 1:
-            if not alert_sent:
-                print("The following proc definitions are duplicated. Please fix!\n")
-                alert_sent = True
+	print("check_duplicate_procs started")
 
-            print(f">>> {proc_path}()")
-            for file, line_num in locations:
-                print(f"{file}:{line_num}")
-            print()
+	exit_code = 0
+	start = time.time()
+	
+	proc_definitions = defaultdict(list)
+	
+	dm_files = glob.glob("**/*.dm", recursive=True)
 
-    if alert_sent:
-        exit(1)
-    else:
-        exit(0)
+	for file in dm_files:
+		with open(file, 'r', encoding='utf-8', errors='ignore') as f:
+			for line_num, line in enumerate(f, 1):
+				if match := re.match(r"^(/[\w/]+/[\w]+)\(\)$", line.strip()):
+					proc_path = match.group(1)
+					proc_definitions[proc_path].append((file, line_num))
+
+	for proc_path, locations in proc_definitions.items():
+		if len(locations) > 1:
+			exit_code = 1
+
+			for file, line_num in locations:
+				print_error(f"Duplicate proc definition of {proc_path}", file, line_num)
+
+	end = time.time()
+	print(f"\ncheck_duplicate_procs tests completed in {end - start:.2f}s\n")
+
+	sys.exit(exit_code)  
 
 if __name__ == "__main__":
     main()
