16c4a350fc99c8d632d03886ba91ab21629eed47
Arthri <41360489+Arthri@users.noreply.github.com>
2025-08-08 18:47:22 +0800

    Fix !merge_upstream (#30031)
    * Deprecate tgui bundle merge driver
    
    * Fix merge upstream command
    
    * Only deprecate merge driver
    
    ---------
    
    Co-authored-by: Arthri <41360489+a@users.noreply.github.com>

--- tgui/bin/tgui_.ps1
+++ tgui/bin/tgui_.ps1
@@ -101,6 +101,7 @@ function task-validate-build {
 
 ## Installs merge drivers and git hooks
 function task-install-git-hooks () {
+  Write-Output "tgui: WARNING: tgui bundle merge drivers are deprecated. Please modify .gitattributes to continue using them"
   Set-Location $global:basedir
   git config --replace-all merge.tgui-merge-bundle.driver "tgui/bin/tgui --merge=bundle %P %A"
   Write-Output "tgui: Merge drivers have been successfully installed!"
