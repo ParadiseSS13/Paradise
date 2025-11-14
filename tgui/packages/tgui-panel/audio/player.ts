a907ed8738bfef6c55ae21779948ce4a60ef9f73
Bmon <no@email.com>
2025-11-14 13:31:07 -0500

    the fix

--- tgui/packages/tgui-panel/audio/player.ts
+++ tgui/packages/tgui-panel/audio/player.ts
@@ -63,7 +63,7 @@ export class AudioPlayer {
       });
     }
 
-    audio.play();
+    audio.play().catch((error) => logger.log('playback error', error));
 
     this.onPlaySubscribers.forEach((subscriber) => subscriber());
   }
