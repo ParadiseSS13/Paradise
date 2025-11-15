246a122b7650eb90753988a370ce32b56c6663ad
Migratingcocofruit <guycxz@gmail.com>
2025-11-16 00:43:44 +0200

    adds detail to the comment in the update file

--- SQL/updates/71-72.sql
+++ SQL/updates/71-72.sql
@@ -1,5 +1,6 @@
 # Updating SQL from 71 to 72 -MigratingCocofruit
-# Change the bug reports table
+# Change the bug reports table to use an incrementing id as the primary key
+# Also replaces uid with its timestamp form and adds a column that indicates the reports status
 ALTER TABLE `bug_reports` ADD `id` INT NOT NULL AUTO_INCREMENT;
 ALTER TABLE `bug_reports` CHANGE COLUMN 'uid' `filetime` DATETIME NOT NULL DEFAULT NOW();
 ALTER TABLE `bug_reports` ADD `submitted` BIT(2) NOT NULL DEFAULT 0,
