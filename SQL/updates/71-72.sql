13af4eeed0c8d8b922fd7344bc62365fa1a024de
Migratingcocofruit <guycxz@gmail.com>
2025-11-16 00:35:51 +0200

    changes the update file

--- SQL/updates/71-72.sql
+++ SQL/updates/71-72.sql
@@ -1,14 +1,7 @@
 # Updating SQL from 71 to 72 -MigratingCocofruit
 # Change the bug reports table
-CREATE TABLE `bug_reports` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `filetime` DATETIME NOT NULL DEFAULT NOW(),
-    `author_ckey` VARCHAR(32) NOT NULL COLLATE 'utf8mb4_general_ci',
-    `title` MEDIUMTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-    `round_id` INT(11) NULL DEFAULT NULL,
-    `contents_json` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-    `submitted` BIT(2) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    INDEX `submitted` (`submitted`)
-)
-COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
+ALTER TABLE `bug_reports` ADD `id` INT NOT NULL AUTO_INCREMENT;
+ALTER TABLE `bug_reports` CHANGE COLUMN 'uid' `filetime` DATETIME NOT NULL DEFAULT NOW();
+ALTER TABLE `bug_reports` ADD `submitted` BIT(2) NOT NULL DEFAULT 0,
+ALTER TABLE `bug_reports`     PRIMARY KEY (`id`),
+ALTER TABLE `bug_reports`   INDEX `submitted` (`submitted`)
