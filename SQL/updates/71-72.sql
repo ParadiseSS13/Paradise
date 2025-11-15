5f0bcc2d21684b3c3012265938a838e274656211
Migratingcocofruit <guycxz@gmail.com>
2025-11-16 00:36:16 +0200

    Update 71-72.sql

--- SQL/updates/71-72.sql
+++ SQL/updates/71-72.sql
@@ -3,5 +3,5 @@
 ALTER TABLE `bug_reports` ADD `id` INT NOT NULL AUTO_INCREMENT;
 ALTER TABLE `bug_reports` CHANGE COLUMN 'uid' `filetime` DATETIME NOT NULL DEFAULT NOW();
 ALTER TABLE `bug_reports` ADD `submitted` BIT(2) NOT NULL DEFAULT 0,
-ALTER TABLE `bug_reports`     PRIMARY KEY (`id`),
-ALTER TABLE `bug_reports`   INDEX `submitted` (`submitted`)
+ALTER TABLE `bug_reports` PRIMARY KEY (`id`),
+ALTER TABLE `bug_reports` INDEX `submitted` (`submitted`)
