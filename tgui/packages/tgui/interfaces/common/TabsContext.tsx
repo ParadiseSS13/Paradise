a6931d31ce7ca89650a4de0dcc9bd3fadb0827aa
warriorstar-orion <orion@snowfrost.garden>
2025-12-11 01:37:53 -0500

    Adds Shuttle Manager, a UI for in-game shuttles

--- tgui/packages/tgui/interfaces/common/TabsContext.tsx
+++ tgui/packages/tgui/interfaces/common/TabsContext.tsx
@@ -1,18 +1,16 @@
+import { noop } from 'lodash';
 import React, { createContext, useState } from 'react';
 
-const TabsContext = Object.assign(
-  createContext(
-    null as null | {
-      tabIndex: number;
-      setTabIndex: (value: number) => void;
-    }
-  ),
-  {
-    Default: (props: { children: React.JSX.Element; tabIndex: number }) => {
-      const [tabIndex, setTabIndex] = useState(props.tabIndex);
-      return <TabsContext.Provider value={{ tabIndex, setTabIndex }}>{props.children}</TabsContext.Provider>;
-    },
-  }
-);
+export interface TabsContextInterface {
+  tabIndex: number;
+  setTabIndex: (value: number) => void;
+}
+
+const TabsContext = Object.assign(createContext({ tabIndex: 0, setTabIndex: noop }), {
+  Default: (props: { children: React.JSX.Element; tabIndex: number }) => {
+    const [tabIndex, setTabIndex] = useState(props.tabIndex);
+    return <TabsContext.Provider value={{ tabIndex, setTabIndex }}>{props.children}</TabsContext.Provider>;
+  },
+});
 
 export default TabsContext;
