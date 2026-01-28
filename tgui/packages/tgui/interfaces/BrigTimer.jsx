87a73485a95a98864c03ea0160d4b2ec10c45e91
BrickTheHooman <106778834+BrickTheHooman@users.noreply.github.com>
2026-01-28 22:06:28 +0800

    adds quick timer

--- tgui/packages/tgui/interfaces/BrigTimer.jsx
+++ tgui/packages/tgui/interfaces/BrigTimer.jsx
@@ -1,10 +1,80 @@
-import { Box, Button, Dropdown, LabeledList, Section } from 'tgui-core/components';
+import { useState } from 'react';
+import { Box, Button, Divider, Dropdown, LabeledList, Section, Stack } from 'tgui-core/components';
 
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
+import { spaceLaw } from './common/SpaceLaw';
 
 export const BrigTimer = (props) => {
   const { act, data } = useBackend();
+  const [selectedCrimes, setSelectedCrimes] = useState(
+    Object.fromEntries(Object.keys(spaceLaw.codes).map((code) => [code, null]))
+  );
+
+  const [selectedModifiers, setSelectedModifiers] = useState(
+    Object.fromEntries(Object.keys(spaceLaw.modifiers).map((modifier) => [modifier, null]))
+  );
+
+  const formattedCrimes = Object.entries(selectedCrimes)
+    .filter(([_, value]) => value !== null)
+    .map(([code, crime]) => spaceLaw.codes[code][crime].name)
+    .join(', ');
+
+  const formattedModifiers = Object.entries(selectedModifiers)
+    .filter(([_, value]) => value !== null)
+    .map(([modifierGroup, modifier]) => spaceLaw.modifiers[modifierGroup][modifier].name)
+    .join(', ');
+
+  const crimeSummary =
+    (data.prisoner_notes
+      ? data.prisoner_notes && formattedCrimes
+        ? `${data.prisoner_notes} - `
+        : data.prisoner_notes
+      : '') +
+    (formattedCrimes ? formattedCrimes : '') +
+    (formattedModifiers ? ` (${formattedModifiers})` : '');
+
+  const baseTimer = Object.entries(selectedCrimes)
+    .filter(([_, value]) => value !== null)
+    .map(([code, crime]) => spaceLaw.codes[code][crime].severity)
+    .map((testingseverity) => spaceLaw.severities[testingseverity])
+    .reduce((acc, val) => ({ minTime: acc.minTime + val.minTime, maxTime: acc.maxTime + val.maxTime }), {
+      minTime: 0,
+      maxTime: 0,
+    });
+
+  const baseModifiers = Object.entries(selectedModifiers)
+    .filter(([_, value]) => value !== null)
+    .map(([modifierGroup, modifier]) => spaceLaw.modifiers[modifierGroup][modifier]);
+
+  const summedTimer = baseModifiers.reduce(
+    (acc, val) => ({ minTime: acc.minTime + (val.timeAdded ?? 0), maxTime: acc.maxTime + (val.timeAdded ?? 0) }),
+    baseTimer
+  );
+
+  const multipliedTimer = baseModifiers.reduce(
+    (acc, val) => ({
+      minTime: acc.minTime * (val.timeMultiplier ?? 1),
+      maxTime: acc.maxTime * (val.timeMultiplier ?? 1),
+    }),
+    summedTimer
+  );
+
+  const suggestedTime = {
+    suggestedMin: Math.min(Math.ceil(multipliedTimer.minTime), 60),
+    suggestedMax: Math.min(Math.ceil(multipliedTimer.maxTime), 60),
+  };
+
+  const formatSuggestedTimer = ({ suggestedMin, suggestedMax }) => {
+    if (suggestedMin === 60 && suggestedMax === 60) return 'Permanent Confinement';
+    if (suggestedMax === 60) return `${suggestedMin} Minutes to Permanent Confinement`;
+    if (suggestedMin === suggestedMax && suggestedMin !== 0) return `${suggestedMin} Minutes`;
+    if (suggestedMin && suggestedMax) return `${suggestedMin} to ${suggestedMax} Minutes`;
+    return 'Select Crimes for Suggestion';
+  };
+
+  const formattedSuggestedTimer = formatSuggestedTimer(suggestedTime);
+
   data.nameText = data.occupant;
   if (data.timing) {
     if (data.prisoner_hasrec) {
@@ -13,98 +83,195 @@ export const BrigTimer = (props) => {
       data.nameText = <Box color="red">{data.occupant}</Box>;
     }
   }
+
   let nameIcon = 'pencil-alt';
   if (data.prisoner_name) {
     if (!data.prisoner_hasrec) {
       nameIcon = 'exclamation-triangle';
     }
   }
+
   let nameOptions = [];
   let i = 0;
   for (i = 0; i < data.spns.length; i++) {
     nameOptions.push(data.spns[i]);
   }
+
   return (
-    <Window width={500} height={!data.timing ? 396 : 237}>
-      <Window.Content>
-        <Section title="Cell Information">
-          <LabeledList>
-            <LabeledList.Item label="Cell ID">{data.cell_id}</LabeledList.Item>
-            <LabeledList.Item label="Occupant">{data.nameText}</LabeledList.Item>
-            <LabeledList.Item label="Crimes">{data.crimes}</LabeledList.Item>
-            <LabeledList.Item label="Brigged By">{data.brigged_by}</LabeledList.Item>
-            <LabeledList.Item label="Time Brigged For">{data.time_set}</LabeledList.Item>
-            <LabeledList.Item label="Time Left">{data.time_left}</LabeledList.Item>
-            <LabeledList.Item label="Actions">
-              <>
-                <Button icon="lightbulb-o" content="Flash" disabled={!data.isAllowed} onClick={() => act('flash')} />
-                <Button
-                  icon="sync"
-                  content="Reset Timer"
-                  disabled={!data.timing || !data.isAllowed}
-                  onClick={() => act('restart_timer')}
-                />
-                <Button
-                  icon="eject"
-                  content="Release Prisoner"
-                  disabled={!data.timing || !data.isAllowed}
-                  onClick={() => act('stop')}
-                />
-              </>
-            </LabeledList.Item>
-          </LabeledList>
-        </Section>
-        {!data.timing && (
-          <Section title="New Prisoner">
-            <LabeledList>
-              <LabeledList.Item label="Prisoner Name">
-                <Button
-                  icon={nameIcon}
-                  content={data.prisoner_name ? data.prisoner_name : '-----'}
-                  disabled={!data.isAllowed}
-                  onClick={() => act('prisoner_name')}
-                />
-                {!!data.spns.length && (
-                  <Box mt="0.3rem">
-                    <Dropdown
-                      disabled={!data.isAllowed || !data.spns.length}
-                      options={data.spns}
-                      width="250px"
-                      onSelected={(value) =>
-                        act('prisoner_name', {
-                          prisoner_name: value,
-                        })
-                      }
-                    />
-                  </Box>
-                )}
-              </LabeledList.Item>
-              <LabeledList.Item label="Prisoner Crimes">
-                <Button
-                  icon="pencil-alt"
-                  content={data.prisoner_charge ? data.prisoner_charge : '-----'}
-                  disabled={!data.isAllowed}
-                  onClick={() => act('prisoner_charge')}
-                />
-              </LabeledList.Item>
-              <LabeledList.Item label="Prisoner Time">
-                <Button
-                  icon="pencil-alt"
-                  content={data.prisoner_time ? data.prisoner_time : '-----'}
-                  disabled={!data.isAllowed}
-                  onClick={() => act('prisoner_time')}
-                />
-              </LabeledList.Item>
-              <LabeledList.Item label="Start">
-                <Button
-                  icon="gavel"
-                  content="Start Sentence"
-                  disabled={!data.prisoner_name || !data.prisoner_charge || !data.prisoner_time || !data.isAllowed}
-                  onClick={() => act('start')}
-                />
-              </LabeledList.Item>
-            </LabeledList>
-          </Section>
+    <Window width={560} height={data.timing ? 270 : 710}>
+      <Window.Content scrollable>
+        {data.timing ? (
+          <Stack fill>
+            <Section title="Cell Information" grow>
+              <LabeledList>
+                <LabeledList.Item label="Cell ID">{data.cell_id}</LabeledList.Item>
+                <LabeledList.Item label="Occupant">{data.nameText}</LabeledList.Item>
+                <LabeledList.Item label="Crimes">{data.crimes}</LabeledList.Item>
+                <LabeledList.Item label="Brigged By">{data.brigged_by}</LabeledList.Item>
+                <LabeledList.Item label="Time Brigged For">{data.time_set}</LabeledList.Item>
+                <LabeledList.Item label="Time Left">{data.time_left}</LabeledList.Item>
+                <LabeledList.Item label="Actions">
+                  <Button icon="lightbulb-o" content="Flash" disabled={!data.isAllowed} onClick={() => act('flash')} />
+                  <Button
+                    icon="plus"
+                    content="Add 10 Minutes"
+                    disabled={!data.timing || !data.isAllowed}
+                    onClick={() => act('add_time')}
+                  />
+                  <Button
+                    icon="sync"
+                    content="Reset Timer"
+                    disabled={!data.timing || !data.isAllowed}
+                    onClick={() => act('restart_timer')}
+                  />
+                  <Button
+                    icon="eject"
+                    content="Release Prisoner"
+                    disabled={!data.timing || !data.isAllowed}
+                    onClick={() => act('stop')}
+                  />
+                </LabeledList.Item>
+              </LabeledList>
+            </Section>
+          </Stack>
+        ) : (
+          <Stack vertical fill>
+            <Stack.Item>
+              <Section title="New Prisoner">
+                <Stack horizontal>
+                  <Stack.Item>
+                    <LabeledList>
+                      <LabeledList.Item label="Prisoner Name">
+                        <Button
+                          icon={nameIcon}
+                          ellipsis
+                          maxWidth="190px"
+                          content={data.prisoner_name ? data.prisoner_name : '-----'}
+                          disabled={!data.isAllowed}
+                          onClick={() => act('prisoner_name')}
+                        />
+                        {!!data.spns.length && (
+                          <Box mt="0.3rem">
+                            <Dropdown
+                              disabled={!data.isAllowed || !data.spns.length}
+                              options={data.spns}
+                              width="150px"
+                              onSelected={(value) =>
+                                act('prisoner_name', {
+                                  prisoner_name: value,
+                                })
+                              }
+                            />
+                          </Box>
+                        )}
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Prisoner Notes">
+                        <Button
+                          icon="pencil-alt"
+                          ellipsis
+                          maxWidth="190px"
+                          wrap
+                          content={data.prisoner_notes ? data.prisoner_notes : '-----'}
+                          disabled={!data.isAllowed}
+                          onClick={() => act('prisoner_notes')}
+                        />
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Suggested Timer">{formattedSuggestedTimer}</LabeledList.Item>
+                      <LabeledList.Item label="Prisoner Time">
+                        <Button
+                          icon="pencil-alt"
+                          content={data.prisoner_time ? data.prisoner_time : '-----'}
+                          disabled={!data.isAllowed}
+                          onClick={() => act('prisoner_time')}
+                        />
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Start">
+                        <Button
+                          icon="gavel"
+                          content="Start Sentence"
+                          disabled={!data.prisoner_name || !crimeSummary || !data.prisoner_time || !data.isAllowed}
+                          onClick={() => act('start', { final_crimes: crimeSummary })}
+                        />
+                      </LabeledList.Item>
+                      <LabeledList.Item />
+                    </LabeledList>
+                  </Stack.Item>
+                  <Stack.Item grow>
+                    <Box bold>Crimes:</Box>
+                    <Box>{formattedCrimes}</Box>
+                    <Divider grow />
+                    <Box bold>Modifiers:</Box>
+                    <Box>{formattedModifiers ? formattedModifiers : 'N/A'}</Box>
+                  </Stack.Item>
+                </Stack>
+              </Section>
+            </Stack.Item>
+            <Stack.Item grow>
+              <Section title="Quick Timer">
+                <Stack vertical>
+                  <Stack.Item align="center">
+                    <Box bold>Crimes</Box>
+                  </Stack.Item>
+                  {Object.entries(spaceLaw.codes).map(([code, category]) => (
+                    <Stack.Item key={code}>
+                      <Stack>
+                        {Object.entries(category).map(([key, crime]) => (
+                          <Stack.Item key={key}>
+                            <Button
+                              tooltip={crime.description}
+                              onClick={() => {
+                                setSelectedCrimes(
+                                  selectedCrimes[code] === key
+                                    ? { ...selectedCrimes, [code]: null }
+                                    : { ...selectedCrimes, [code]: key }
+                                );
+                              }}
+                              selected={selectedCrimes[code] === key}
+                            >
+                              {crime.name}
+                            </Button>
+                          </Stack.Item>
+                        ))}
+                      </Stack>
+                    </Stack.Item>
+                  ))}
+                  <Divider />
+                  <Stack.Item align="center">
+                    <Box bold>Modifiers</Box>
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Box>
+                      <Stack vertical>
+                        {Object.entries(spaceLaw.modifiers).map(([modifierGroup, category]) => (
+                          <Stack.Item key={modifierGroup}>
+                            <Stack>
+                              {Object.entries(category).map(([key, modifier]) => (
+                                <Stack.Item key={key}>
+                                  <Button
+                                    tooltip={modifier.description}
+                                    onClick={() => {
+                                      setSelectedModifiers(
+                                        selectedModifiers[modifierGroup] === key
+                                          ? { ...selectedModifiers, [modifierGroup]: null }
+                                          : { ...selectedModifiers, [modifierGroup]: key }
+                                      );
+                                    }}
+                                    color={selectedModifiers[modifierGroup] === key ? 'green' : ''}
+                                  >
+                                    {modifier.name}
+                                  </Button>
+                                </Stack.Item>
+                              ))}
+                            </Stack>
+                          </Stack.Item>
+                        ))}
+                      </Stack>
+                    </Box>
+                  </Stack.Item>
+                </Stack>
+              </Section>
+            </Stack.Item>
+          </Stack>
         )}
       </Window.Content>
     </Window>
---
+++ tgui/packages/tgui/interfaces/common/SpaceLaw.jsx
@@ -0,0 +1,204 @@
+export const spaceLaw = {
+  codes: {
+    '00': {
+      damageStation: {
+        name: 'Damage to Station Assets',
+        severity: 'minor',
+        description: 'To deliberately damage the station or station property to a minor degree with malicious intent.',
+      },
+      workplaceHazard: {
+        name: 'Workplace Hazard',
+        severity: 'medium',
+        description: 'To endanger the crew or station through negligent but not deliberately malicious actions.',
+      },
+      sabotage: {
+        name: 'Sabotage',
+        severity: 'major',
+        description: 'To hinder the work of the crew or station through malicious actions.',
+      },
+    },
+    '01': {
+      kidnapping: {
+        name: 'Kidnapping',
+        severity: 'medium',
+        description: 'To hold a crewmember under duress or against their will.',
+      },
+    },
+    '02': {
+      battery: {
+        name: 'Battery',
+        severity: 'minor',
+        description: 'To use minor physical force against someone without intent to seriously injure them.',
+      },
+      assault: {
+        name: 'Assault',
+        severity: 'medium',
+        description: 'To use excessive physical force against someone without the apparent intent to kill them.',
+      },
+      aggrevatedAssault: {
+        name: 'Aggrevated Assault',
+        severity: 'major',
+        description: 'To use excessive physical force resulting in severe or life-threatening harm.',
+      },
+    },
+    '03': {
+      drugPossession: {
+        name: 'Drug Possession',
+        severity: 'minor',
+        description:
+          'The unauthorized possession of recreational use drugs such as ambrosia, krokodil, crank, meth, aranesp, bath salts, or THC.',
+      },
+      narcoticsDistribution: {
+        name: 'Narcotics Distribution',
+        severity: 'medium',
+        description:
+          'To distribute narcotics and other controlled substances. This includes ambrosia and space drugs. It is not illegal for them to be grown.',
+      },
+    },
+    '04': {
+      weaponPossession: {
+        name: 'Possession of a Weapon',
+        severity: 'medium',
+        description: "To be in possession of a dangerous item that is not part of one's job.",
+      },
+      restrictedWeapon: {
+        name: 'Possession of a Restricted Weapon',
+        severity: 'major',
+        description:
+          'To be in unauthorized possession of restricted weapons/items such as: Guns, Batons, Harmful Chemicals, Non-Beneficial Explosives, Combat Implants (Anti-Drop, CNS Rebooter, Razorwire), MODsuit Modules (Power Kick, Stealth).',
+      },
+    },
+    '05': {
+      indecentExposure: {
+        name: 'Indecent Exposure',
+        severity: 'minor',
+        description: 'To be intentionally and publicly unclothed.',
+      },
+      rioting: {
+        name: 'Rioting',
+        severity: 'medium',
+        description: 'To partake in an unauthorized and disruptive assembly of crewmen.',
+      },
+      incitingRiot: {
+        name: 'Inciting a Riot',
+        severity: 'major',
+        description: 'To attempt to stir the crew into a riot.',
+      },
+    },
+    '06': {
+      abuseEquiptment: {
+        name: 'Abuse of Equiptment',
+        severity: 'minor',
+        description: 'To utilize security/non-lethal equipment in an illegitimate fashion.',
+      },
+      abuseConfiscated: {
+        name: 'Abuse of Confiscated Equiptment',
+        severity: 'medium',
+        description: 'To take and use equipment confiscated as evidence.',
+      },
+      contrabandPossession: {
+        name: 'Possession of Contraband',
+        severity: 'major',
+        description: 'To be in the possession of contraband items.',
+      },
+    },
+    '07': {
+      pettyTheft: {
+        name: 'Petty Theft',
+        severity: 'minor',
+        description:
+          'To take items from areas one lacks access to or to take items belonging to others or the station as a whole.',
+      },
+      robbery: {
+        name: 'Robbery',
+        severity: 'medium',
+        description: "To steal items from another's person.",
+      },
+      theft: {
+        name: 'Theft',
+        severity: 'major',
+        description: "To steal restricted or dangerous items from either an area or one's person.",
+      },
+    },
+    '08': {
+      trespass: {
+        name: 'Trespass',
+        severity: 'minor',
+        description:
+          'To be in an area which a person lacks authorized ID access for. This counts for general areas of the station.',
+      },
+      majorTrespass: {
+        name: 'Major Trespass',
+        severity: 'major',
+        description:
+          'Being in a restricted area without prior authorization. This includes Security areas, Command areas (including EVA), the Engine Room, Atmos, or Toxins Research.',
+      },
+    },
+  },
+  modifiers: {
+    cooperation: {
+      cooperate: {
+        name: 'Cooperation with Security',
+        description: 'Being helpful to the members of security, revealing things during questioning.',
+        timeMultiplier: 0.5,
+      },
+      refuseCooperate: {
+        name: 'Refusal to Cooperate',
+        description: 'Non-cooperative behaviour while already detained inside the brig and awaiting a sentence.',
+        timeMultiplier: 1.5,
+      },
+    },
+    surrenderResisting: {
+      resistingArrest: {
+        name: 'Resisting Arrest',
+        description: 'To resist or impede an officer legally arresting or searching you.',
+        timeAdded: 5,
+      },
+      surrender: {
+        name: 'Surrender',
+        description: 'Willfully surrendering to Security.',
+        timeMultiplier: 0.5,
+      },
+    },
+    aiding: {
+      aidingAbetting: {
+        name: 'Aiding and Abetting',
+        description: 'To knowingly assist a criminal.',
+      },
+    },
+    officer: {
+      againstAnOfficer: {
+        name: 'Offence Against an Officer',
+        description:
+          'To batter, assault or kidnap an Officer. An Officer is defined as any member of Security, Command, or of Dignitary status (Magistrate, Blueshield, Representative). ',
+        timeAdded: 5,
+      },
+    },
+    repeatOffender: {
+      repeatOffenderFirst: {
+        name: 'Repeat Offender - Second Offence',
+        description: 'To be brigged for the same offense more than once.',
+        timeAdded: 10,
+      },
+      repeatOffenderSecond: {
+        name: 'Repeat Offender - Third Offence',
+        description: 'To be brigged for the same offense more than once.',
+        timeAdded: 20,
+      },
+    },
+  },
+  severities: {
+    minor: {
+      minTime: 1,
+      maxTime: 5,
+    },
+    medium: {
+      minTime: 5,
+      maxTime: 10,
+    },
+    major: {
+      minTime: 10,
+      maxTime: 15,
+    },
+  },
+};
